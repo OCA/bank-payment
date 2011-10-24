3a3151c62d16e6ca9bd1f57f122be962f768f705
Tristan Hill <tristan.hill@credativ.co.uk>
2011-10-24 16:26:13 +0100

    [IMP] import module to integrate with HSBC

---
+++ account_banking_uk_hsbc/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2011 credativ Ltd (<http://www.credativ.co.uk>).
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'HSBC Account Banking',
+    'version': '0.1',
+    'license': 'GPL-3',
+    'author': 'credativ Ltd',
+    'website': 'http://www.credativ.co.uk',
+    'category': 'Account Banking',
+    'depends': ['account_banking'],
+    'init_xml': [],
+    'update_xml': [
+        'account_banking_uk_hsbc.xml',
+        'data/banking_export_hsbc.xml',
+        'wizard/export_hsbc_view.xml',
+    ],
+    'demo_xml': [],
+    'description': '''
+    Module to import HSBC format transation files and to export payments for HSBC.net.
+
+    Currently targetting UK market.
+
+    This modules contains no logic, just an import/export filter for account_banking.
+    ''',
+    'active': False,
+    'installable': True,
+}
