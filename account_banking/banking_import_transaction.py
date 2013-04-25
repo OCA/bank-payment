32ed1691b8ec61a3ed1bce2413ea4dd342c97397
Stefan Rijnhart <stefan@therp.nl>
2013-04-25 21:28:56 +0200

    [FIX] Adapt to API change in last commit

--- account_banking/banking_import_transaction.py
+++ account_banking/banking_import_transaction.py
@@ -1318,8 +1318,7 @@ class banking_import_transaction(osv.osv):
                                 transaction.remote_owner, 
                                 transaction.remote_owner_address,
                                 transaction.remote_owner_city,
-                                country_id, results['log'],
-                                bic=transaction.remote_bank_bic
+                                country_id, bic=transaction.remote_bank_bic,
                                 )
                             partner_banks = partner_bank_obj.browse(
                                 cr, uid, [partner_bank_id]
