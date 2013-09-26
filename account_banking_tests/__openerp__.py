96ae2d6bba1d5121c4579a66be270a4e3812d2ee
Stefan Rijnhart <stefan@therp.nl>
2013-09-26 14:51:39 +0200

    [IMP] Description

--- account_banking_tests/__openerp__.py
+++ account_banking_tests/__openerp__.py
@@ -31,7 +31,11 @@
         'account_banking_sepa_credit_transfer',
         ],
     'description': '''
-    This addon adds unit tests for the Banking addons
+This addon adds unit tests for the Banking addons. Installing this
+module will not give you any benefit other than having the tests'
+dependencies installed, so that you can run the tests. If you only
+run the tests manually, you don't even have to install this module,
+only its dependencies.
     ''',
     'auto_install': False,
     'installable': True,
