0ebc565cd03462abb3cb83408ba0bb21feb06e51
Stefan Rijnhart <stefan@therp.nl>
2013-10-05 16:55:37 +0200

    [ADD] Attribution

--- account_bank_statement_tax/__openerp__.py
+++ account_bank_statement_tax/__openerp__.py
@@ -35,6 +35,8 @@
     'description': '''
 Allow an (inclusive) tax to be set on a bank statement line. When the
 statement is configured, the tax will be processed like on a move line.
+
+This module is co-funded by BAS Solutions.
     ''',
     'installable': True,
 }
