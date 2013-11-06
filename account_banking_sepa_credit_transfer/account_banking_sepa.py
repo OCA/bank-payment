9effe68814b907f7cb6a8130e0ec03b60b9f9ebe
Alexis de Lattre <alexis@via.ecp.fr>
2013-11-06 16:42:24 +0100

    [IMP] account_banking_sepa_credit_transfer: Support new format + other things:
    * Add support for pain.001.001.05 Fix dependencies (I need the field "payment_order_type" which is in account_banking_payment) Only use ASCII characters in the XML file (that's how banks use XML !) Use the payment order reference as the Message Identification of the XML file. Use the payment line sequence as the "End to End Identification" Remove the Instruction Identification ('InstrId') because it is not a required field (only the End to End identification is required) Rename variables in the function create_sepa() to include the number of each field as used in the EPC guidelines Reduce the number of flake8 warnings Make code more modular : - self.pool.get('banking.export.sepa').create() has a _prepare fonction - most of the content of the XML file can be inherited via _prepare_field()
    * Update strings. Update FR translation.
    * fields.date.context_today doesn't work with datetime => we now use the native field create_date Coding style improvements.
    * Add logo

--- account_banking_sepa_credit_transfer/account_banking_sepa.py
+++ account_banking_sepa_credit_transfer/account_banking_sepa.py
@@ -21,18 +21,24 @@
 
 from openerp.osv import orm, fields
 from openerp.addons.decimal_precision import decimal_precision as dp
+from unidecode import unidecode
 
 
 class banking_export_sepa(orm.Model):
     '''SEPA export'''
     _name = 'banking.export.sepa'
     _description = __doc__
-    _rec_name = 'msg_identification'
+    _rec_name = 'filename'
 
     def _generate_filename(self, cr, uid, ids, name, arg, context=None):
         res = {}
         for sepa_file in self.browse(cr, uid, ids, context=context):
-            res[sepa_file.id] = 'sepa_' + (sepa_file.msg_identification or '') + '.xml'
+            ref = sepa_file.payment_order_ids[0].reference
+            if ref:
+                label = unidecode(ref.replace('/', '-'))
+            else:
+                label = 'error'
+            res[sepa_file.id] = 'sct_%s.xml' % label
         return res
 
     _columns = {
@@ -40,36 +46,36 @@ class banking_export_sepa(orm.Model):
             'payment.order',
             'account_payment_order_sepa_rel',
             'banking_export_sepa_id', 'account_order_id',
-            'Payment orders',
+            'Payment Orders',
             readonly=True),
-        'prefered_exec_date': fields.date('Prefered execution date', readonly=True),
-        'nb_transactions': fields.integer('Number of transactions', readonly=True),
-        'total_amount': fields.float('Total amount',
+        'prefered_exec_date': fields.date(
+            'Prefered Execution Date', readonly=True),
+        'nb_transactions': fields.integer(
+            'Number of Transactions', readonly=True),
+        'total_amount': fields.float('Total Amount',
             digits_compute=dp.get_precision('Account'), readonly=True),
-        'msg_identification': fields.char('Message identification', size=35,
-            readonly=True),
-        'batch_booking': fields.boolean('Batch booking', readonly=True,
+        'batch_booking': fields.boolean(
+            'Batch Booking', readonly=True,
             help="If true, the bank statement will display only one debit line for all the wire transfers of the SEPA XML file ; if false, the bank statement will display one debit line per wire transfer of the SEPA XML file."),
         'charge_bearer': fields.selection([
             ('SHAR', 'Shared'),
-            ('CRED', 'Borne by creditor'),
-            ('DEBT', 'Borne by debtor'),
-            ('SLEV', 'Following service level'),
-            ], 'Charge bearer', readonly=True,
+            ('CRED', 'Borne by Creditor'),
+            ('DEBT', 'Borne by Debtor'),
+            ('SLEV', 'Following Service Level'),
+            ], 'Charge Bearer', readonly=True,
             help='Shared : transaction charges on the sender side are to be borne by the debtor, transaction charges on the receiver side are to be borne by the creditor (most transfers use this). Borne by creditor : all transaction charges are to be borne by the creditor. Borne by debtor : all transaction charges are to be borne by the debtor. Following service level : transaction charges are to be applied following the rules agreed in the service level and/or scheme.'),
-        'generation_date': fields.datetime('Generation date',
+        'create_date': fields.datetime('Generation Date', readonly=True),
+        'file': fields.binary('SEPA XML File', readonly=True),
+        'filename': fields.function(
+            _generate_filename, type='char', size=256, string='Filename',
             readonly=True),
-        'file': fields.binary('SEPA XML file', readonly=True),
-        'filename': fields.function(_generate_filename, type='char', size=256,
-            method=True, string='Filename', readonly=True),
         'state': fields.selection([
-                ('draft', 'Draft'),
-                ('sent', 'Sent'),
-                ('done', 'Reconciled'),
+            ('draft', 'Draft'),
+            ('sent', 'Sent'),
+            ('done', 'Reconciled'),
             ], 'State', readonly=True),
     }
 
     _defaults = {
-        'generation_date': fields.date.context_today,
         'state': 'draft',
     }
