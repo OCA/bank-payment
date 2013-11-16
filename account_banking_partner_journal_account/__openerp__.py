20c3fa915f5c990f3a277159e541a4f9c8b06912
Stefan Rijnhart <stefan@therp.nl>
2013-11-16 11:37:09 +0100

    [RFR] Refactor out the code that retrieves default debit or credit 	account of the partner [RFR] Remove suspicious references to the company partner 	debit and credit accounts [ADD] Module that allows for alternative partner journal accounts 	to be used as defaults in imported bank statements

---
+++ account_banking_partner_journal_account/__openerp__.py
@@ -0,0 +1,34 @@
+##############################################################################
+#
+#    Copyright (C) 2013 Therp BV (<http://therp.nl>)
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Banking Addons - Default partner journal accounts for bank transactions',
+    'version': '0.1',
+    'license': 'AGPL-3',
+    'author': 'Therp BV',
+    'website': 'https://launchpad.net/banking-addons',
+    'category': 'Banking addons',
+    'depends': ['account_banking'],
+    'description': '''
+This module allows to set alternative journal accounts on partners to use
+as default accounts in imported bank statements.
+    ''',
+    'data': ['res_partner_view.xml'],
+    'installable': True,
+}
