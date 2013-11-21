1194869ca24f40cca4a3e8417fdc00663db60179
Alexis de Lattre <alexis@via.ecp.fr>
2013-11-22 00:41:32 +0100

    [ADD] account_banking_pain_base: Akretion-Noviat code sprint dated 2013-11-21:
    - add module account_banking_pain_base
    - add support for initiating party identification, priority and structured remittance info in XML file
    - the requested execution date now uses the fields date_prefered and date_scheduled (the field of the wizard has been removed)
    - the 'convert to ascii' feature is now an option of the payment mode (enabled by default)
    - set 'communication' field of payment.line to 140 chars and hide field 'communication2'
    - Code factoring for SCT and SDD
    - Add support for direct debit migration from national format to SEPA
    - Source : Standard-XML-SDD-Initiation-v3-EN by Febelfin
    - Add group in order to mask the fields for "Original Mandate Indentification" for users in countries that don't required it.
    - Add tracking on important fields of the mandate.
    - Better form view on company.
    - Update constraint on mandate following my devs on "Original Mandate Identification".
    - Add support for Party Identifier for Belgium. Can be easily extended for other countries.
    - Mutualize more code between SCT and SDD.

---
+++ account_banking_pain_base/payment_line.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    PAIN Base module for OpenERP
+#    Copyright (C) 2013 Akretion (http://www.akretion.com)
+#    @author: Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+
+
+class payment_line(orm.Model):
+    _inherit = 'payment.line'
+
+    def _get_struct_communication_types(self, cr, uid, context=None):
+        return [('ISO', 'ISO')]
+
+    _columns = {
+        'priority': fields.selection([
+            ('NORM', 'Normal'),
+            ('HIGH', 'High'),
+            ], 'Priority',
+            help="This field will be used as the 'Instruction Priority' in "
+            "the generated PAIN file."),
+        # Update size from 64 to 140, because PAIN allows 140 caracters
+        'communication': fields.char(
+            'Communication', size=140, required=True,
+            help="Used as the message between ordering customer and current "
+            "company. Depicts 'What do you want to say to the recipient "
+            "about this order ?'"),
+        'struct_communication_type': fields.selection(
+            _get_struct_communication_types, 'Structured Communication Type'),
+    }
+
+    _defaults = {
+        'priority': 'NORM',
+        'struct_communication_type': 'ISO',
+    }
