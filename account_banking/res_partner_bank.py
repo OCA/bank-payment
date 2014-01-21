ef7b208267afd601183823a2a8b8297c61f4c566
Stefan Rijnhart <stefan@therp.nl>
2014-01-21 21:38:31 +0100

    [FIX] Missing imports [FIX] Model and class name [FIX] Keep and improve the account number expansion in res.partner.bank::search(). 	We need to search for the prettified IBANs as stored in the database. 	Maybe any matching up until now was by BBAN conversion?

--- account_banking/res_partner_bank.py
+++ account_banking/res_partner_bank.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2011 - 2014 Therp BV (<http://therp.nl>).
+#    Copyright (C) 2009 EduSense BV (<http://www.edusense.nl>).
+#              (C) 2011 - 2014 Therp BV (<http://therp.nl>).
 #            
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -18,10 +19,11 @@
 #
 ##############################################################################
 from openerp.osv import orm
+from openerp.addons.account_banking import sepa
 
 
-class ResBank(orm.Model):
-    _inherit = 'res.bank'
+class ResPartnerBank(orm.Model):
+    _inherit = 'res.partner.bank'
 
     def online_account_info(
             self, cr, uid, country_code, acc_number, context=None):
@@ -30,3 +32,72 @@ class ResBank(orm.Model):
         to be removed in OpenERP 8.0.
         """
         return False
+
+    def search(self, cr, uid, args, *rest, **kwargs):
+        '''
+        Overwrite search, as both acc_number and iban now can be filled, so
+        the original base_iban 'search and search again fuzzy' tactic now can
+        result in doubled findings. Also there is now enough info to search
+        for local accounts when a valid IBAN was supplied.
+        
+        Chosen strategy: create complex filter to find all results in just
+                         one search
+        '''
+
+        def is_term(arg):
+            '''Flag an arg as term or otherwise'''
+            return isinstance(arg, (list, tuple)) and len(arg) == 3
+
+        def extended_filter_term(term):
+            '''
+            Extend the search criteria in term when appropriate.
+            '''
+            extra_terms = []
+            if term[0].lower() == 'acc_number' and term[1] in ('=', '=='):
+                iban = sepa.IBAN(term[2])
+                import pdb
+                pdb.set_trace()
+                if iban.valid:
+                    extra_terms.append(('acc_number', term[1], iban.__repr__()))
+                    if 'acc_number_domestic' in self._columns:
+                        # Some countries can't convert to BBAN
+                        try:
+                            bban = iban.localized_BBAN
+                            # Prevent empty search filters
+                            if bban:
+                                extra_terms.append(('acc_number_domestic', term[1], bban))
+                        except:
+                            pass
+            result = [term]
+            for extra_term in extra_terms:
+                result = ['|'] + result + [extra_term]
+            return result
+
+        def extended_search_expression(args):
+            '''
+            Extend the search expression in args when appropriate.
+            The expression itself is in reverse polish notation, so recursion
+            is not needed.
+            '''
+            if not args:
+                return []
+
+            all = []
+            if is_term(args[0]) and len(args) > 1:
+                # Classic filter, implicit '&'
+                all += ['&']
+            
+            for arg in args:
+                if is_term(arg):
+                    all += extended_filter_term(arg)
+                else:
+                    all += arg
+            return all
+
+        # Extend search filter
+        newargs = extended_search_expression(args)
+        
+        # Original search
+        results = super(ResPartnerBank, self).search(
+            cr, uid, newargs, *rest, **kwargs)
+        return results
