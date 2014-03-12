034e5732b423ed9bb9476db88201f40d5082aa2c
Alexis de Lattre <alexis@via.ecp.fr>
2014-03-12 23:11:58 +0100

    Add module for French LCR.

---
+++ account_banking_fr_lcr/__openerp__.py
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    French LCR module for OpenERP
+#    Copyright (C) 2014 Akretion (http://www.akretion.com)
+#    @author: Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'French LCR',
+    'summary': 'Create French LCR CFONB files',
+    'version': '0.1',
+    'license': 'AGPL-3',
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'category': 'Banking addons',
+    'depends': ['account_direct_debit'],
+    'external_dependencies': {
+        'python': ['unidecode'],
+        },
+    'data': [
+        'account_banking_lcr_view.xml',
+        'wizard/export_lcr_view.xml',
+        'data/payment_type_lcr.xml',
+        'security/ir.model.access.csv',
+        ],
+    'demo': ['lcr_demo.xml'],
+    'description': '''
+French LCR
+==========
+
+This modules adds support for French LCR (Lettre de Change Relevés). This payment type is still in use in France and it is *not* replaced by SEPA one-off Direct Debits. With this module, you can generate a CFNOB file to send to your bank.
+
+This module uses the framework provided by the banking addons, cf https://launchpad.net/banking-addons
+
+Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for any help or question about this module.
+    ''',
+    'active': False,
+}
