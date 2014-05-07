1ecc7a1e1d22d8aee836ff9f05cbbdf5f9bdd599
Holger Brunn <hbrunn@therp.nl>
2014-05-07 14:35:54 +0200

    [FIX] type field can be arbitrary characters

--- account_banking_nl_ing_mt940/account_banking_nl_ing_mt940.py
+++ account_banking_nl_ing_mt940/account_banking_nl_ing_mt940.py
@@ -36,7 +36,7 @@ class IngMT940Parser(MT940, parser):
     code = 'INT_MT940_STRUC'
 
     tag_61_regex = re.compile(
-        '^(?P<date>\d{6})(?P<sign>[CD])(?P<amount>\d+,\d{2})N(?P<type>\d{3})'
+        '^(?P<date>\d{6})(?P<sign>[CD])(?P<amount>\d+,\d{2})N(?P<type>.{3})'
         '(?P<reference>\w{1,16})')
 
     def create_transaction(self, cr):
