348371ae7a9531065c7c03dc6c7d3f526d530318
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-09-10 12:48:09 +0200

    [MIG] Migration and enhancement of all modules involved in SEPA

---
+++ account_payment_partner/__openerp__.py
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Account Payment Partner module for OpenERP
+#    Copyright (C) 2014 Akretion (http://www.akretion.com)
+#    @author Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Account Payment Partner',
+    'version': '0.1',
+    'category': 'Banking addons',
+    'license': 'AGPL-3',
+    'summary': 'Adds payment mode on partners and invoices',
+    'description': """
+Account Payment Partner
+=======================
+
+This module adds severals fields :
+
+* the *Supplier Payment Mode* and *Customer Payment Mode* on Partners,
+
+* the *Payment Mode* on Invoices.
+
+On a Payment Order, in the wizard *Select Invoices to Pay*, the invoices will
+be filtered per Payment Mode.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'contributors': ['Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>'],
+    'depends': ['account_banking_payment_export'],
+    'data': [
+        'views/res_partner_view.xml',
+        'views/account_invoice_view.xml',
+    ],
+    'demo': ['demo/partner_demo.xml'],
+    'installable': True,
+}
