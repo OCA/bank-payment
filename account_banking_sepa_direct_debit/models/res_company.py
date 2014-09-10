c2dc29f9e754fa624ca44aef72fd6070a6d2d383
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-09-10 12:19:20 +0200

    [MIG] account_banking_sepa_direct_debit: Migration to 8.0

--- account_banking_sepa_direct_debit/models/res_company.py
+++ account_banking_sepa_direct_debit/models/res_company.py
@@ -20,29 +20,27 @@
 #
 ##############################################################################
 
-from openerp.osv import orm, fields
+from openerp import models, fields, api, exceptions, _
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-class res_company(orm.Model):
+class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    _columns = {
-        'sepa_creditor_identifier': fields.char(
-            'SEPA Creditor Identifier', size=35,
-            help="Enter the Creditor Identifier that has been attributed "
-            "to your company to make SEPA Direct Debits. This identifier "
-            "is composed of :\n- your country ISO code (2 letters)\n- a "
-            "2-digits checkum\n- a 3-letters business code\n- a "
-            "country-specific identifier"),
-        'original_creditor_identifier': fields.char(
-            'Original Creditor Identifier', size=70),
-    }
+    sepa_creditor_identifier = fields.Char(
+        string='SEPA Creditor Identifier', size=35,
+        help="Enter the Creditor Identifier that has been attributed to your "
+             "company to make SEPA Direct Debits. This identifier is composed "
+             "of :\n- your country ISO code (2 letters)\n- a 2-digits "
+             "checkum\n- a 3-letters business code\n- a country-specific "
+             "identifier")
+    original_creditor_identifier = fields.Char(
+        string='Original Creditor Identifier', size=70)
 
     def is_sepa_creditor_identifier_valid(
-            self, cr, uid, sepa_creditor_identifier, context=None):
+            self, sepa_creditor_identifier):
         """Check if SEPA Creditor Identifier is valid
         @param sepa_creditor_identifier: SEPA Creditor Identifier as str
             or unicode
@@ -51,12 +49,11 @@ class res_company(orm.Model):
         if not isinstance(sepa_creditor_identifier, (str, unicode)):
             return False
         try:
-            sci_str = str(sepa_creditor_identifier)
+            sci = str(sepa_creditor_identifier).lower()
         except:
             logger.warning(
                 "SEPA Creditor ID should contain only ASCII caracters.")
             return False
-        sci = sci_str.lower()
         if len(sci) < 9:
             return False
         before_replacement = sci[7:] + sci[0:2] + '00'
@@ -70,21 +67,14 @@ class res_company(orm.Model):
                 after_replacement += char
         logger.debug(
             "SEPA ID check after_replacement = %s" % after_replacement)
-        if int(sci[2:4]) == (98 - (int(after_replacement) % 97)):
-            return True
-        else:
-            return False
+        return int(sci[2:4]) == (98 - (int(after_replacement) % 97))
 
-    def _check_sepa_creditor_identifier(self, cr, uid, ids):
-        for company in self.browse(cr, uid, ids):
-            if company.sepa_creditor_identifier:
-                if not self.is_sepa_creditor_identifier_valid(
-                        cr, uid, company.sepa_creditor_identifier):
-                    return False
-        return True
-
-    _constraints = [
-        (_check_sepa_creditor_identifier,
-            "Invalid SEPA Creditor Identifier.",
-            ['sepa_creditor_identifier']),
-    ]
+    @api.one
+    @api.constrains('sepa_creditor_identifier')
+    def _check_sepa_creditor_identifier(self):
+        if self.sepa_creditor_identifier:
+            if not self.is_sepa_creditor_identifier_valid(
+                    self.sepa_creditor_identifier):
+                raise exceptions.Warning(
+                    _('Error'),
+                    _("Invalid SEPA Creditor Identifier."))
