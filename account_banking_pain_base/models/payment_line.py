d63d62d1a636a6105aa2be3dfc96600a2cc1c55d
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-09-10 12:19:20 +0200

    [MIG] account_banking_pain_base: Migration to 8.0

--- account_banking_pain_base/models/payment_line.py
+++ account_banking_pain_base/models/payment_line.py
@@ -23,19 +23,18 @@
 from openerp.osv import orm, fields
 
 
-class payment_line(orm.Model):
+class PaymentLine(orm.Model):
     _inherit = 'payment.line'
 
     def _get_struct_communication_types(self, cr, uid, context=None):
         return [('ISO', 'ISO')]
 
     _columns = {
-        'priority': fields.selection([
-            ('NORM', 'Normal'),
-            ('HIGH', 'High'),
-            ], 'Priority',
+        'priority': fields.selection(
+            [('NORM', 'Normal'),
+             ('HIGH', 'High')], 'Priority',
             help="This field will be used as the 'Instruction Priority' in "
-            "the generated PAIN file."),
+                 "the generated PAIN file."),
         # Update size from 64 to 140, because PAIN allows 140 caracters
         'communication': fields.char(
             'Communication', size=140, required=True,
