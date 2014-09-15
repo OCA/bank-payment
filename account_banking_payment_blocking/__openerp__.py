1ea580872d7e5af9872ded1a00d378fc75354e0d
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2014-09-15 15:46:56 +0200

    [ADD] account_banking_payment_blocking
    In the payment order creation wizard, do not propose invoices that are under litigation.
    
    This is based on the blocked (aka No Followup) flag on the account move lines.

---
+++ account_banking_payment_blocking/__openerp__.py
@@ -0,0 +1,52 @@
+##############################################################################
+#
+#    Copyright (C) ACSONE SA/NV (<http://acsone.eu>)
+#
+#    All other contributions are (C) by their respective contributors
+#
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'account banking payment blocking',
+    'version': '0.1',
+    'category': 'Banking addons',
+    'description': """
+    Prevent invoices under litigation to be proposed in payment orders
+    """,
+    'author': 'Stéphane Bidoul',
+    'website': 'http://acsone.eu',
+    'depends': [
+        'base',
+        'account_banking_payment_export'
+    ],
+    'data': [
+    ],
+    'test': [
+    ],
+    'demo': [
+    ],
+    'js': [
+    ],
+    'qweb': [
+    ],
+    'css': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
