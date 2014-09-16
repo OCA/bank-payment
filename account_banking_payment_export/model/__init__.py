5b9a026d98304d3351e25c5b30af289c176b1e5d
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-09-16 12:28:58 +0200

    [DEL] Duplicated files

--- account_banking_payment_export/model/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import account_move_line
-from . import account_payment
-from . import bank_payment_manual
-from . import payment_mode
-from . import payment_mode_type
-from . import payment_order_create
