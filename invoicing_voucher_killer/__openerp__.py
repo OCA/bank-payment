715fbe9c70855eea92720f79c3a28293d6a7d423
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2014-11-21 14:22:01 +0100

    [MIG] move invoicing_voucher_killer from OCA/bank-statement-reconcile/__upported__

---
+++ invoicing_voucher_killer/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2013 Camptocamp SA (http://www.camptocamp.com)
+#   @author Nicolas Bessi
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Invoicing voucher killer',
+ 'version': '1.0.0',
+ 'category': 'other',
+ 'description': """
+Prevent the usage of voucher from invoices
+##########################################
+This add-on will disable "Register Payment" button
+""",
+ 'author': 'Camptocamp',
+ 'website': 'http://www.camptocamp.com',
+ 'depends': ['account_voucher'],
+ 'data': ['invoice_data.xml',
+          'invoice_view.xml'],
+ 'demo_xml': [],
+ 'test': [],
+ 'installable': False,
+ 'active': False,
+ }
