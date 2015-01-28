c7a2e4a85147d2ecfcc17895f761af5768b366f8
Julien Laloux <julien.laloux@eezee-it.com>
2015-01-28 09:53:38 +0100

    Fix an error when no payment_order_ids

--- account_banking_sepa_credit_transfer/models/account_banking_sepa.py
+++ account_banking_sepa_credit_transfer/models/account_banking_sepa.py
@@ -33,7 +33,7 @@ class BankingExportSepa(models.Model):
 
     @api.one
     def _generate_filename(self):
-        ref = self.payment_order_ids[0].reference
+        ref = self.payment_order_ids and self.payment_order_ids[0].reference or False
         if ref:
             label = unidecode(ref.replace('/', '-'))
         else:
