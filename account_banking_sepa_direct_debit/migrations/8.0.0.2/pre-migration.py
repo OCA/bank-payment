5e7d262d6ddecb6db848b7396e021826c9f33ec5
Alexis de Lattre <alexis@via.ecp.fr>
2015-01-29 16:30:02 +0100

    Use attachments instead of creating a specific object for each kind of payment order

--- account_banking_sepa_direct_debit/migrations/8.0.0.2/pre-migration.py
+++ account_banking_sepa_direct_debit/migrations/8.0.0.2/pre-migration.py
@@ -1,8 +1,7 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    SEPA Direct Debit module for OpenERP
-#    Copyright (C) 2013 Akretion (http://www.akretion.com)
+#    Copyright (C) 2015 Akretion (http://www.akretion.com/)
 #    @author: Alexis de Lattre <alexis.delattre@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -20,5 +19,14 @@
 #
 ##############################################################################
 
-from . import res_company
-from . import account_banking_mandate
+
+def migrate(cr, version):
+    if not version:
+        return
+
+    cr.execute(
+        'ALTER TABLE banking_export_sdd '
+        'RENAME TO migration_banking_export_sdd')
+    cr.execute(
+        'ALTER TABLE account_payment_order_sdd_rel '
+        'RENAME TO migration_account_payment_order_sdd_rel')
