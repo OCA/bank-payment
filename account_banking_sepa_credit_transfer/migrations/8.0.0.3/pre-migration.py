2f48bb7e266e24ba8c22af04b3643d5ac97affe9
Alexis de Lattre <alexis@via.ecp.fr>
2015-01-29 16:30:02 +0100

    Use attachments instead of creating a specific object for each kind of payment order

--- account_banking_sepa_credit_transfer/migrations/8.0.0.3/pre-migration.py
+++ account_banking_sepa_credit_transfer/migrations/8.0.0.3/pre-migration.py
@@ -1,8 +1,7 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    SEPA Credit Transfer module for OpenERP
-#    Copyright (C) 2010-2013 Akretion (http://www.akretion.com)
+#    Copyright (C) 2015 Akretion (http://www.akretion.com/)
 #    @author: Alexis de Lattre <alexis.delattre@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -20,4 +19,14 @@
 #
 ##############################################################################
 
-from . import account_banking_sepa
+
+def migrate(cr, version):
+    if not version:
+        return
+
+    cr.execute(
+        'ALTER TABLE banking_export_sepa '
+        'RENAME TO migration_banking_export_sepa')
+    cr.execute(
+        'ALTER TABLE account_payment_order_sepa_rel '
+        'RENAME TO migration_account_payment_order_sepa_rel')
