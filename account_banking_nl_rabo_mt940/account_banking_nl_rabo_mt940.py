0b8cfbae827f6e937a80c477128b8b6fb82b29f9
Ronald Portier (Therp BV) <rportier@therp.nl>
2015-03-13 01:16:15 +0100

    [WIP] Change mt940 rabobank import to actually work.

--- account_banking_nl_rabo_mt940/account_banking_nl_rabo_mt940.py
+++ account_banking_nl_rabo_mt940/account_banking_nl_rabo_mt940.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+"""Implement parser for MT940 files - Rabobank dialect."""
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2014 Therp BV (<http://therp.nl>).
+#    Copyright (C) 2015 Therp BV <http://therp.nl>.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -19,19 +20,33 @@
 #
 ##############################################################################
 import re
+from string import printable
+import logging
+from datetime import datetime
 from openerp.tools.translate import _
-from openerp.addons.account_banking.parsers.models import parser,\
-    mem_bank_transaction
+from openerp.addons.account_banking.parsers.models import (
+    parser,
+    mem_bank_transaction,
+)
 from openerp.addons.account_banking_mt940.mt940 import MT940, str2float
 
 
-class transaction(mem_bank_transaction):
+class RabobankTransaction(mem_bank_transaction):
+    """Extentions for MT940 Rabobank transactions."""
+
     def is_valid(self):
-        '''allow transactions without remote account'''
-        return bool(self.execution_date) and bool(self.transferred_amount)
+        """allow transactions without remote account"""
+        if bool(self.execution_date) and bool(self.transferred_amount):
+            return True
+        if not bool(self.execution_date):
+            logging.debug(_('Missing execution_date in transaction'))
+        if not bool(self.transferred_amount):
+            logging.debug(_('Missing transferred_amount in transaction'))
+        return False
 
 
 class RaboMT940Parser(MT940, parser):
+    """Implement parser for MT940 files - Rabobank dialect."""
     name = _('MT940 Rabobank structured')
     country_code = 'NL'
     code = 'MT940 Rabobank'
@@ -42,56 +57,70 @@ class RaboMT940Parser(MT940, parser):
         r'(?P<reference>\w{1,16})')
 
     def parse(self, cr, data):
-        'implements account_banking.parsers.models.parser.parse()'
-        res = super(RaboMT940Parser, self).parse(cr, data + '\n-XXX')
-        return res
+        """Filter Unprintable characters from file data.
 
-    def handle_tag_25(self, cr, data):
-        '''get account owner information'''
-        self.current_statement.local_account = data.split(' ')[0]
+        The file contents of the Rabobank tend to contain unprintable
+        characters that prevent proper parsing. These will be removed.
+        """
+        data = ''.join([x for x in data if x in printable])
+        return super(RaboMT940Parser, self).parse(cr, data)
 
     def create_transaction(self, cr):
-        return transaction()
+        """Return Rabobank Transaction (with overriden is_valid() method)."""
+        return RabobankTransaction()
+
+    def handle_tag_25(self, cr, data):
+        """Handle tag 25: local bank account information."""
+        data = data.replace('EUR', '').replace('.', '').strip()
+        super(RaboMT940Parser, self).handle_tag_25(cr, data)
 
     def handle_tag_60F(self, cr, data):
-        super(RaboMT940Parser, self).handle_tag_60F(cr, data)
-        self.current_statement.id = '%s-%s' % (
-            self.get_unique_account_identifier(
-                cr, self.current_statement.local_account),
-            self.current_statement.id)
+        """get start balance and currency"""
+        # For the moment only first 60F record
+        # The alternative would be to split the file and start a new
+        # statement for each 20: tag encountered.
+        stmt = self.current_statement
+        if not stmt.local_currency:
+            stmt.local_currency = data[7:10]
+            stmt.date = datetime.strptime(data[1:7], '%y%m%d')
+            stmt.start_balance = (
+                (1 if data[0] == 'C' else -1) * str2float(data[10:]))
+            stmt.id = '%s-%s' % (
+                self.get_unique_account_identifier(cr, stmt.local_account),
+                stmt.id)
 
     def handle_tag_61(self, cr, data):
+        """Handle tag 61: transaction data."""
         super(RaboMT940Parser, self).handle_tag_61(cr, data)
         parsed_data = self.tag_61_regex.match(data).groupdict()
         self.current_transaction.transferred_amount = \
             (-1 if parsed_data['sign'] == 'D' else 1) * str2float(
                 parsed_data['amount'])
-        reference = parsed_data['reference']
-        self.current_transaction.reference = reference
-        # Bank account is last field, following reference. In some cases
-        # there might be a reference of the other bank. In that case the
-        # reference will be followed by '//' and the 16 positions for the
-        # other bank;
-        account_start = data.find(reference)
-        if account_start >= 0:
-            # If no reference found, we can not determine bank-account.
-            account_start += len(reference)
-            # check for other bank
-            other_start = data.find('//')
-            if other_start >= 0:
-                account_start = other_start + 18
-            if account_start < len(data):
-                self.current_transaction.remote_account = (
-                    data[account_start:].strip())
+        self.current_transaction.reference = parsed_data['reference']
 
     def handle_tag_86(self, cr, data):
+        """Handle tag 86: transaction details"""
         if not self.current_transaction:
             return
+
+        def get_counterpart(transaction, subfield):
+            """Same information can come from different field."""
+            if not subfield:
+                return  # subfield is empty
+            if len(subfield) >= 1 and subfield[0]:
+                transaction.remote_account = subfield[0]
+            if len(subfield) >= 2 and subfield[1]:
+                transaction.remote_bank_bic = subfield[1]
+            if len(subfield) >= 3 and subfield[2]:
+                transaction.remote_owner = subfield[2]
+            if len(subfield) >= 4 and subfield[3]:
+                transaction.remote_owner_city = subfield[3]
+
         super(RaboMT940Parser, self).handle_tag_86(cr, data)
-        codewords = ['MARF', 'EREF', 'PREF', 'RTRN', 'ACCW', 'BENM',
-                     'ORDP', 'NAME', 'ID', 'ADDR', 'REMI', 'CDTRREFTP',
-                     'CD', 'SCOR', 'ISSR', 'CUR', 'CDTRREF', 'CSID', 'ISDT',
-                     'ULTD', 'ULTB', 'PURP']
+        codewords = ['RTRN', 'BENM', 'ORDP', 'CSID', 'BUSP', 'MARF', 'EREF',
+                     'PREF', 'REMI', 'ID', 'PURP', 'ULTB', 'ULTD',
+                     'CREF', 'IREF', 'NAME', 'ADDR', 'ULTC', 'EXCH', 'CHGS']
+        # Fill subfields:
         subfields = {}
         current_codeword = None
         for word in data.split('/'):
@@ -103,24 +132,25 @@ class RaboMT940Parser(MT940, parser):
                 continue
             if current_codeword in subfields:
                 subfields[current_codeword].append(word)
-
+        # Use subfields for transaction details:
         if 'NAME' in subfields:
             self.current_transaction.remote_owner = (
                 ' '.join(subfields['NAME']))
-
         if 'ADDR' in subfields:
             # Do NOT join address fields, array is expected on other code!
             self.current_transaction.remote_owner_address = subfields['ADDR']
-
+        if 'BENM' in subfields:
+            get_counterpart(self.current_transaction, subfields['BENM'])
+        if 'ORDP' in subfields:
+            get_counterpart(self.current_transaction, subfields['ORDP'])
         if 'REMI' in subfields:
             self.current_transaction.message = '/'.join(
                 f for f in subfields['REMI'] if f)
-
         if self.current_transaction.reference in subfields:
             self.current_transaction.reference = ''.join(
                 subfields[self.current_transaction.reference])
-
         if not subfields:
             self.current_transaction.message = data
-
         self.current_transaction = None
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
