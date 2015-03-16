cfb5b7f5d521a9e463f5938ce7b96ea17e399b82
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-03-16 04:06:30 +0100

    [FIX] account_banking_pain_base: Warning text + demo data to not break tests

--- account_banking_pain_base/models/banking_export_pain.py
+++ account_banking_pain_base/models/banking_export_pain.py
@@ -272,7 +272,8 @@ class BankingExportPain(orm.AbstractModel):
         if not initiating_party_identifier or not initiating_party_issuer:
             raise orm.except_orm(
                 _("Error"),
-                _("You must fill initiating party identifier and issuer."))
+                _("You must fill initiating party identifier and issuer in "
+                  "the company form."))
         iniparty_id = etree.SubElement(initiating_party_1_8, 'Id')
         iniparty_org_id = etree.SubElement(iniparty_id, 'OrgId')
         iniparty_org_other = etree.SubElement(iniparty_org_id, 'Othr')
