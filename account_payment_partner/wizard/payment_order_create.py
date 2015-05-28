1b888b073bd9d53dc1a49559086c0d0bb2ae13f0
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-05-28 15:13:52 +0200

    [IMP] account_payment_partner/account_banking_payment_export: Allow to import invoices/move lines separately

--- account_payment_partner/wizard/payment_order_create.py
+++ account_payment_partner/wizard/payment_order_create.py
@@ -20,39 +20,43 @@
 #
 ##############################################################################
 
-from openerp import models, api
+from openerp import models, fields, api
 
 
 class PaymentOrderCreate(models.TransientModel):
     _inherit = 'payment.order.create'
 
+    def default_for_invoices(self):
+        return self.env.context.get('invoices', False)
+
+    for_invoices = fields.Boolean(default=default_for_invoices)
+    include_invoices = fields.Boolean(string="Include also invoices")
+
     @api.multi
     def extend_payment_order_domain(self, payment_order, domain):
         res = super(PaymentOrderCreate, self).extend_payment_order_domain(
             payment_order, domain)
-        # Monkey patch for fixing problem with the core search function
-        # when args has ('invoice', '=', False), referred in the issue #4857
-        # (https://github.com/odoo/odoo/issues/4857)
-        #
-        # Original domain:
-        # domain += ['|', '|',
-        #            ('invoice', '=', False),
-        #            ('invoice.payment_mode_id', '=', False),
-        #            ('invoice.payment_mode_id', '=', payment_order.mode.id)]
-        self.env.cr.execute(
-            "SELECT l.id "
-            "FROM account_move_line l "
-            "LEFT OUTER JOIN account_invoice i "
-            "ON l.move_id = i.move_id "
-            "INNER JOIN account_account a "
-            "ON a.id = l.account_id "
-            "WHERE i.id IS NULL"
-            "  AND l.reconcile_id IS NULL"
-            "  AND a.type in ('receivable', 'payable')")
-        ids = [x[0] for x in self.env.cr.fetchall()]
-        domain += ['|',
-                   ('id', 'in', ids),
-                   '|',
-                   ('invoice.payment_mode_id', '=', False),
-                   ('invoice.payment_mode_id', '=', payment_order.mode.id)]
+        if self.env.context.get('invoices'):
+            domain += ['|',
+                       ('invoice.payment_mode_id', '=', False),
+                       ('invoice.payment_mode_id', '=', payment_order.mode.id)]
+        elif not self.include_invoices:
+            # Monkey patch for fixing problem with the core search function
+            # when args has ('invoice', '=', False), referred in the issue
+            # #4857 (https://github.com/odoo/odoo/issues/4857)
+            #
+            # Original domain:
+            # domain += [('invoice', '=', False)]
+            self.env.cr.execute(
+                "SELECT l.id "
+                "FROM account_move_line l "
+                "LEFT OUTER JOIN account_invoice i "
+                "ON l.move_id = i.move_id "
+                "INNER JOIN account_account a "
+                "ON a.id = l.account_id "
+                "WHERE i.id IS NULL"
+                "  AND l.reconcile_id IS NULL"
+                "  AND a.type in ('receivable', 'payable')")
+            ids = [x[0] for x in self.env.cr.fetchall()]
+            domain += [('id', 'in', ids)]
         return res
