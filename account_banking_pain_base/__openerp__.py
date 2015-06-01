58c6e4bb3b26525c411ada16a677acc4445453fe
Philippe Schmidt <philippe.schmidt@acsone.eu>
2015-06-01 17:10:58 +0200

    Add requested  descriptions in an OCA README.rst file

--- account_banking_pain_base/__openerp__.py
+++ account_banking_pain_base/__openerp__.py
@@ -41,17 +41,5 @@
         'views/res_company_view.xml',
     ],
     'post_init_hook': 'set_default_initiating_party',
-    'description': '''
-Base module for PAIN file generation
-====================================
-
-This module contains fields and functions that are used by the module for SEPA
-Credit Transfer (account_banking_sepa_credit_transfer) and SEPA Direct Debit
-(account_banking_sepa_direct_debit). This module doesn't provide any
-functionality by itself.
-
-This module was started during the Akretion-Noviat code sprint of November
-21st 2013 in Epiais les Louvres (France).
-    ''',
     'installable': True,
 }
