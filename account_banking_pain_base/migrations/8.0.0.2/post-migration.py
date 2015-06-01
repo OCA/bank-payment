01c6e7abb7aa6367a795ff175673bde477305c75
Alexis de Lattre <alexis@via.ecp.fr>
2015-06-01 19:22:15 +0200

    Fix bug #111 New field initiating_party_identifier on res.company (migration script included)

--- account_banking_pain_base/migrations/8.0.0.2/post-migration.py
+++ account_banking_pain_base/migrations/8.0.0.2/post-migration.py
@@ -1,8 +1,7 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    PAIN Base module for OpenERP
-#    Copyright (C) 2013 Akretion (http://www.akretion.com)
+#    Copyright (C) 2015 Akretion (http://www.akretion.com/)
 #    @author: Alexis de Lattre <alexis.delattre@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -20,5 +19,14 @@
 #
 ##############################################################################
 
-from . import models
-from .post_install import set_default_initiating_party
+from openerp.addons.account_banking_pain_base.post_install\
+    import set_default_initiating_party
+from openerp import pooler
+
+
+def migrate(cr, version):
+    if not version:
+        return
+
+    pool = pooler.get_pool(cr.dbname)
+    set_default_initiating_party(cr, pool)
