84ed8f8c1edb5f9518379637eedd61f7551f30ae
Alexis de Lattre <alexis@via.ecp.fr>
2015-06-01 19:28:47 +0200

    Better import

--- account_banking_pain_base/__init__.py
+++ account_banking_pain_base/__init__.py
@@ -21,4 +21,4 @@
 ##############################################################################
 
 from . import models
-from post_install import set_default_initiating_party
+from .post_install import set_default_initiating_party
