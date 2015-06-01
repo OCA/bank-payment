3260c6c801105fc3203b492e4bd7fdef9ea68feb
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-06-01 15:09:37 +0200

    [FIX] let the not null constraint on payment_mode.type be created correctly
    This is a hack: in an ideal world, Odoo would set such
    constraint after module install, not before.

--- account_banking_payment_export/models/__init__.py
+++ account_banking_payment_export/models/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from . import account_payment
-from . import payment_mode
+# important: import payment_mode_type before payment_mode
+# to let the _auto_init work properly
 from . import payment_mode_type
+from . import payment_mode
 from . import account_move_line
 from . import account_invoice
