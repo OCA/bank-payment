3b6490ca854f15a0db43295b3bff092bfb77d02e
Philippe Schmidt <philippe.schmidt@acsone.eu>
2015-06-01 17:10:58 +0200

    Add requested descriptions in an OCA README.rst file

--- account_payment_purchase/__openerp__.py
+++ account_payment_purchase/__openerp__.py
@@ -26,19 +26,6 @@
     'category': 'Banking addons',
     'license': 'AGPL-3',
     'summary': "Adds Bank Account and Payment Mode on Purchase Orders",
-    'description': """
-Account Payment Purchase
-========================
-
-This module adds 2 fields on purchase orders: *Bank Account* and *Payment
-Mode*. These fields are copied from partner to purchase order and then from
-purchase order to supplier invoice.
-
-This module is similar to the *purchase_payment* module; the main difference
-is that it doesn't depend on the *account_payment_extension* module (it's not
-the only module to conflict with *account_payment_extension*; all the SEPA
-modules in the banking addons conflict with *account_payment_extension*).
-    """,
     'author': "Akretion, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
               "Odoo Community Association (OCA)",
