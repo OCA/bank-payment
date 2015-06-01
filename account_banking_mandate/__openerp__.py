eaa7b80f8868a657dad2976faba522fa1b21de27
Philippe Schmidt <philippe.schmidt@acsone.eu>
2015-06-01 17:10:58 +0200

    Add requested  descriptions in an OCA README.rst file

--- account_banking_mandate/__openerp__.py
+++ account_banking_mandate/__openerp__.py
@@ -45,16 +45,5 @@
     ],
     'demo': [],
     'test': ['test/banking_mandate.yml'],
-    'description': '''
-    This module adds a generic model for banking mandates.
-    These mandates can be specialized to fit any banking mandates (such as
-    sepa or lsv).
-
-    A banking mandate is attached to a bank account and represents an
-    authorization that the bank account owner gives to a company for a
-    specific operation (such as direct debit).
-    You can setup mandates from the accounting menu or directly from a bank
-    account.
-    ''',
     'installable': True,
 }
