4eb8ada3530179c8f943a3e7a6a902a6aff57a22
Alexis de Lattre <alexis@via.ecp.fr>
2015-06-02 11:13:26 +0200

    Replace SQL by code that uses the ORM, as suggested by Pedro Move part of the migration code to res_company.py, so that it's possible to inherit in other modules

--- account_banking_pain_base/migrations/8.0.0.2/post-migration.py
+++ account_banking_pain_base/migrations/8.0.0.2/post-migration.py
@@ -21,10 +21,12 @@
 
 from openerp.addons.account_banking_pain_base.post_install\
     import set_default_initiating_party
+from openerp import pooler
 
 
 def migrate(cr, version):
     if not version:
         return
 
-    set_default_initiating_party(cr, None)
+    pool = pooler.get_pool(cr.dbname)
+    set_default_initiating_party(cr, pool)
