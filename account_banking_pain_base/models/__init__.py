1c337d84f9d61a62352dd0cee6521583e926525c
Alexis de Lattre <alexis.delattre@akretion.com>
2015-06-06 00:20:41 +0200

    [FIX+IMP] account_banking_pain_base: Port to new API fix + Add bank.payment.lines object to allow grouping in the payments

--- account_banking_pain_base/models/__init__.py
+++ account_banking_pain_base/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    PAIN Base module for OpenERP
+#    PAIN Base module for Odoo
 #    Copyright (C) 2013 Akretion (http://www.akretion.com)
 #    @author: Alexis de Lattre <alexis.delattre@akretion.com>
 #
@@ -21,6 +21,7 @@
 ##############################################################################
 
 from . import payment_line
+from . import bank_payment_line
 from . import payment_mode
 from . import res_company
 from . import banking_export_pain
