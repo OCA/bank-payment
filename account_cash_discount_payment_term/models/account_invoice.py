6aed79e25527cd4eb27ba4ca2cb4460131c25274
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-09-01 11:11:05 +0200

    [ADD] Add account cash discount payment term module

---
+++ account_cash_discount_payment_term/models/account_invoice.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+##############################################################################
+#
+#    Authors: Adrien Peiffer
+#    Copyright (c) 2014 Acsone SA/NV (http://www.acsone.eu)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import api, models
+
+
+class account_invoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.one
+    @api.onchange('payment_term')
+    def change_payment_term_discount(self):
+        if self.type in ['in_invoice', 'out_invoice']:
+            self.discount_percent = self.payment_term.discount_percent
+            self.discount_delay = self.payment_term.discount_delay
