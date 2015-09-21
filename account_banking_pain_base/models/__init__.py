0590547adca5ff18f00119a6621d7bc65001a60a
Alexis de Lattre <alexis.delattre@akretion.com>
2015-09-21 12:32:45 +0200

    Add bank.payment.lines object to allow grouping in the payments

--- account_banking_pain_base/models/__init__.py
+++ account_banking_pain_base/models/__init__.py
@@ -21,6 +21,7 @@
 ##############################################################################
 
 from . import payment_line
+from . import bank_payment_line
 from . import payment_mode
 from . import res_company
 from . import banking_export_pain
