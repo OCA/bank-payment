f7594ec67da61e6941b6484970e8db587e7ad35d
Ronald Portier (Therp BV) <rportier@therp.nl>
2015-11-27 12:46:05 +0100

    [FIX] Correct pylint errors in test code.

--- account_banking_mt940/tests/test_mt940.py
+++ account_banking_mt940/tests/test_mt940.py
@@ -19,8 +19,7 @@
 #
 ##############################################################################
 from openerp.tests.common import SingleTransactionCase
-from openerp.addons.account_banking_mt940.mt940 import (
-    MT940, str2amount, get_subfields, handle_common_subfields)
+from openerp.addons.account_banking_mt940.mt940 import get_subfields
 
 
 class TestMT940(SingleTransactionCase):
@@ -28,7 +27,7 @@ class TestMT940(SingleTransactionCase):
     def check_subfields(self, reg, cr, uid):
         """Check splitting of structured field in subfields."""
         data = (
-           "/BENM//NAME/Kosten/REMI/Periode 01-10-2013 t/m 31-12-2013/ISDT/20"
+            "/BENM//NAME/Kosten/REMI/Periode 01-10-2013 t/m 31-12-2013/ISDT/20"
         )
         codewords = ['BENM', 'ADDR', 'NAME', 'CNTP', 'ISDT', 'REMI']
         subfields = get_subfields(data, codewords)
