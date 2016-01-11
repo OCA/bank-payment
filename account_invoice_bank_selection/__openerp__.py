097fb5c543231b2d54c94072a31476be8b14cff8
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-01-11 17:32:12 +0100

    [ADD] module account_invoice_bank_selection

---
+++ account_invoice_bank_selection/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016 Yannick Vaucher (Camptocamp SA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html). 
+{'name': 'Invoice - bank selection',
+ 'summary': 'Allow to choose bank per invoice',
+ 'version': '9.0.1.0.0',
+ 'category': '',
+ 'author': 'Camptocamp',
+ 'maintainer': 'Camptocamp, Odoo Community Association (OCA)',
+ 'website': 'http://www.camptocamp.com/',
+ 'depends': ['account'],
+ 'data': ['views/account_invoice.xml'],
+ 'test': [],
+ 'installable': True,
+ 'auto_install': False,
+ 'application': False,
+ 'license': 'AGPL-3',
+ }
