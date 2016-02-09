3a4075615f827296188fcafc2a9d07dfac1b2850
zeroincombenze <info@shs-av.com>
2016-02-09 12:14:34 +0100

    [NEW] SCT for Italy based on xml standard CBI

--- account_banking_pain_base/__openerp__.py
+++ account_banking_pain_base/__openerp__.py
@@ -43,13 +43,15 @@ Base module for PAIN file generation
 This module contains fields and functions that are used by the module for SEPA
 Credit Transfer (account_banking_sepa_credit_transfer) and SEPA Direct Debit
 (account_banking_sepa_direct_debit). This module doesn't provide any
-functionnality by itself.
+functionality by itself.
 
 This module is part of the banking addons:
     https://www.github.com/OCA/banking-addons
 
 This module was started during the Akretion-Noviat code sprint of
 November 21st 2013 in Epiais les Louvres (France).
+
+Module contains some pieces of code not ISO 20022 but used by Italian Banks.
     ''',
     'active': False,
     'installable': True,
