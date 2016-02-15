8981d2f34c76a72b3ef0fc20fbebb6e4ed27d3a8
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-02-15 23:10:18 +0100

    [IMP] account_banking_mandate: Cleanup
    * Short headers
    * Bump version numbers
    * es translations

--- account_banking_mandate/models/res_partner_bank.py
+++ account_banking_mandate/models/res_partner_bank.py
@@ -1,25 +1,7 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Mandate module for openERP
-#    Copyright (C) 2014 Compassion CH (http://www.compassion.ch)
-#    @author: Cyril Sester <csester@compassion.ch>,
-#             Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2014 Compassion CH - Cyril Sester <csester@compassion.ch>
+# © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields
 
