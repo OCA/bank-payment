264be465d1c0c76667f55f551996e78d0ed20354
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-02-15 23:10:18 +0100

    [IMP] *: Short headers

--- account_banking_pain_base/models/res_company.py
+++ account_banking_pain_base/models/res_company.py
@@ -1,26 +1,8 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    PAIN Base module for Odoo
-#    Copyright (C) 2013-2015 Akretion (http://www.akretion.com)
-#    Copyright (C) 2013 Noviat (http://www.noviat.com)
-#    @author: Alexis de Lattre <alexis.delattre@akretion.com>
-#    @author: Luc de Meyer (Noviat)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2013-2015 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
+# © 2013 Noviat (http://www.noviat.com) - Luc de Meyer
+# © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields, api
 import logging
