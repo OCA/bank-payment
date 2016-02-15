77ff49f9de8c2f31b22d7c1b764ad5973b84e8eb
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-02-15 23:10:18 +0100

    [IMP] account_payment_partner: Several things
    * Short headers
    * Remove label field
    
      There's already a field in the payment mode called 'note' that is
      printed on the invoices, so there's no need of another field for
      that purpose. This field is added by account_banking_payment_export
    
    * Bump version numbers
    * es translations
    * Name to filter for better inheratiblity

--- account_payment_partner/models/payment_mode.py
+++ account_payment_partner/models/payment_mode.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
+# © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields
 
@@ -25,9 +9,6 @@ from openerp import models, fields
 class PaymentMode(models.Model):
     _inherit = "payment.mode"
 
-    label = fields.Char(
-        string='Label', translate=True,
-        help="This field is designed to be used in the invoice report")
     default_payment_mode = fields.Selection([
         ('same', 'Same'),
         ('same_or_null', 'Same or empty'),
