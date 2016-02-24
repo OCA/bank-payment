37853e42308fe233bf212ca5eafcf6fa37063d6b
Gonzalo Borras <gonxi.gonzalo@gmail.com>
2016-02-24 20:39:12 +0100

    add changes for do bank account optional

--- account_banking_payment_export/models/payment_mode.py
+++ account_banking_payment_export/models/payment_mode.py
@@ -104,3 +104,7 @@ class PaymentMode(models.Model):
                 aj_ids = ajo.search([
                     ('type', 'in', ('sale_refund', 'sale'))]).ids
             self.default_journal_ids = [(6, 0, aj_ids)]
+
+    bank_id = fields.Many2one(required=False)
+    check_required = fields.Boolean(default=True, string="Bank account required",
+        help="This check do bank account optional for payment mode that isn't necessary to inform")
