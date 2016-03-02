6994eaf62dfa093e91620a5aeb7f0a4ee22ce95a
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-03-02 11:40:20 +0100

    [FIX] account_payment_transfer_reconcile_batch: Exclude non-processed moves

--- account_payment_transfer_reconcile_batch/__init__.py
+++ account_payment_transfer_reconcile_batch/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2015-2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
+from . import wizard
---
+++ account_payment_transfer_reconcile_batch/wizard/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import payment_order_create
