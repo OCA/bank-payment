ba26b849cd2f13ab43f2b5e60e4ce74940e6c529
Antonio Espinosa <antespi@gmail.com>
2016-03-04 17:15:53 +0100

    [IMP] Define SEPA identifiers per payment mode

--- account_banking_pain_base/__openerp__.py
+++ account_banking_pain_base/__openerp__.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 # © 2013-2015 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2016 Antiun Ingenieria S.L. - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Banking PAIN Base Module',
     'summary': 'Base module for PAIN file generation',
-    'version': '8.0.0.3.0',
+    'version': '8.0.0.4.0',
     'license': 'AGPL-3',
     'author': "Akretion, "
               "Noviat, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Antiun Ingeniería S.L., "
               "Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/bank-payment',
     'contributors': ['Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>'],
