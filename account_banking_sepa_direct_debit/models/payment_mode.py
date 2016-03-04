c59dd1331aad22597afb1130a4d0e913021a25b5
Antonio Espinosa <antespi@gmail.com>
2016-03-04 17:15:53 +0100

    Define SEPA identifiers per payment mode

--- account_banking_sepa_direct_debit/models/payment_mode.py
+++ account_banking_sepa_direct_debit/models/payment_mode.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2013 Akretion (www.akretion.com)
-# © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # © 2016 Antiun Ingenieria S.L. - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -8,18 +6,30 @@ from openerp import models, fields, api, exceptions, _
 from .common import is_sepa_creditor_identifier_valid
 
 
-class ResCompany(models.Model):
-    _inherit = 'res.company'
+class PaymentMode(models.Model):
+    _inherit = 'payment.mode'
 
     sepa_creditor_identifier = fields.Char(
         string='SEPA Creditor Identifier', size=35,
         help="Enter the Creditor Identifier that has been attributed to your "
-             "company to make SEPA Direct Debits. This identifier is composed "
-             "of :\n- your country ISO code (2 letters)\n- a 2-digits "
-             "checkum\n- a 3-letters business code\n- a country-specific "
-             "identifier")
+             "company to make SEPA Direct Debits. If not defined, "
+             "SEPA Creditor Identifier from company will be used.\n"
+             "This identifier is composed of :\n"
+             "- your country ISO code (2 letters)\n"
+             "- a 2-digits checkum\n"
+             "- a 3-letters business code\n"
+             "- a country-specific identifier")
     original_creditor_identifier = fields.Char(
-        string='Original Creditor Identifier', size=70)
+        string='Original Creditor Identifier', size=70,
+        help="If not defined, Original Creditor Identifier from company "
+             "will be used.")
+
+    def _sepa_type_get(self):
+        res = super(PaymentMode, self)._sepa_type_get()
+        if not res:
+            if self.type.code and self.type.code.startswith('pain.008'):
+                res = 'sepa_direct_debit'
+        return res
 
     @api.one
     @api.constrains('sepa_creditor_identifier')
