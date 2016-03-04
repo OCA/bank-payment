5bcebe7277f45c43761d5ff7049bcb32768972df
Antonio Espinosa <antespi@gmail.com>
2016-03-04 17:15:53 +0100

    Define SEPA identifiers per payment mode

--- account_banking_sepa_direct_debit/__openerp__.py
+++ account_banking_sepa_direct_debit/__openerp__.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 # © 2013-2015 Akretion (www.akretion.com)
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2016 Antiun Ingenieria S.L. - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Banking SEPA Direct Debit',
     'summary': 'Create SEPA files for Direct Debit',
-    'version': '8.0.0.3.0',
+    'version': '8.0.0.4.0',
     'license': 'AGPL-3',
     'author': "Akretion, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Antiun Ingeniería S.L., "
               "Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/bank-payment',
     'category': 'Banking addons',
@@ -21,6 +23,7 @@
     'data': [
         'views/account_banking_mandate_view.xml',
         'views/res_company_view.xml',
+        'views/payment_mode_view.xml',
         'wizard/export_sdd_view.xml',
         'data/mandate_expire_cron.xml',
         'data/payment_type_sdd.xml',
