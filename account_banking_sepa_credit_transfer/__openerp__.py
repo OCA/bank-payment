31265fe271f1ab8122814bf47ba77ecc6905fd30
Antonio Espinosa <antespi@gmail.com>
2016-03-04 17:15:53 +0100

    Define SEPA identifiers per payment mode

--- account_banking_sepa_credit_transfer/__openerp__.py
+++ account_banking_sepa_credit_transfer/__openerp__.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 # © 2010-2015 Akretion (www.akretion.com)
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2016 Antiun Ingenieria S.L. - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Banking SEPA Credit Transfer',
     'summary': 'Create SEPA XML files for Credit Transfers',
-    'version': '8.0.0.4.0',
+    'version': '8.0.0.5.0',
     'license': 'AGPL-3',
     'author': "Akretion, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Antiun Ingeniería S.L., "
               "Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/bank-payment',
     'category': 'Banking addons',
