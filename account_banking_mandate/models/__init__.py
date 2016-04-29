50d0756e941e80ff38c47e510c783f25123aab99
Alexis de Lattre <alexis.delattre@akretion.com>
2016-04-30 01:46:34 +0200

    [MIG] account_banking_mandate: Migration to v9

--- account_banking_mandate/models/__init__.py
+++ account_banking_mandate/models/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2014 Compassion CH - Cyril Sester <csester@compassion.ch>
-# © 2015 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import account_banking_mandate
+from . import account_payment_method
 from . import account_invoice
 from . import res_partner_bank
-from . import payment_line
+from . import res_partner
+from . import account_payment_line
 from . import bank_payment_line
+from . import account_move_line
