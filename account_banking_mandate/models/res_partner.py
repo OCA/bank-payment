0e489796c885c3e230187b7d398d3d9e76138113
Alexis de Lattre <alexis.delattre@akretion.com>
2016-04-30 01:46:34 +0200

    [MIG] account_banking_mandate: Migration to v9

---
+++ account_banking_mandate/models/res_partner.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    mandate_count = fields.Integer(
+        compute='_compute_mandate_count', string="Number of Mandates",
+        readonly=True)
+
+    @api.multi
+    def _compute_mandate_count(self):
+        mandate_data = self.env['account.banking.mandate'].read_group(
+            [('partner_id', 'in', self.ids)], ['partner_id'], ['partner_id'])
+        mapped_data = dict([
+            (mandate['partner_id'][0], mandate['partner_id_count'])
+            for mandate in mandate_data])
+        for partner in self:
+            partner.mandate_count = mapped_data.get(partner.id, 0)
