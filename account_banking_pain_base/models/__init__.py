19a0380b69f8720e0dd130bb2e7637b0497e8ec2
Alexis de Lattre <alexis.delattre@akretion.com>
2016-04-30 01:46:34 +0200

    [MIG] account_banking_pain_base: Migration to 9.0
    * Add support for non-SEPA DD and CT without without IBANs
    * Add hooks for l10n_ch_sepa (pain.001.001.03.ch.02)
    * Move code of account_banking_pain_base/models/banking_export_pain.py to account_banking_pain_base/models/account_payment_order.py
    * Add postal address in party block
    * Adds flexibility to support direct debit direct debit for switzerland (pain.008.001.02.ch.01)
    * Implement SEPA Core recommandations for postal address
    * Add field local_instrument in payment lines and bank payment lines
    * Add the bank_line as argument to some methods that generate SEPA XML

--- account_banking_pain_base/models/__init__.py
+++ account_banking_pain_base/models/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2013 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
+# © 2013-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import payment_line
+from . import account_payment_line
+from . import account_payment_order
 from . import bank_payment_line
-from . import payment_mode
+from . import account_payment_mode
 from . import res_company
-from . import banking_export_pain
-from . import res_partner_bank
+from . import account_payment_method
