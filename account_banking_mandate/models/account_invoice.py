0fa08b14c4f85e78ca3161a5bcd6437af67c2952
Alexis de Lattre <alexis.delattre@akretion.com>
2016-04-30 01:46:34 +0200

    Start to port bank-payment to v9 (with a lot of improvements) during the Sorrento Code sprint 2016
    Improvements include:
    - full re-organisation of modules and big re-organisation of the code
    - simplification of the code related to the fact that support for direct debit is now in t
    he base module, not added by an optional module account_direct_debit (module was removed)
    - new design of the wizard to select move lines to pay
    - support for non-SEPA file transfer-
    - support for German direct debit SEPA files (fixes bug #129)
    - remove workflow of payment.order
    
    This port to v9 is not finished... there is still a lot of work:
    - finish the code of account_payment_order/wizard/account_payment_line_create.py
    - port account_banking_payment_transfer and integrate it inside account_payment_order
    - fix bugs
    - clean-up code, remove dead code
    - test in several complex scenarios

--- account_banking_mandate/models/account_invoice.py
+++ account_banking_mandate/models/account_invoice.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # © 2014 Compassion CH - Cyril Sester <csester@compassion.ch>
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class AccountInvoice(models.Model):
@@ -12,5 +13,15 @@ class AccountInvoice(models.Model):
 
     mandate_id = fields.Many2one(
         'account.banking.mandate', string='Direct Debit Mandate',
-        domain=[('state', '=', 'valid')], readonly=True,
-        states={'draft': [('readonly', False)]})
+        ondelete='restrict',
+        readonly=True, states={'draft': [('readonly', False)]})
+
+    @api.model
+    def line_get_convert(self, line, part):
+        """Copy mandate from invoice to account move line"""
+        res = super(AccountInvoice, self).line_get_convert(line, part)
+        if line.get('type') == 'dest' and line.get('invoice_id'):
+            invoice = self.browse(line['invoice_id'])
+            if invoice.type in ('out_invoice', 'out_refund'):
+                res['mandate_id'] = invoice.mandate_id.id or False
+        return res
--- account_banking_mandate/models/account_payment_line.py
+++ account_banking_mandate/models/account_payment_line.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 # © 2014 Compassion CH - Cyril Sester <csester@compassion.ch>
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
-# © 2015 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
+# © 2015-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, exceptions, _
+from openerp import models, fields, api, _
+from openerp.exceptions import ValidationError
 
 
-class PaymentLine(models.Model):
-    _inherit = 'payment.line'
+class AccountPaymentLine(models.Model):
+    _inherit = 'account.payment.line'
 
     mandate_id = fields.Many2one(
         comodel_name='account.banking.mandate', string='Direct Debit Mandate',
         domain=[('state', '=', 'valid')])
 
+    # TODO : remove this
     @api.model
     def create(self, vals=None):
         """If the customer invoice has a mandate, take it
@@ -21,7 +23,7 @@ class PaymentLine(models.Model):
         """
         if vals is None:
             vals = {}
-        partner_bank_id = vals.get('bank_id')
+        partner_bank_id = vals.get('partner_bank_id')
         move_line_id = vals.get('move_line_id')
         if (self.env.context.get('search_payment_order_type') == 'debit' and
                 'mandate_id' not in vals):
@@ -31,7 +33,7 @@ class PaymentLine(models.Model):
                         line.invoice.mandate_id):
                     vals.update({
                         'mandate_id': line.invoice.mandate_id.id,
-                        'bank_id': line.invoice.mandate_id.partner_bank_id.id,
+                        'partner_bank_id': line.invoice.mandate_id.partner_bank_id.id,
                     })
             if partner_bank_id and 'mandate_id' not in vals:
                 mandates = self.env['account.banking.mandate'].search(
@@ -39,21 +41,23 @@ class PaymentLine(models.Model):
                      ('state', '=', 'valid')])
                 if mandates:
                     vals['mandate_id'] = mandates[0].id
-        return super(PaymentLine, self).create(vals)
+        return super(AccountPaymentLine, self).create(vals)
 
     @api.one
-    @api.constrains('mandate_id', 'bank_id')
+    @api.constrains('mandate_id', 'partner_bank_id')
     def _check_mandate_bank_link(self):
-        if (self.mandate_id and self.bank_id and
+        if (self.mandate_id and self.partner_bank_id and
                 self.mandate_id.partner_bank_id.id !=
-                self.bank_id.id):
-            raise exceptions.Warning(
+                self.partner_bank_id.id):
+            raise ValidationError(
                 _("The payment line with reference '%s' has the bank account "
                   "'%s' which is not attached to the mandate '%s' (this "
                   "mandate is attached to the bank account '%s').") %
                 (self.name,
-                 self.env['res.partner.bank'].name_get(
-                     [self.bank_id.id])[0][1],
+                 self.partner_bank_id.name_get()[0][1],
                  self.mandate_id.unique_mandate_reference,
-                 self.env['res.partner.bank'].name_get(
-                     [self.mandate_id.partner_bank_id.id])[0][1]))
+                 self.mandate_id.partner_bank_id.name_get()[0][1]))
+
+#    @api.multi
+#    def check_payment_line(self):
+# TODO : i would like to block here is mandate is missing... but how do you know it's required ? => create option on payment order ?
