4aeba8989c1308b925dc5cc07adf1b134e7ec1c9
Alexis de Lattre <alexis.delattre@akretion.com>
2016-04-30 01:46:34 +0200

    [MIG] account_banking_sepa_credit_transfer: Migration to 9.0

--- account_banking_sepa_credit_transfer/__openerp__.py
+++ account_banking_sepa_credit_transfer/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2010-2015 Akretion (www.akretion.com)
+# © 2010-2016 Akretion (www.akretion.com)
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # © 2016 Antiun Ingenieria S.L. - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -7,7 +7,7 @@
 {
     'name': 'Account Banking SEPA Credit Transfer',
     'summary': 'Create SEPA XML files for Credit Transfers',
-    'version': '8.0.0.5.0',
+    'version': '9.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Akretion, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
@@ -15,13 +15,14 @@
               "Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/bank-payment',
     'category': 'Banking addons',
+    'conflicts': ['account_sepa'],
     'depends': ['account_banking_pain_base'],
     'data': [
-        'wizard/export_sepa_view.xml',
-        'data/payment_type_sepa_sct.xml',
+        'data/account_payment_method.xml',
     ],
     'demo': [
         'demo/sepa_credit_transfer_demo.xml'
     ],
+    'post_init_hook': 'update_bank_journals',
     'installable': True,
 }
