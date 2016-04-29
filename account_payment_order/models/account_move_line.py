d052cc7274701ee9262b773529ea7e1de8c8be65
Alexis de Lattre <alexis.delattre@akretion.com>
2016-04-30 01:46:34 +0200

    Start to port bank-payment to v9 (with a lot of improvements) during the Sorrento Code sprint 2016
    Improvements include:
    - full re-organisation of modules and big re-organisation of the code
    - simplification of the code related to the fact that support for direct debit is now in t
    he base module, not added by an optional module account_direct_debit (module was removed)
    - new design of the wizard to select move lines to pay
    - support for non-SEPA file transfer-
    - support for German direct debit SEPA files (fixes bug #129)
    - remove workflow of payment.order
    - add wizard of selection of move lines to pay

---
+++ account_payment_order/models/account_move_line.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    partner_bank_id = fields.Many2one(
+        'res.partner.bank', string='Partner Bank Account',
+        help='Bank account on which we should pay the supplier')
+    bank_payment_line_id = fields.Many2one(
+        'bank.payment.line', string='Bank Payment Line',
+        readonly=True)
+
+    @api.multi
+    def _prepare_payment_line_vals(self, payment_order):
+        self.ensure_one()
+        assert payment_order, 'Missing payment order'
+        aplo = self.env['account.payment.line']
+        # default values for communication_type and communication
+        communication_type = 'normal'
+        communication = self.move_id.name or '-'
+        # change these default values if move line is linked to an invoice
+        if self.invoice_id:
+            if self.invoice_id.reference_type != 'none':
+                communication = self.invoice_id.reference
+                ref2comm_type =\
+                    aplo.invoice_reference_type2communication_type()
+                communication_type =\
+                    ref2comm_type[self.invoice_id.reference_type]
+            else:
+                if (
+                        self.invoice_id.type in ('in_invoice', 'in_refund') and
+                        self.invoice_id.reference):
+                    communication = self.invoice_id.reference
+        if self.currency_id:
+            currency_id = self.currency_id.id
+            amount_currency = self.amount_residual_currency
+        else:
+            currency_id = self.company_id.currency_id.id
+            amount_currency = self.amount_residual
+            # TODO : check that self.amount_residual_currency is 0
+            # in this case
+        if payment_order.payment_type == 'outbound':
+            amount_currency *= -1
+        vals = {
+            'order_id': payment_order.id,
+            'partner_bank_id': self.partner_bank_id.id,
+            'partner_id': self.partner_id.id,
+            'move_line_id': self.id,
+            'communication': communication,
+            'communication_type': communication_type,
+            'currency_id': currency_id,
+            'amount_currency': amount_currency,
+            # date is set when the user confirms the payment order
+            }
+        return vals
+
+    @api.multi
+    def create_payment_line_from_move_line(self, payment_order):
+        aplo = self.env['account.payment.line']
+        for mline in self:
+            aplo.create(mline._prepare_payment_line_vals(payment_order))
+        return
