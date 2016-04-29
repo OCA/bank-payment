00bbd54aad8060dd0be53bd3c5b7a6c12de9b929
Alexis de Lattre <alexis.delattre@akretion.com>
2016-04-30 01:46:34 +0200

    [MIG] account_banking_mandate: Migration to v9

--- account_banking_mandate/__openerp__.py
+++ account_banking_mandate/__openerp__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # © 2014 Compassion CH - Cyril Sester <csester@compassion.ch>
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
-# © 2015 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
+# © 2015-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Banking Mandate',
     'summary': 'Banking mandates',
-    'version': '8.0.0.2.0',
+    'version': '9.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Compassion CH, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
@@ -16,19 +16,21 @@
     'website': 'https://github.com/OCA/bank-payment',
     'category': 'Banking addons',
     'depends': [
-        'account_banking_payment_export',
+        'account_payment_order',
     ],
     'data': [
         'views/account_banking_mandate_view.xml',
+        'views/account_payment_method.xml',
         'views/account_invoice_view.xml',
-        'views/account_payment_view.xml',
+        'views/account_payment_line.xml',
         'views/res_partner_bank_view.xml',
+        'views/res_partner.xml',
         'views/bank_payment_line_view.xml',
+        'views/account_move_line.xml',
         'data/mandate_reference_sequence.xml',
         'security/mandate_security.xml',
         'security/ir.model.access.csv',
     ],
-    'demo': [],
     'test': ['test/banking_mandate.yml'],
     'installable': True,
 }
