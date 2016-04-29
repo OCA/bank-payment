e083da2ceae28d4ba43db1936993b5ca3d1216de
Alexis de Lattre <alexis.delattre@akretion.com>
2016-04-30 01:46:34 +0200

    [MIG] account_banking_pain_base: Migration to 9.0
    * Add support for non-SEPA DD and CT without without IBANs
    * Add hooks for l10n_ch_sepa (pain.001.001.03.ch.02)
    * Move code of account_banking_pain_base/models/banking_export_pain.py to account_banking_pain_base/models/account_payment_order.py
    * Add postal address in party block
    * Adds flexibility to support direct debit direct debit for switzerland (pain.008.001.02.ch.01)
    * Implement SEPA Core recommandations for postal address
    * Add field local_instrument in payment lines and bank payment lines
    * Add the bank_line as argument to some methods that generate SEPA XML

--- account_banking_pain_base/__openerp__.py
+++ account_banking_pain_base/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2013-2015 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
+# © 2013-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # © 2016 Antiun Ingenieria S.L. - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -7,7 +7,7 @@
 {
     'name': 'Account Banking PAIN Base Module',
     'summary': 'Base module for PAIN file generation',
-    'version': '8.0.0.4.0',
+    'version': '9.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Akretion, "
               "Noviat, "
@@ -17,15 +17,17 @@
     'website': 'https://github.com/OCA/bank-payment',
     'contributors': ['Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>'],
     'category': 'Hidden',
-    'depends': ['account_banking_payment_export'],
+    'depends': ['account_payment_order'],
     'external_dependencies': {
         'python': ['unidecode', 'lxml'],
     },
     'data': [
-        'views/payment_line_view.xml',
+        'views/account_payment_line.xml',
+        'views/account_payment_order.xml',
         'views/bank_payment_line_view.xml',
-        'views/payment_mode_view.xml',
+        'views/account_payment_mode.xml',
         'views/res_company_view.xml',
+        'views/account_payment_method.xml',
     ],
     'post_init_hook': 'set_default_initiating_party',
     'installable': True,
