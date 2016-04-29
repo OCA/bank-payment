294cd5ffe984e9b126d9aafcae1c967fe0f9ac07
Alexis de Lattre <alexis.delattre@akretion.com>
2016-04-30 01:46:34 +0200

    [MIG] account_payment_partner: Start to port bank-payment to v9 (with a lot of improvements) during the Sorrento Code sprint 2016
    Improvements include:
    - full re-organisation of modules and big re-organisation of the code
    - simplification of the code related to the fact that support for direct debit is now in t
    he base module, not added by an optional module account_direct_debit (module was removed)
    - new design of the wizard to select move lines to pay
    - support for non-SEPA file transfer-
    - support for German direct debit SEPA files (fixes bug #129)
    - remove workflow of payment.order
    * Finalise the wizard of selection of move lines to pay
    * Add button "Add to payment/debit order" on invoice form view
    * Started to integrate payment transfer in account_payment_order (not finished at all though)
    * Copy payment mode and mandate from invoice to refund
    * Run dos2unix on the few files that had DOS carriage return
    * Add _id suffix on M2O fields customer_payment_mode and supplier_payment_mode (Odoo did the same on all the M2O property fields of res.partner)
    * Better handling of partner_bank_id and mandate_id on invoice: invisible/required/onchange
    * Move field bank_account_required from module account_payment_partner to account_payment_mode

--- account_payment_partner/models/account_invoice.py
+++ account_payment_partner/models/account_invoice.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2014 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
+# © 2014-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -10,27 +10,70 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     payment_mode_id = fields.Many2one(
-        comodel_name='payment.mode', string="Payment Mode",
-        domain="[('type', '=', type)]")
+        comodel_name='account.payment.mode', string="Payment Mode",
+        ondelete='restrict',
+        readonly=True, states={'draft': [('readonly', False)]})
+    bank_account_required = fields.Boolean(
+        related='payment_mode_id.payment_method_id.bank_account_required',
+        readonly=True)
 
-    @api.multi
-    def onchange_partner_id(
-            self, type, partner_id, date_invoice=False,
-            payment_term=False, partner_bank_id=False, company_id=False):
-        res = super(AccountInvoice, self).onchange_partner_id(
-            type, partner_id, date_invoice=date_invoice,
-            payment_term=payment_term, partner_bank_id=partner_bank_id,
-            company_id=company_id)
-        if partner_id:
-            partner = self.env['res.partner'].browse(partner_id)
-            if type == 'in_invoice':
-                res['value']['payment_mode_id'] = \
-                    partner.supplier_payment_mode.id
-            elif type == 'out_invoice':
-                res['value'].update({
-                    'payment_mode_id': partner.customer_payment_mode.id,
-                    'partner_bank_id': partner.customer_payment_mode.bank_id.id
-                })
+    @api.onchange('partner_id', 'company_id')
+    def _onchange_partner_id(self):
+        super(AccountInvoice, self)._onchange_partner_id()
+        if self.partner_id:
+            if self.type == 'in_invoice':
+                pay_mode = self.partner_id.supplier_payment_mode_id
+                self.payment_mode_id = pay_mode
+                if (
+                        pay_mode and
+                        pay_mode.payment_type == 'outbound' and
+                        pay_mode.payment_method_id.bank_account_required and
+                        self.commercial_partner_id.bank_ids):
+                    self.partner_bank_id =\
+                        self.commercial_partner_id.bank_ids[0]
+            elif self.type == 'out_invoice':
+                pay_mode = self.partner_id.customer_payment_mode_id
+                self.payment_mode_id = pay_mode
+                if pay_mode and pay_mode.bank_account_link == 'fixed':
+                    self.partner_bank_id = pay_mode.fixed_journal_id.\
+                        bank_account_id
         else:
-            res['value']['payment_mode_id'] = False
+            self.payment_mode_id = False
+            if self.type == 'in_invoice':
+                self.partner_bank_id = False
+
+    @api.onchange('payment_mode_id')
+    def payment_mode_id_change(self):
+        if (
+                self.payment_mode_id and
+                self.payment_mode_id.payment_type == 'outbound' and
+                not self.payment_mode_id.payment_method_id.
+                bank_account_required):
+            self.partner_bank_id = False
+        elif not self.payment_mode_id:
+            self.partner_bank_id = False
+
+    @api.model
+    def line_get_convert(self, line, part):
+        """Copy payment mode from invoice to account move line"""
+        res = super(AccountInvoice, self).line_get_convert(line, part)
+        if line.get('type') == 'dest' and line.get('invoice_id'):
+            invoice = self.browse(line['invoice_id'])
+            res['payment_mode_id'] = invoice.payment_mode_id.id or False
         return res
+
+    # I think copying payment mode from invoice to refund by default
+    # is a good idea because the most common way of "paying" a refund is to
+    # deduct it on the payment of the next invoice (and OCA/bank-payment
+    # allows to have negative payment lines since March 2016)
+    @api.model
+    def _prepare_refund(
+            self, invoice, date_invoice=None, date=None, description=None,
+            journal_id=None):
+        vals = super(AccountInvoice, self)._prepare_refund(
+            invoice, date_invoice=date_invoice, date=date,
+            description=description, journal_id=journal_id)
+        vals['payment_mode_id'] = invoice.payment_mode_id.id
+        if invoice.type == 'in_invoice':
+            vals['partner_bank_id'] = invoice.partner_bank_id.id
+        return vals
