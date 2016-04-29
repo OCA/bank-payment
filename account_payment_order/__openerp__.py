5b8b35163ea92fee72a04a5372a3b296979305c8
Alexis de Lattre <alexis.delattre@akretion.com>
2016-04-30 01:46:34 +0200

    Start to port bank-payment to v9 (with a lot of improvements) during the Sorrento Code sprint 2016
    Improvements include:
    - full re-organisation of modules and big re-organisation of the code
    - simplification of the code related to the fact that support for direct debit is now in t
    he base module, not added by an optional module account_direct_debit (module was removed)
    - new design of the wizard to select move lines to pay
    - support for non-SEPA file transfer-
    - support for German direct debit SEPA files (fixes bug #129)
    - remove workflow of payment.order
    - add wizard of selection of move lines to pay

---
+++ account_payment_order/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# © 2009 EduSense BV (<http://www.edusense.nl>)
+# © 2011-2013 Therp BV (<http://therp.nl>)
+# © 2013-2014 ACSONE SA (<http://acsone.eu>).
+# © 2014-2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2016 Akretion (<http://www.akretion.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Account Payment Order',
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': "ACSONE SA/NV, "
+              "Therp BV, "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Akretion, "
+              "Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/bank-payment',
+    'category': 'Banking addons',
+    'depends': [
+        'account_payment_partner',
+        'base_iban',  # for manual_bank_tranfer
+        'document',  # to see the attachments on payment.order
+    ],
+    'data': [
+        'security/payment_security.xml',
+        'security/ir.model.access.csv',
+        'wizard/account_payment_line_create_view.xml',
+        'wizard/account_invoice_payment_line_multi_view.xml',
+        'views/account_payment_mode.xml',
+        'views/account_payment_order.xml',
+        'views/account_payment_line.xml',
+        'views/bank_payment_line.xml',
+        'views/account_move_line.xml',
+        'views/ir_attachment.xml',
+        'views/account_invoice_view.xml',
+        'data/payment_seq.xml',
+    ],
+    'demo': ['demo/payment_demo.xml'],
+    'installable': True,
+}
---
+++ account_payment_order/migrations/8.0.0.1.166/pre-migrate.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This module copyright (C) 2015 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+def migrate(cr, version):
+    cr.execute(
+        'SELECT count(attname) FROM pg_attribute '
+        'WHERE attrelid = '
+        '( SELECT oid FROM pg_class WHERE relname = %s ) '
+        'AND attname = %s',
+        ('payment_order', 'total'))
+    if cr.fetchone()[0] == 0:
+        cr.execute('alter table payment_order add column total numeric')
+    cr.execute(
+        'update payment_order '
+        'set total=totals.total '
+        'from '
+        '(select order_id, sum(amount_currency) total '
+        'from payment_line group by order_id) totals '
+        'where payment_order.id=totals.order_id')
