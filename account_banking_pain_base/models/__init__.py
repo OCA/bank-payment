d616d195648686820fc4fb68d0b8bb6efa2d6906
Alexis de Lattre <alexis.delattre@akretion.com>
2016-05-06 12:37:31 +0200

    bank_bic on res.partner.bank is a related field in v9 and is not displayed in form view, so no need for explainations any more

--- account_banking_pain_base/models/__init__.py
+++ account_banking_pain_base/models/__init__.py
@@ -8,5 +8,4 @@ from . import bank_payment_line
 from . import account_payment_mode
 from . import res_company
 from . import banking_export_pain
-from . import res_partner_bank
 from . import account_payment_method
