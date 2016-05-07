21a0319f8e194046357b3c49a029d743553f8688
Alexis de Lattre <alexis.delattre@akretion.com>
2016-05-07 23:55:46 +0200

    Add module account_banking_mandate_sale
    Add option 'mandate_required' on payment orders
    Autoselect first valid mandate on customer invoice that have a payment mode 'mandate_required' = True
    Add option on select move lines to pay wizard to allow selection of litigation moves (unchecked by default), in order to integrate the feature of the module account_payment_blocking

--- account_payment_partner/models/account_move_line.py
+++ account_payment_partner/models/account_move_line.py
@@ -9,5 +9,4 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     payment_mode_id = fields.Many2one(
-        'account.payment.mode', string='Payment Mode',
-        ondelete='restrict')
+        'account.payment.mode', string='Payment Mode', ondelete='restrict')
