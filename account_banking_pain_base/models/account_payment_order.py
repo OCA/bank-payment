71d1e3883d62476ff98633fa96464229613e75c7
Alexis de Lattre <alexis.delattre@akretion.com>
2016-05-10 23:16:31 +0200

    Move fields mandate_required and export_ascii from payment mode to payment method
    Display chatter on payment orders
    Several small usability improvements

--- account_banking_pain_base/models/account_payment_order.py
+++ account_banking_pain_base/models/account_payment_order.py
@@ -10,14 +10,15 @@ class AccountPaymentOrder(models.Model):
     _inherit = 'account.payment.order'
 
     sepa = fields.Boolean(
-        compute='compute_sepa', readonly=True,
-        string="SEPA Payment")
+        compute='compute_sepa', readonly=True, string="SEPA Payment")
     charge_bearer = fields.Selection([
         ('SLEV', 'Following Service Level'),
         ('SHAR', 'Shared'),
         ('CRED', 'Borne by Creditor'),
         ('DEBT', 'Borne by Debtor')], string='Charge Bearer',
-        default='SLEV',
+        default='SLEV', readonly=True,
+        states={'draft': [('readonly', False)], 'open': [('readonly', False)]},
+        track_visibility='onchange',
         help="Following service level : transaction charges are to be "
         "applied following the rules agreed in the service level "
         "and/or scheme (SEPA Core messages must use this). Shared : "
@@ -28,7 +29,9 @@ class AccountPaymentOrder(models.Model):
         "by debtor : all transaction charges are to be borne by the "
         "debtor.")
     batch_booking = fields.Boolean(
-        string='Batch Booking',
+        string='Batch Booking', readonly=True,
+        states={'draft': [('readonly', False)], 'open': [('readonly', False)]},
+        track_visibility='onchange',
         help="If true, the bank statement will display only one debit "
         "line for all the wire transfers of the SEPA XML file ; if "
         "false, the bank statement will display one debit line per wire "
