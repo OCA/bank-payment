f6143e4e066c22bf044127946248a76ead0960e1
Alexis de Lattre <alexis.delattre@akretion.com>
2016-06-02 23:58:02 +0200

    Add hooks for l10n_ch_sepa (pain.001.001.03.ch.02)
    Move code of account_banking_pain_base/models/banking_export_pain.py to account_banking_pain_base/models/account_payment_order.py
    Allow cancel when payment order is in "File Generated" state
    Add postal address in party block

--- account_banking_pain_base/models/__init__.py
+++ account_banking_pain_base/models/__init__.py
@@ -7,5 +7,4 @@ from . import account_payment_order
 from . import bank_payment_line
 from . import account_payment_mode
 from . import res_company
-from . import banking_export_pain
 from . import account_payment_method
