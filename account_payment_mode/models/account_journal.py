60e0816c803c5d5b0234442f6e74bcd2d718dbf5
Alexis de Lattre <alexis.delattre@akretion.com>
2016-06-16 16:51:03 +0200

    Enable the payment methods by default on bank journals (including existing bank journals via post_install scripts)
    Enable "Generate Accounting Entries On File Upload" by default

---
+++ account_payment_mode/models/account_journal.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class AccountJournal(models.Model):
+    _inherit = 'account.journal'
+
+    def _default_outbound_payment_methods(self):
+        all_out = self.env['account.payment.method'].search([
+            ('payment_type', '=', 'outbound')])
+        return all_out
+
+    def _default_inbound_payment_methods(self):
+        all_in = self.env['account.payment.method'].search([
+            ('payment_type', '=', 'inbound')])
+        return all_in
+
+    outbound_payment_method_ids = fields.Many2many(
+        default=_default_outbound_payment_methods)
+    inbound_payment_method_ids = fields.Many2many(
+        default=_default_inbound_payment_methods)
