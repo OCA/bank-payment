9c816ce584fc7bb36657308f7cb8cbb13cf29160
Holger Brunn <hbrunn@therp.nl>
2016-07-13 08:02:13 +0200

    [FIX] make tests time agnostic

--- account_payment_restrict_mandate/tests/test_account_payment_restrict_mandate.py
+++ account_payment_restrict_mandate/tests/test_account_payment_restrict_mandate.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2016 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import time
 from openerp.tests.common import TransactionCase
 
 
@@ -10,16 +11,16 @@ class TestAccountPaymentRestrictMandate(TransactionCase):
             'partner_bank_id': self.env.ref('base.res_partner_2').bank_ids
             .filtered(lambda x: x.state == 'iban').id,
             'max_amount_per_date': 600,
-            'date_start': '2016-01-01 00:00:00',
-            'date_end': '2016-03-02 00:00:00',
-            'signature_date': '2016-01-01',
+            'date_start': time.strftime('%Y') + '-01-01 00:00:00',
+            'date_end': time.strftime('%Y') + '-03-02 00:00:00',
+            'signature_date': time.strftime('%Y') + '-01-01',
         })
         self.assertEqual(mandate.max_amount, 1800)
         mandate.validate()
         payment_order = self.env['payment.order'].create({
             'reference': 'test',
             'date_prefered': 'fixed',
-            'date_scheduled': '2016-01-01',
+            'date_scheduled': time.strftime('%Y') + '-01-01',
             'mode': self.env.ref('account_payment.payment_mode_1').id,
             'payment_order_type': 'debit',
         })
