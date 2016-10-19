f858826dc59e4527ccd8257036e437dd52745003
Alexis de Lattre <alexis@via.ecp.fr>
2016-10-20 01:41:41 +0200

    Port almost all modules to v10 (#305)
    Port almost all modules to v10
    
    * Update to EPC Rulebook v9.2 that start to apply on 2016-11-20 (bug #300)

--- account_banking_mandate/models/account_banking_mandate.py
+++ account_banking_mandate/models/account_banking_mandate.py
@@ -4,8 +4,8 @@
 # © 2015-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, _
-from openerp.exceptions import UserError, ValidationError
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
 
 
 class AccountBankingMandate(models.Model):
