e53cc125e14715f8fbdadb6ca235f2785b59a329
Alexis de Lattre <alexis@via.ecp.fr>
2016-10-20 01:41:41 +0200

    [MIG] account_banking_sepa_direct_debit: Migration to 10.0

--- account_banking_sepa_direct_debit/models/account_banking_mandate.py
+++ account_banking_sepa_direct_debit/models/account_banking_mandate.py
@@ -3,7 +3,7 @@
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, exceptions, _
+from odoo import models, fields, api, exceptions, _
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import logging
