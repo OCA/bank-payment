07b5b6b1b2cfb0ceebb407c2c1c390cef6ec661f
Alexis de Lattre <alexis@via.ecp.fr>
2016-10-20 01:41:41 +0200

    Port almost all modules to v10 (#305)
    Port almost all modules to v10
    
    * Update to EPC Rulebook v9.2 that start to apply on 2016-11-20 (bug #300)

--- account_banking_pain_base/__manifest__.py
+++ account_banking_pain_base/__manifest__.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 # © 2013-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
-# © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
-# © 2016 Antiun Ingenieria S.L. - Antonio Espinosa
+# © 2014 Tecnativa - Pedro M. Baeza
+# © 2016 Tecnativa - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Banking PAIN Base Module',
     'summary': 'Base module for PAIN file generation',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Akretion, "
               "Noviat, "
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
-              "Antiun Ingeniería S.L., "
+              "Tecnativa, "
               "Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/bank-payment',
     'contributors': ['Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>'],
@@ -26,7 +25,7 @@
         'views/account_payment_order.xml',
         'views/bank_payment_line_view.xml',
         'views/account_payment_mode.xml',
-        'views/res_company_view.xml',
+        'views/account_config_settings.xml',
         'views/account_payment_method.xml',
     ],
     'post_init_hook': 'set_default_initiating_party',
