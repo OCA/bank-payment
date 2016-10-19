550cfc0a2323ec32b2c1da748ff29830c3b6d682
Alexis de Lattre <alexis@via.ecp.fr>
2016-10-20 01:41:41 +0200

    Port almost all modules to v10 (#305)
    Port almost all modules to v10
    
    * Update to EPC Rulebook v9.2 that start to apply on 2016-11-20 (bug #300)

--- account_payment_sale/models/sale_order.py
+++ account_payment_sale/models/sale_order.py
@@ -2,7 +2,7 @@
 # © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class SaleOrder(models.Model):
