7e43ef753c0a1be5ccee750e079727df9a257e15
Alexis de Lattre <alexis@via.ecp.fr>
2016-10-20 01:41:41 +0200

    Port almost all modules to v10 (#305)
    Port almost all modules to v10
    
    * Update to EPC Rulebook v9.2 that start to apply on 2016-11-20 (bug #300)

--- account_payment_order/wizard/account_invoice_payment_line_multi.py
+++ account_payment_order/wizard/account_invoice_payment_line_multi.py
@@ -2,7 +2,7 @@
 # © 2016 Akretion (<http://www.akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class AccountInvoicePaymentLineMulti(models.TransientModel):
