225c7e589b8b5ead87d3924fa551130d74edd54c
Alexis de Lattre <alexis@via.ecp.fr>
2016-10-20 01:41:41 +0200

    [MIG] account_banking_sepa_credit_transfer: Migration to 10.0

--- account_banking_sepa_credit_transfer/models/account_payment_order.py
+++ account_banking_sepa_credit_transfer/models/account_payment_order.py
@@ -3,8 +3,8 @@
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api, _
-from openerp.exceptions import UserError
+from odoo import models, api, _
+from odoo.exceptions import UserError
 from lxml import etree
 
 
