93c73a778bf0deb97f59c4998c683b0397a0ca4b
Alexis de Lattre <alexis@via.ecp.fr>
2016-10-20 01:41:41 +0200

    [MIG] account_banking_sepa_credit_transfer: Migration to 10.0

--- account_banking_sepa_credit_transfer/models/account_payment_method.py
+++ account_banking_sepa_credit_transfer/models/account_payment_method.py
@@ -2,7 +2,7 @@
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class AccountPaymentMethod(models.Model):
