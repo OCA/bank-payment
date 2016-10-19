201baa0a86fc98320900dd215b0d2ab66aae58a2
Alexis de Lattre <alexis@via.ecp.fr>
2016-10-20 01:41:41 +0200

    Port almost all modules to v10 (#305)
    Port almost all modules to v10
    
    * Update to EPC Rulebook v9.2 that start to apply on 2016-11-20 (bug #300)

--- account_banking_pain_base/models/account_payment_line.py
+++ account_banking_pain_base/models/account_payment_line.py
@@ -3,7 +3,7 @@
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class AccountPaymentLine(models.Model):
