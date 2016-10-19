1915a843c215a5f38f79e4b41eae0bd89cff6ce6
Alexis de Lattre <alexis@via.ecp.fr>
2016-10-20 01:41:41 +0200

    Port almost all modules to v10 (#305)
    Port almost all modules to v10
    
    * Update to EPC Rulebook v9.2 that start to apply on 2016-11-20 (bug #300)

--- account_payment_sale/wizard/sale_make_invoice_advance.py
+++ account_payment_sale/wizard/sale_make_invoice_advance.py
@@ -2,7 +2,7 @@
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class SaleAdvancePaymentInv(models.TransientModel):
