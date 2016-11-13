223287bc161ce24c4c79a7bfc9e9e8b583525945
Jose Maria Alzaga <jose.alzaga@aselcis.com>
2016-11-13 16:48:24 +0100

    Remove internal_type=other restriction for transfer account

--- account_payment_order/__openerp__.py
+++ account_payment_order/__openerp__.py
@@ -4,6 +4,7 @@
 # © 2013-2014 ACSONE SA (<http://acsone.eu>).
 # © 2014-2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # © 2016 Akretion (<http://www.akretion.com>).
+# © 2016 Aselcis Consulting (<http://www.aselcis.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -14,6 +15,7 @@
               "Therp BV, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
               "Akretion, "
+              "Aselcis, "
               "Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/bank-payment',
     'category': 'Banking addons',
