e9f2133e76f358b92726127a9abf1c1a68047e57
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-04-02 04:04:55 +0200

    [IMP] account_payment_partner: Several things:
    * Change copyright and author after company merging
    * Fill payment mode in invoices if none is provided
    
      Using same method as in upstream, payment mode is filled on invoice creation if
      no payment method is provided. This way, we don't need to install
      account_payment_sale if we don't want to handle several payment modes at sales
      level. Even more, if we install the module later and we have already existing
      sales orders without payment mode filled, those orders will be invoiced with
      the customer payment mode.
    
    * Signature changed in convert_to_write

--- account_payment_partner/__manifest__.py
+++ account_payment_partner/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # © 2014 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
-# © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2014 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Payment Partner',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'category': 'Banking addons',
     'license': 'AGPL-3',
     'summary': 'Adds payment mode on partners and invoices',
