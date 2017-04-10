f575de9965be44e008be56449f74f34b121aa63b
cubells <vicent@vcubells.net>
2017-04-10 18:35:20 +0200

    [9.0][MIG] account_payment_purchase module

--- account_payment_purchase/models/procurement_order.py
+++ account_payment_purchase/models/procurement_order.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# Copyright 2015 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import api, models
@@ -8,17 +8,14 @@ from openerp import api, models
 class ProcurementOrder(models.Model):
     _inherit = "procurement.order"
 
-    @api.model
-    def create_procurement_purchase_order(self, procurement, po_vals,
-                                          line_vals):
+    @api.multi
+    def _prepare_purchase_order(self, partner):
         """Propagate payment mode on MTO/drop shipping."""
-        if po_vals.get('partner_id'):
-            partner = self.env['res.partner'].browse(po_vals['partner_id'])
-            po_vals['payment_mode_id'] = partner.with_context(
-                force_company=procurement.company_id.id).\
-                supplier_payment_mode.id
-            po_vals['supplier_partner_bank_id'] = (
+        values = super(ProcurementOrder, self)._prepare_purchase_order(partner)
+        if partner:
+            values['payment_mode_id'] = partner.with_context(
+                force_company=self.company_id.id).supplier_payment_mode_id.id
+            values['supplier_partner_bank_id'] = (
                 self.env['purchase.order']._get_default_supplier_partner_bank(
                     partner))
-        return super(ProcurementOrder, self).create_procurement_purchase_order(
-            procurement, po_vals, line_vals)
+        return values
