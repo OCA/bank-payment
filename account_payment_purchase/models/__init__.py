e0ae173d3585cc55032150a5bebb249dcac9c96b
cubells <vicent@vcubells.net>
2017-04-10 18:35:20 +0200

    account_payment_purchase module

---
+++ account_payment_purchase/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Akretion (<http://www.akretion.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import procurement_order
+from . import purchase_order
+from . import account_invoice
