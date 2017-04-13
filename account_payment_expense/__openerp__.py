ed68898e155ebb729046c7818894bcb8d053ac18
Hermann Stöckle <hermann.stoeckle@braintec-group.com>
2017-04-13 15:37:47 +0200

    Create __openerp__.py

---
+++ account_payment_expense/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2017 brain-tec AG (http://www.braintec-group.com)
+#    All Right Reserved
+#
+#    See LICENSE file for full licensing details.
+##############################################################################
+
+{
+    'name': 'Account Payment Expense',
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': "brain-tec AG, "
+              "Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/bank-payment',
+    'category': 'Banking addons',
+    'depends': ['account_payment_order',
+                'hr_expense',
+                ],
+    'installable': True,
+    'auto_install': True,
+}
