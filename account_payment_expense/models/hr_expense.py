f0df7fc356d978c0a58110d2c143c2065c4bb6c1
Hermann Stöckle <hermann.stoeckle@braintec-group.com>
2017-04-13 15:44:31 +0200

    New module account_payment_expense to copy bank details from exployee to account move lines of expense

---
+++ account_payment_expense/models/hr_expense.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2017 brain-tec AG (http://www.braintec-group.com)
+#    All Right Reserved
+#
+#    See LICENSE file for full licensing details.
+##############################################################################
+from openerp import api, models
+
+
+class HrExpense(models.Model):
+
+    _inherit = "hr.expense"
+
+    @api.multi
+    def action_move_create(self):
+        result = super(HrExpense, self).action_move_create()
+
+        for line in self.account_move_id.line_ids:
+            line.partner_bank_id = self.employee_id.bank_account_id
+
+        return result
