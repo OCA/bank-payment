f209e8230d1d468c23061aba489a01de567632ad
Hermann Stöckle <hermann.stoeckle@braintec-group.com>
2017-04-18 10:39:46 +0200

    Update hr_expense.py

--- account_payment_expense/models/hr_expense.py
+++ account_payment_expense/models/hr_expense.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#    Copyright (c) 2017 brain-tec AG (http://www.braintec-group.com)
+# Copyright (c) 2017 brain-tec AG (http://www.braintec-group.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, models
