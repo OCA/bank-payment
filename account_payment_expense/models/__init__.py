f3e5113209298d7cad7dcd89d1b9c118d2dbde62
Hermann Stöckle <hermann.stoeckle@braintec-group.com>
2017-04-18 10:37:45 +0200

    Update __init__.py

--- account_payment_expense/models/__init__.py
+++ account_payment_expense/models/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
+#    Copyright (c) 2017 brain-tec AG (http://www.braintec-group.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import hr_expense
