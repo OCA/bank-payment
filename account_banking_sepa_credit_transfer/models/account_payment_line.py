e38dabf12db5d0ca64781ab30c37e5ecb16236cc
Alexis de Lattre <alexis.delattre@akretion.com>
2017-05-29 18:07:40 +0200

    Add support for ISO20022 "Category Purpose"
    Add local instrument 'INST' for SEPA Instant Credit Transfer

---
+++ account_banking_sepa_credit_transfer/models/account_payment_line.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2017 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class AccountPaymentLine(models.Model):
+    _inherit = 'account.payment.line'
+
+    # local_instrument 'INST' used for instant credit transfers
+    # which will begin on November 21st 2017, cf
+    # https://www.europeanpaymentscouncil.eu/document-library/
+    # rulebooks/2017-sepa-instant-credit-transfer-rulebook
+    local_instrument = fields.Selection(
+        selection_add=[('INST', 'Instant Transfer')])
