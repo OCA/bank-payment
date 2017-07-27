7addb7157ab31416d11d51fd680052e80352a092
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-07-27 10:23:35 +0200

    [IMP] account_payment_order: Restriction + tests
    * Restrict the deletion of bank payment line
    * Add test_cancel_payment_order

--- account_payment_order/__manifest__.py
+++ account_payment_order/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'Account Payment Order',
-    'version': '10.0.1.2.0',
+    'version': '10.0.1.3.0',
     'license': 'AGPL-3',
     'author': "ACSONE SA/NV, "
               "Therp BV, "
