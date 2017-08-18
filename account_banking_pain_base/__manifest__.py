d99851e445a64b0716e860f3c029875a92b8d8cd
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-08-18 19:59:31 +0200

    [IMP] account_banking*: Show identifiers at payment mode level
    Now the visibility is controlled through a security group.

--- account_banking_pain_base/__manifest__.py
+++ account_banking_pain_base/__manifest__.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
-# © 2013-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
-# © 2014 Tecnativa - Pedro M. Baeza
-# © 2016 Tecnativa - Antonio Espinosa
+# Copyright 2013-2016 Akretion - Alexis de Lattre
+# Copyright 2014-2017 Tecnativa - Pedro M. Baeza
+# Copyright 2016 Tecnativa - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Banking PAIN Base Module',
     'summary': 'Base module for PAIN file generation',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'license': 'AGPL-3',
     'author': "Akretion, "
               "Noviat, "
               "Tecnativa, "
               "Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/bank-payment',
-    'contributors': ['Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>'],
     'category': 'Hidden',
     'depends': ['account_payment_order'],
     'external_dependencies': {
         'python': ['unidecode', 'lxml'],
     },
     'data': [
+        'security/security.xml',
         'views/account_payment_line.xml',
         'views/account_payment_order.xml',
         'views/bank_payment_line_view.xml',
