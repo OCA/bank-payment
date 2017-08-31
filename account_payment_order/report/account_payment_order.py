0ff10f6ccc995402b272b612561f7ce5427f2192
Benjamin Willig <benjamin.willig@acsone.eu>
2017-08-31 08:44:21 +0200

    [FIX] Flake8 and pylint

--- account_payment_order/report/account_payment_order.py
+++ account_payment_order/report/account_payment_order.py
@@ -2,7 +2,7 @@
 # © 2017 Acsone SA/NV (<https://www.acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models, _
+from odoo import api, models
 from odoo.tools.misc import formatLang
 
 
