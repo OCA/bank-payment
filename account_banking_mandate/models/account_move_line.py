8594bb4415791177d7fa9a65cd1c94c8f92876cb
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-09-05 15:22:08 +0200

    [UPD] if a mandate is linked to the bank account used, put it automatically on the payment line'

--- account_banking_mandate/models/account_move_line.py
+++ account_banking_mandate/models/account_move_line.py
@@ -19,4 +19,11 @@ class AccountMoveLine(models.Model):
         if payment_order.payment_type == 'inbound' and self.mandate_id:
             vals['mandate_id'] = self.mandate_id.id
             vals['partner_bank_id'] = self.mandate_id.partner_bank_id.id
+        partner_bank_id = vals.get('partner_bank_id', False)
+        if partner_bank_id and 'mandate_id' not in vals:
+            mandate = self.env['account.banking.mandate'].search(
+                [('partner_bank_id', '=', partner_bank_id),
+                 ('state', '=', 'valid')], limit=1)
+            if mandate:
+                vals['mandate_id'] = mandate.id
         return vals
