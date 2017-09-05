6dc9d7cfc524bfbdaf9b8c7294c1d3222ca67bee
David Vidal <chienandalu@gmail.com>
2017-09-05 09:39:44 +0200

    [MIG] portal_payment_mode: Migration to 9.0

--- portal_payment_mode/README.rst
+++ portal_payment_mode/README.rst
@@ -15,19 +15,16 @@ Usage
 
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
-   :target: https://runbot.odoo-community.org/runbot/173/8.0
-
-For further information, please visit:
-
-* https://www.odoo.com/forum/help-1
+   :target: https://runbot.odoo-community.org/runbot/173/9.0
 
 Bug Tracker
 ===========
 
-Bugs are tracked on `GitHub Issues <https://github.com/OCA/bank-payment/issues>`_.
-In case of trouble, please check there if your issue has already been reported.
-If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
-`here <https://github.com/OCA/bank-payment/issues/new?body=module:%20portal_payment_mode%0Aversion:%208.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/bank-payment/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smash it by providing detailed and welcomed feedback.
 
 Credits
 =======
@@ -35,9 +32,10 @@ Credits
 Contributors
 ------------
 
-* Rafael Blasco <rafabn@antiun.com>
-* Carlos Dauden <carlos@incaser.es>
-* Sergio Teruel <sergio@incaser.es>
+* Rafael Blasco <rafael.blasco@tecnativa.com>
+* Carlos Dauden <carlos.dauden@tecnativa.com>
+* Sergio Teruel <sergio.teruel@tecnativa.com>
+* David Vidal <david.vidal@tecnativa.com>
 
 Maintainer
 ----------
@@ -52,4 +50,4 @@ OCA, or the Odoo Community Association, is a nonprofit organization whose
 mission is to support the collaborative development of Odoo features and
 promote its widespread use.
 
-To contribute to this module, please visit http://odoo-community.org.
+To contribute to this module, please visit https://odoo-community.org.
--- portal_payment_mode/tests/test_portal_payment_mode.py
+++ portal_payment_mode/tests/test_portal_payment_mode.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Antiun Ingeniería S.L. - Sergio Teruel
-# (c) 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# Copyright 2015 Tecnativa - Sergio Teruel
+# Copyright 2015 Tecnativa - Carlos Dauden
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp.tests.common import TransactionCase
@@ -19,26 +19,38 @@ class TestPortalPaymentMode(TransactionCase):
             'name': 'Test journal',
             'code': 'TEST',
             'type': 'general'})
-        self.payment_mode = self.env['payment.mode'].create({
+        self.payment_mode = self.env['account.payment.mode'].create({
             'name': 'Test Payment Mode',
             'journal': self.journal.id,
             'bank_id': self.bank.id,
-            'type': self.env.ref(
-                'account_banking_payment_export.manual_bank_tranfer').id,
+            'bank_account_link': 'variable',
+            'payment_method_id': self.env.ref(
+                'account.account_payment_method_manual_in').id,
             'sale_ok': True,
+        })
+        self.account = self.env['account.account'].create({
+            'name': 'Test account',
+            'code': 'TESTACC',
+            'user_type_id': self.env.ref(
+                'account.data_account_type_receivable').id,
+            'reconcile': True,
+        })
+        self.journal = self.env['account.journal'].create({
+            'name': 'TEST JOURNAL',
+            'code': 'TSTJRNL',
+            'type': 'sale',
         })
         vals_invoice = {
             'partner_id': self.partner.id,
-            'account_id': self.env.ref('account.a_sale').id,
-            'journal_id': self.env.ref('account.sales_journal').id,
+            'account_id': self.account.id,
+            'journal_id': self.journal.id,
             'payment_mode_id': self.payment_mode.id,
             'invoice_line': [(0, 0, {
                 'name': 'test',
-                'account_id': self.env.ref('account.a_sale').id,
+                'account_id': self.account.id,
                 'price_unit': 100.00,
                 'quantity': 1
             })],
-
         }
         self.invoice = self.env['account.invoice'].create(vals_invoice)
         self.invoice.invoice_validate()
