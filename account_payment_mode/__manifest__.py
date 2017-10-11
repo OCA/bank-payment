e95c4fd97f79a6e00142cdc90feb5a623a441f51
Enric Tobella <etobella@creublanca.es>
2017-10-11 13:35:05 +0200

    [MIG] account_payment_mode

--- account_payment_mode/__manifest__.py
+++ account_payment_mode/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (<http://www.akretion.com>).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2016 Akretion (<https://www.akretion.com>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Payment Mode',
-    'version': '10.0.1.0.1',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/bank-payment',
