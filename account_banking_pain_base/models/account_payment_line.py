5ce0d1bfefc0cee343788f5a8b3658934021ca8d
etobella <etobella@creublanca.es>
2017-10-16 17:20:40 +0200

    [MIG] account_banking_pain_base

--- account_banking_pain_base/models/account_payment_line.py
+++ account_banking_pain_base/models/account_payment_line.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # © 2013-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields
 
