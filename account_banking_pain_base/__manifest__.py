2d010e74ff6a3745ed71bc0ce9f18747504c53e9
etobella <etobella@creublanca.es>
2017-10-16 17:20:40 +0200

    [MIG] account_banking_pain_base

--- account_banking_pain_base/__manifest__.py
+++ account_banking_pain_base/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013-2016 Akretion - Alexis de Lattre
 # Copyright 2014-2017 Tecnativa - Pedro M. Baeza
 # Copyright 2016 Tecnativa - Antonio Espinosa
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Banking PAIN Base Module',
     'summary': 'Base module for PAIN file generation',
-    'version': '10.0.1.1.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Akretion, "
               "Noviat, "
@@ -25,7 +24,7 @@
         'views/account_payment_order.xml',
         'views/bank_payment_line_view.xml',
         'views/account_payment_mode.xml',
-        'views/account_config_settings.xml',
+        'views/res_config_settings.xml',
         'views/account_payment_method.xml',
     ],
     'post_init_hook': 'set_default_initiating_party',
--- account_banking_pain_base/post_install.py
+++ account_banking_pain_base/post_install.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # © 2015-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 
 from odoo import api, SUPERUSER_ID
@@ -9,6 +8,6 @@ from odoo import api, SUPERUSER_ID
 def set_default_initiating_party(cr, registry):
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        companies = env['res.company'].search([])
-        companies._default_initiating_party()
+        for company in env['res.company'].search([]):
+            company._default_initiating_party()
     return
