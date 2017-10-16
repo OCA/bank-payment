39db146c00c0a046395161bc84a18ff48c4abdac
etobella <etobella@creublanca.es>
2017-10-16 17:20:40 +0200

    [MIG] account_banking_pain_base

--- account_banking_pain_base/__init__.py
+++ account_banking_pain_base/__init__.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # © 2013 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import models
 from .post_install import set_default_initiating_party
