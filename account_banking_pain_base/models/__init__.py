7886b190f4f8fcfeb6a5b5a10a01e37f712daa56
etobella <etobella@creublanca.es>
2017-10-16 17:20:40 +0200

    [MIG] account_banking_pain_base

--- account_banking_pain_base/models/__init__.py
+++ account_banking_pain_base/models/__init__.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
-
 from . import account_payment_line
 from . import account_payment_order
 from . import bank_payment_line
 from . import account_payment_mode
 from . import res_company
-from . import account_config_settings
+from . import res_config_settings
 from . import account_payment_method
