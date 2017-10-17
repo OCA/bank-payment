73d7f2d984c72132af9b11e2043a06a01c87ea21
etobella <etobella@creublanca.es>
2017-10-17 16:05:26 +0200

    [MIG] account_payment_partner

--- account_payment_partner/models/account_move_line.py
+++ account_payment_partner/models/account_move_line.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (http://www.akretion.com/)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2016 Akretion (https://www.akretion.com/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields
 
@@ -9,4 +9,8 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     payment_mode_id = fields.Many2one(
-        'account.payment.mode', string='Payment Mode', ondelete='restrict')
+        'account.payment.mode',
+        string='Payment Mode',
+        domain="[('company_id', '=', company_id)]",
+        ondelete='restrict'
+    )
