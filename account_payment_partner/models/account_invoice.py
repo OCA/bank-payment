6abf14a7650a1dcb4ae02195b10c6d3c0b751a3a
etobella <etobella@creublanca.es>
2017-10-17 16:05:26 +0200

    [MIG] account_payment_partner

--- account_payment_partner/models/account_invoice.py
+++ account_payment_partner/models/account_invoice.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2014-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
 # © 2014 Serv. Tecnol. Avanzados - Pedro M. Baeza
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api
 
@@ -22,26 +22,43 @@ class AccountInvoice(models.Model):
         res = super(AccountInvoice, self)._onchange_partner_id()
         if self.partner_id:
             if self.type == 'in_invoice':
-                pay_mode = self.partner_id.supplier_payment_mode_id
+                pay_mode = self.with_context(
+                    company_id=self.company_id.id
+                ).partner_id.supplier_payment_mode_id
                 self.payment_mode_id = pay_mode
                 if (
-                        pay_mode and
-                        pay_mode.payment_type == 'outbound' and
-                        pay_mode.payment_method_id.bank_account_required and
-                        self.commercial_partner_id.bank_ids):
-                    self.partner_bank_id =\
+                    pay_mode and
+                    pay_mode.payment_type == 'outbound' and
+                    pay_mode.payment_method_id.bank_account_required and
+                    self.commercial_partner_id.bank_ids
+                ):
+                    self.partner_bank_id = \
                         self.commercial_partner_id.bank_ids[0]
             elif self.type == 'out_invoice':
-                pay_mode = self.partner_id.customer_payment_mode_id
+                pay_mode = self.with_context(
+                    company_id=self.company_id.id
+                ).partner_id.customer_payment_mode_id
                 self.payment_mode_id = pay_mode
                 if pay_mode and pay_mode.bank_account_link == 'fixed':
-                    self.partner_bank_id = pay_mode.fixed_journal_id.\
+                    self.partner_bank_id = pay_mode.fixed_journal_id. \
                         bank_account_id
         else:
             self.payment_mode_id = False
             if self.type == 'in_invoice':
                 self.partner_bank_id = False
         return res
+
+    @api.onchange('payment_mode_id')
+    def _onchange_payment_mode_id(self):
+        pay_mode = self.payment_mode_id
+        if (
+            pay_mode and
+            pay_mode.payment_type == 'outbound' and not
+            pay_mode.payment_method_id.bank_account_required
+        ):
+            self.partner_bank_id = False
+        elif not self.payment_mode_id:
+            self.partner_bank_id = False
 
     @api.model
     def create(self, vals):
@@ -60,17 +77,6 @@ class AccountInvoice(models.Model):
                             invoice[field], invoice,
                         )
         return super(AccountInvoice, self).create(vals)
-
-    @api.onchange('payment_mode_id')
-    def payment_mode_id_change(self):
-        if (
-                self.payment_mode_id and
-                self.payment_mode_id.payment_type == 'outbound' and
-                not self.payment_mode_id.payment_method_id.
-                bank_account_required):
-            self.partner_bank_id = False
-        elif not self.payment_mode_id:
-            self.partner_bank_id = False
 
     @api.model
     def line_get_convert(self, line, part):
