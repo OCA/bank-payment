bc19537abf536b291a30c2cc473905208eacd895
etobella <etobella@creublanca.es>
2017-10-17 16:02:57 +0200

    [MIG] account_payment_order

--- account_payment_order/wizard/account_payment_line_create.py
+++ account_payment_order/wizard/account_payment_line_create.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # © 2009 EduSense BV (<http://www.edusense.nl>)
-# © 2011-2013 Therp BV (<http://therp.nl>)
-# © 2014-2015 ACSONE SA/NV (<http://acsone.eu>)
-# © 2015-2016 Akretion (<http://www.akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2011-2013 Therp BV (<https://therp.nl>)
+# © 2014-2015 ACSONE SA/NV (<https://acsone.eu>)
+# © 2015-2016 Akretion (<https://www.akretion.com>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api, _
 
