e69d39cea1a36adae2ad45dfc70c8f0c33c40073
etobella <etobella@creublanca.es>
2017-10-17 16:02:57 +0200

    [MIG] account_payment_order

--- account_payment_order/models/res_bank.py
+++ account_payment_order/models/res_bank.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2015-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, api, _
 from odoo.exceptions import ValidationError
