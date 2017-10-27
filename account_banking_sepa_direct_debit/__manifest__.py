1dbf254e5c54af2b0671d90482f009fce08e548a
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-10-27 09:32:52 +0200

    [FIX] account_payment_order: Sequences restarted when update module

--- account_banking_sepa_direct_debit/__manifest__.py
+++ account_banking_sepa_direct_debit/__manifest__.py
@@ -2,12 +2,13 @@
 # Copyright 2013-2016 Akretion (www.akretion.com)
 # Copyright 2014-2017 Tecnativa - Pedro M. Baeza
 # Copyright 2016 Tecnativa - Antonio Espinosa
+# Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Banking SEPA Direct Debit',
     'summary': 'Create SEPA files for Direct Debit',
-    'version': '10.0.1.1.0',
+    'version': '10.0.1.1.1',
     'license': 'AGPL-3',
     'author': "Akretion, "
               "Tecnativa, "
