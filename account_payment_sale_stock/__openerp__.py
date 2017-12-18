e273913e0d7c029211d69ba6f5cc9ccadb5e3a10
Almudena <almudena.delapuente@diagram.es>
2017-12-18 09:00:13 +0100

    [FIX] fix travis error of encoding

--- account_payment_sale_stock/__openerp__.py
+++ account_payment_sale_stock/__openerp__.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Account Payment Sale Stock module for OpenERP
