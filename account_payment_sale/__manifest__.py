e181310bfd8e28ec52fac8948e87eab5ef5d03a7
Hamza Talibi <htalibi.alami@gmail.com>
2018-01-01 15:10:30 +0000

    Migration to version 11.0

---
+++ account_payment_sale/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2014-2016 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+
+{
+    'name': 'Account Payment Sale',
+    'version': '11.0.1.0.0',
+    'category': 'Banking addons',
+    'license': 'AGPL-3',
+    'summary': "Adds payment mode on sale orders",
+    'author': "Akretion, "
+              "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/bank-payment',
+    'depends': [
+        'sale',
+        'sale_management',
+        'account_payment_partner',
+    ],
+    'conflicts': ['sale_payment'],
+    'data': [
+        'views/sale_order_view.xml',
+        'views/sale_report_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
