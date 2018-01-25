2a8c6c5a694b34d84fca4fbea5d653b2dcee814c
mpanarin <m.panarin@mobilunity.com>
2018-01-25 19:04:46 +0200

    [IMP] account_payment_sale: Add tests to increase coverage

--- account_payment_sale/__manifest__.py
+++ account_payment_sale/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Account Payment Sale',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'category': 'Banking addons',
     'license': 'AGPL-3',
     'summary': "Adds payment mode on sale orders",
