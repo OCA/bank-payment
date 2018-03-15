250e7a2f0ce754a5eb612d704acb1d53aee7ae22
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-03-15 14:02:30 +0100

    [FIX+IMP] account_payment_partner: Fix invoice bank account assignation + show it on invoice report
    * IMP: Add options to show partner bank account on invoice report
    * FIX: Don't auto-assign partner bank account due to problems when you have a direct debit payment
      method with bank_account_link = 'fixed'. Bank account is shown through new mechanism.

--- account_payment_partner/__manifest__.py
+++ account_payment_partner/__manifest__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
-# © 2014 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
-# © 2014 Tecnativa - Pedro M. Baeza
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# Copyright 2014 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
+# Copyright 2014 Tecnativa - Pedro M. Baeza
+# Copyright 2018 Tecnativa - Carlos Dauden
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Payment Partner',
-    'version': '11.0.1.2.1',
+    'version': '11.0.1.3.0',
     'category': 'Banking addons',
     'license': 'AGPL-3',
     'summary': 'Adds payment mode on partners and invoices',
@@ -18,6 +18,7 @@
         'views/res_partner_view.xml',
         'views/account_invoice_view.xml',
         'views/account_move_line.xml',
+        'views/account_payment_mode.xml',
         'views/report_invoice.xml',
     ],
     'demo': ['demo/partner_demo.xml'],
---
+++ account_payment_partner/readme/USAGE.rst
@@ -0,0 +1,12 @@
+[ This file must be present and contains the usage instructions
+  for end-users. As all other rst files included in the README,
+  it MUST NOT contain reStructuredText sections
+  only body text (paragraphs, lists, tables, etc). Should you need
+  a more elaborate structure to explain the addon, please create a
+  Sphinx documentation (which may include this file as a "quick start"
+  section). ]
+
+You are able to add a payment mode directly on a partner.
+This payment mode is automatically associated to the invoice related to the partner. This default value could be change in a draft invoice.
+When you create an payment order, only invoices related to chosen payment mode are displayed.
+Invoices without any payment mode are displayed to.
