65564a5fc4a7781f57504bbfc8521947fb68c65c
Cristian Salamea <ovnicraft@gmail.com>
2018-03-31 11:10:41 -0500

    Squashed commit of the following:
    commit abdbcaf7c9aa0b696344531a140326ae354acbc2
    Author: Cristian Salamea <ovnicraft@gmail.com>
    Date:   Sat Mar 31 11:01:11 2018 -0500
    
        [ADD] 10.0 Account payment order date module
    
        This module allow to set accounting date from scheduled date in order

---
+++ account_payment_date/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 @author Cristian Salamea
+
+{
+    'name': 'Accounting Payments Dates',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting',
+    'author': "PriseHub, Odoo Community Association (OCA)",
+    'website': 'http://www.prisehub.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_payment_order'
+    ],
+    'data': [
+        'views/payment_views.xml',
+    ],
+    'installable': True,
+ }
