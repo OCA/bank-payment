89e6dbf04930ceeb2f007963308dde31e78b7a90
Cristian Salamea <ovnicraft@gmail.com>
2018-03-31 12:05:36 -0500

    typo

--- account_payment_date/__manifest__.py
+++ account_payment_date/__manifest__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # © <2018> <Cristian Salamea>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
     'name': 'Accounting Payments Dates',
     'version': '10.0.1.0.0',
