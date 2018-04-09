9262d8a831c23ef87f8985bff1165e51aae0f4ea
Alexis de Lattre <alexis@via.ecp.fr>
2018-04-09 11:54:59 +0200

    [10.0][FIX] Split account move generated for SEPA direct debits per sequence_type (#464)
    The sequence_type of mandates switches from First to Recurring:
    - BEFORE: when state switched from 'open' to 'generated'
    - NOW: when state switches from 'generated' to 'uploaded'

--- account_banking_sepa_direct_debit/__manifest__.py
+++ account_banking_sepa_direct_debit/__manifest__.py
@@ -7,7 +7,7 @@
 {
     'name': 'Account Banking SEPA Direct Debit',
     'summary': 'Create SEPA files for Direct Debit',
-    'version': '11.0.1.0.1',
+    'version': '11.0.1.0.2',
     'license': 'AGPL-3',
     'author': "Akretion, "
               "Tecnativa, "
