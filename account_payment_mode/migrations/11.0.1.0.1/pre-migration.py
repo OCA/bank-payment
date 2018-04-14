60685acc2133202e9df9bbc7783df5a15f986e87
Alexis de Lattre <alexis@via.ecp.fr>
2018-04-14 13:03:16 +0200

    [FIX+IMP] account_payment_mode: Several small usability improvements + fix
    - FIX: Don't store acc_type of res.partner.bank (#467)
    - when manually encoding a payment line, get company currency as default currency
    - Search on payment order numbers
    - Direct search on bank journal name in payment orders
    - remove widget="selection" on account.payement.mode : this object is rarely modified, so widget='selection' is not a time saver, but we miss the HTML link, which can be pretty convenient to check the configuration.

---
+++ account_payment_mode/migrations/11.0.1.0.1/pre-migration.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+def migrate(cr, version):
+    if not version:
+        return
+
+    cr.execute('ALTER TABLE res_partner_bank DROP COLUMN acc_type')
