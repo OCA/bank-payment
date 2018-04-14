2667affc14ca29df8028838871cffa1f0dbd3ced
Alexis de Lattre <alexis.delattre@akretion.com>
2018-04-14 12:27:48 +0200

    PEP8 fix

--- account_payment_mode/migrations/10.0.1.0.2/pre-migration.py
+++ account_payment_mode/migrations/10.0.1.0.2/pre-migration.py
@@ -2,6 +2,7 @@
 # Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+
 def migrate(cr, version):
     if not version:
         return
