c4319608de5a695d70af34623c0233190350b07f
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-05-03 19:11:58 +0200

    [FIX] account_payment_order: Add bank payment line multi-company rule

--- account_payment_order/__manifest__.py
+++ account_payment_order/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'Account Payment Order',
-    'version': '10.0.1.3.4',
+    'version': '10.0.1.4.0',
     'license': 'AGPL-3',
     'author': "ACSONE SA/NV, "
               "Therp BV, "
