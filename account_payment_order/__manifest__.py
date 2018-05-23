d1617d9339b9345da0692c8e7ba5d3aada15e205
Enric Tobella <etobella@creublanca.es>
2018-05-23 13:08:00 +0200

    [FIX] report should show the invoice number, not the move name (#477)
    [IMP] account_payment_order: report show the invoice number or the move name

--- account_payment_order/__manifest__.py
+++ account_payment_order/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Account Payment Order',
-    'version': '11.0.1.2.0',
+    'version': '11.0.1.2.1',
     'license': 'AGPL-3',
     'author': "ACSONE SA/NV, "
               "Therp BV, "
