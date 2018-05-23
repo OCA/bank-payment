d8dac8fc42b2238f4d0e377fec421d9299774acb
lembregtse <eric@lembregts.eu>
2018-05-23 10:10:29 +0200

    [FIX] account_banking_mandate: Add signature_date for _check_valid_state constraint (#445)

--- account_banking_mandate/__manifest__.py
+++ account_banking_mandate/__manifest__.py
@@ -7,7 +7,7 @@
 {
     'name': 'Account Banking Mandate',
     'summary': 'Banking mandates',
-    'version': '11.0.1.2.1',
+    'version': '11.0.1.2.2',
     'license': 'AGPL-3',
     'author': "Compassion CH, "
               "Tecnativa, "
