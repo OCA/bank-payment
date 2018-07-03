51ead83583837c131331708f578e0d44ce9560d4
andreparames <github@andreparames.com>
2018-07-03 11:38:54 +0100

    [ADD] sepa: support Initiating Party Scheme

--- account_banking_pain_base/models/account_payment_order.py
+++ account_banking_pain_base/models/account_payment_order.py
@@ -284,6 +284,9 @@ class AccountPaymentOrder(models.Model):
         initiating_party_issuer = (
             self.payment_mode_id.initiating_party_issuer or
             self.payment_mode_id.company_id.initiating_party_issuer)
+        initiating_party_scheme = (
+            self.payment_mode_id.initiating_party_scheme or
+            self.payment_mode_id.company_id.initiating_party_scheme)
         # in pain.008.001.02.ch.01.xsd files they use
         # initiating_party_identifier but not initiating_party_issuer
         if initiating_party_identifier:
@@ -292,6 +295,12 @@ class AccountPaymentOrder(models.Model):
             iniparty_org_other = etree.SubElement(iniparty_org_id, 'Othr')
             iniparty_org_other_id = etree.SubElement(iniparty_org_other, 'Id')
             iniparty_org_other_id.text = initiating_party_identifier
+            if initiating_party_scheme:
+                iniparty_org_other_scheme = etree.SubElement(
+                    iniparty_org_other, 'SchmeNm')
+                iniparty_org_other_scheme_name = etree.SubElement(
+                    iniparty_org_other_scheme, 'Prtry')
+                iniparty_org_other_scheme_name.text = initiating_party_scheme
             if initiating_party_issuer:
                 iniparty_org_other_issuer = etree.SubElement(
                     iniparty_org_other, 'Issr')
