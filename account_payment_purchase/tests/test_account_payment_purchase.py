386c7dc196b6fd77f1937d91dcf425b5dbe2084c
Abranes <abrahamanes@gmail.com>
2018-07-05 16:16:58 +0200

    account_payment_purchase: Migration to 10.0

--- account_payment_purchase/tests/test_account_payment_purchase.py
+++ account_payment_purchase/tests/test_account_payment_purchase.py
@@ -3,8 +3,8 @@
 # Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import fields
-from openerp.tests import common
+from odoo import fields
+from odoo.tests import common
 
 
 class TestAccountPaymentPurchase(common.SavepointCase):
