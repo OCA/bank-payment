5db66e7a1317b195dd791754791a007e96fd50c5
Abranes <abrahamanes@gmail.com>
2018-07-05 16:16:58 +0200

    [MIG] account_payment_purchase: Migration to 10.0

--- account_payment_purchase/models/purchase_order.py
+++ account_payment_purchase/models/purchase_order.py
@@ -3,7 +3,7 @@
 # Copyright 2017 Tecnativa - Vicent Cubells.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
