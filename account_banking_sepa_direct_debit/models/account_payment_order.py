7949d1d1a0bde9ea5e581a340dfecc5272727262
andreparames <github@andreparames.com>
2018-07-18 16:06:38 +0100

    [ADD] sepa: add Instruction Identification field
    InstrId is optional, so there should be no harm in
    adding it always, with the same value as EndToEndId.

--- account_banking_sepa_direct_debit/models/account_payment_order.py
+++ account_banking_sepa_direct_debit/models/account_payment_order.py
@@ -156,12 +156,11 @@ class AccountPaymentOrder(models.Model):
                     payment_info, 'DrctDbtTxInf')
                 payment_identification = etree.SubElement(
                     dd_transaction_info, 'PmtId')
-                if pain_flavor == 'pain.008.001.02.ch.01':
-                    instruction_identification = etree.SubElement(
-                        payment_identification, 'InstrId')
-                    instruction_identification.text = self._prepare_field(
-                        'Intruction Identification', 'line.name',
-                        {'line': line}, 35, gen_args=gen_args)
+                instruction_identification = etree.SubElement(
+                    payment_identification, 'InstrId')
+                instruction_identification.text = self._prepare_field(
+                    'Instruction Identification', 'line.name',
+                    {'line': line}, 35, gen_args=gen_args)
                 end2end_identification = etree.SubElement(
                     payment_identification, 'EndToEndId')
                 end2end_identification.text = self._prepare_field(
