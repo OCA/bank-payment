f27f18072310d85da328d8489fd1edc8d85e4488
Enric Tobella <etobella@creublanca.es>
2018-08-29 17:07:53 +0200

    [FIX] default_partner_id added on partner_bank if it is created.

--- account_payment_order/__manifest__.py
+++ account_payment_order/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Account Payment Order',
-    'version': '11.0.1.2.1',
+    'version': '11.0.1.2.2',
     'license': 'AGPL-3',
     'author': "ACSONE SA/NV, "
               "Therp BV, "
