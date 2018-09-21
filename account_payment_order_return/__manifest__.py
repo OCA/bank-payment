d6be1f3a4c42c0bcf75de5cdad49b6163e487cd2
cubells <vicent@vcubells.net>
2018-09-21 13:10:07 +0200

    [MIG] account_payment_order_return: Migration to 11.0

--- account_payment_order_return/__manifest__.py
+++ account_payment_order_return/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - Luis M. Ontalba
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Account Payment Order Return',
-    'version': '9.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Banking addons',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
@@ -14,7 +13,7 @@
         'account_payment_order',
     ],
     'data': [
-        'wizard/account_payment_line_create_view.xml',
+        'wizards/account_payment_line_create_view.xml',
     ],
     'license': 'AGPL-3',
     'installable': True,
