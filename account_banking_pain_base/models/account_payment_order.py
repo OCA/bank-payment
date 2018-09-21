de1cb7f8459507b0bdc2b1c80b7dcd7bf53c7768
BT-dherreros <daniel.herreros@braintec-group.com>
2018-09-21 10:48:06 +0200

    [FIX] Following latest PR comments: Changed compute_display_name and created a conditional set on name for partner.bank

--- account_banking_pain_base/models/account_payment_order.py
+++ account_banking_pain_base/models/account_payment_order.py
@@ -373,7 +373,9 @@ class AccountPaymentOrder(models.Model):
             party_type_label = 'Creditor'
         elif party_type == 'Dbtr':
             party_type_label = 'Debtor'
-        name = 'partner_bank.account_holder' or 'partner_bank.partner_id.name'
+        name = ('partner_bank.account_holder'
+                if partner_bank.account_holder
+                else 'partner_bank.partner_id.name')
         eval_ctx = {'partner_bank': partner_bank}
         party_name = self._prepare_field(
             '%s Name' % party_type_label, name, eval_ctx,
