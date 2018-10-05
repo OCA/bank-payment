516755e957f1534ecaa4db02776ba818542f7031
cubells <vicent@vcubells.net>
2018-10-05 10:09:42 +0200

    account_payment_purchase: Migration to 11.0

--- account_payment_purchase/models/__init__.py
+++ account_payment_purchase/models/__init__.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Akretion (<http://www.akretion.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import procurement_order
-from . import purchase_order
 from . import account_invoice
+from . import purchase_order
+from . import procurement_rule
--- account_payment_purchase/tests/__init__.py
+++ account_payment_purchase/tests/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013-2015 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
