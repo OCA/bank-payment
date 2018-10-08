2be1685259e6f2ab4e26bcc12963cc4b54bbdf61
BT-dherreros <daniel.herreros@braintec-group.com>
2018-10-08 09:26:17 +0200

    [FIX] Travis fixes

--- account_banking_pain_base/models/res_partner_bank.py
+++ account_banking_pain_base/models/res_partner_bank.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Brain-Tec Group
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResPartnerBank(models.Model):
