1eac10f41909d002122fcc80116f9de0d86c49f6
cubells <vicent@vcubells.net>
2018-12-03 12:45:56 +0100

    [IMP] account_payment_partner: Add several tests

--- account_payment_partner/models/account_move_line.py
+++ account_payment_partner/models/account_move_line.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-# © 2016 Akretion (https://www.akretion.com/)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# Copyright 2016 Akretion (http://www.akretion.com/)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountMoveLine(models.Model):
