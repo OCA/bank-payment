d396e409edf8e04642cf58b9d3bdf4dbc7b90c65
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2018-12-21 12:14:42 +0100

    [MIG] Account Banking Mandate Sale

--- account_banking_mandate_sale/__manifest__.py
+++ account_banking_mandate_sale/__manifest__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Banking Mandate Sale',
-    'version': '10.0.1.0.1',
+    'version': '12.0.1.0.1',
     'category': 'Banking addons',
     'license': 'AGPL-3',
     'summary': "Adds mandates on sale orders",
-    'author': "Akretion, "
-              "Odoo Community Association (OCA)",
+    'author': "Odoo Community Association (OCA), "
+              "Akretion",
     'website': 'https://github.com/OCA/bank-payment',
     'depends': [
         'account_payment_sale',
