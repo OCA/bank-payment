e5c1a548daa656350ef2297e3520bc2d5e8de67c
Nikul-Chaudhary <nikulchaudhary2112@gmail.com>
2019-01-19 16:55:49 +0530

    [MIG] account_payment_purchase v11 to v12

--- account_payment_purchase/models/__init__.py
+++ account_payment_purchase/models/__init__.py
@@ -1,6 +1,5 @@
-# Copyright 2016 Akretion (<http://www.akretion.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import account_invoice
 from . import purchase_order
-from . import procurement_rule
+from . import stock_rule
