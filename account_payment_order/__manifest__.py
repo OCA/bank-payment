c9be71a1ee63d6d092f54b8888ec1708c4b46e91
Jon Zaballa <jonzaballa@digital5.es>
2019-03-07 18:23:51 +0100

    [FIX] 11.0 Account Payment Order: domain bug on create line wizard
    When `populate` method is called and the wizard action is returned
    again to refresh the view, the dynamic domain of `move_line_ids` gets
    lost. Updating move_line_ids field and returning `ir.actions.do_nothing`
    solves the issue.

--- account_payment_order/__manifest__.py
+++ account_payment_order/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Account Payment Order',
-    'version': '11.0.1.3.0',
+    'version': '11.0.1.3.1',
     'license': 'AGPL-3',
     'author': "ACSONE SA/NV, "
               "Therp BV, "
--- account_payment_order/models/account_payment_order.py
+++ account_payment_order/models/account_payment_order.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import base64
-from odoo import models, fields, api, _
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 
 
