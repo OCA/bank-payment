f53147a4b285383cd4c46bdf86d2b119c784600d
Dave Burkholder <dave@compassfoundation.io>
2019-03-14 11:52:59 -0400

    [11.0][MIG] account payment sale

--- account_banking_mandate_sale/__manifest__.py
+++ account_banking_mandate_sale/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Banking Mandate Sale',
-    'version': '10.0.1.0.1',
+    'version': '11.0.1.0.0',
     'category': 'Banking addons',
     'license': 'AGPL-3',
     'summary': "Adds mandates on sale orders",
