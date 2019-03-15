d6c1472fec95019619f8169f56471c082de9db4a
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2019-03-15 16:54:44 +0100

    [ADD] Payment Order: only payment order option on payment methods New option on payment methods: payment order only (unchecked by default) allowing to enforce the use of payment orders for some payment methods

--- account_payment_order/models/__init__.py
+++ account_payment_order/models/__init__.py
@@ -6,3 +6,6 @@ from . import account_move
 from . import account_move_line
 from . import account_invoice
 from . import res_bank
+from . import account_payment_method
+from . import account_journal
+from . import account_payment
