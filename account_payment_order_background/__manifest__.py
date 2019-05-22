fc290db41f7bea19083b57ae96cf4ac50c32a57c
sbejaoui <souheil.bejaoui@acsone.eu>
2019-05-22 15:45:51 +0200

    [MIG] - migration account_payment_order_background to 12.0

--- account_payment_order_background/__manifest__.py
+++ account_payment_order_background/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Payment Order in Background',
-    'summary': 'The reconciliation of move lines from payment order ',
-               'is processed in jobs.'
-    'version': '10.0.1.0.0',
+    'summary': 'The reconciliation of move lines from payment order '
+               'is processed in jobs.',
+    'version': '12.0.1.0.0',
     'author': 'Camptocamp',
     'license': 'AGPL-3',
     'category': 'Sales',
