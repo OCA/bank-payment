02eaeebde0f402c10410b759a21bbf0d3a185bd5
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-09-09 11:32:52 +0200

    [FIX] account_payment_sale: Propagate payment_mode to invoices created from a call to sale.action_invoice_create Into the normal process of the invoice creation from a sale order by odoo, the onchange_partner is called. In some case, this call will reset the specific payment_mode and/or partner_bank_id set on the sale_order. To fix this issue, we must override the _finalize_invoices method, take a snapshot of these values before the call to super and restore these values if these are changed. fixes #536

--- account_payment_sale/models/sale_order.py
+++ account_payment_sale/models/sale_order.py
@@ -33,3 +33,31 @@ class SaleOrder(models.Model):
         """Copy bank partner from sale order to invoice"""
         vals = super()._prepare_invoice()
         return self._get_payment_mode_vals(vals)
+
+    def _finalize_invoices(self, invoices, references):
+        """
+        Invoked after creating invoices at the end of action_invoice_create.
+
+        We must override this method since the onchange on partner is called by
+        the base method and therefore will change the specific payment_mode set
+        on the SO if one is defined on the partner..
+
+        :param invoices: {group_key: invoice}
+        :param references: {invoice: order}
+        """
+        payment_vals_by_invoice = {}
+        for invoice in invoices.values():
+            payment_vals_by_invoice[invoice] = {
+                'payment_mode_id': invoice.payment_mode_id.id,
+                'partner_bank_id': invoice.partner_bank_id.id
+            }
+        res = super()._finalize_invoices(invoices, references)
+        for invoice in invoices.values():
+            payment_vals = payment_vals_by_invoice[invoice]
+            if invoice.payment_mode_id.id == payment_vals['payment_mode_id']:
+                payment_vals.pop("payment_mode_id")
+            if invoice.partner_bank_id.id == payment_vals["partner_bank_id"]:
+                payment_vals.pop("partner_bank_id")
+            if payment_vals:
+                invoice.write(payment_vals)
+        return res
