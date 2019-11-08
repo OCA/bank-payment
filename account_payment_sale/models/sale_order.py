653530ddf5b2c45bfee363c2f23f994e4991b4f6
Raf Ven <raf.ven@dynapps.be>
2019-11-08 11:29:07 +0100

    [IMP] account_payment_sale: black, isort

--- account_payment_sale/models/sale_order.py
+++ account_payment_sale/models/sale_order.py
@@ -1,25 +1,28 @@
 # Copyright 2014-2016 Akretion - Alexis de Lattre
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     payment_mode_id = fields.Many2one(
-        'account.payment.mode', string='Payment Mode',
-        domain=[('payment_type', '=', 'inbound')])
+        "account.payment.mode",
+        string="Payment Mode",
+        domain=[("payment_type", "=", "inbound")],
+    )
 
     def _get_payment_mode_vals(self, vals):
         if self.payment_mode_id:
-            vals['payment_mode_id'] = self.payment_mode_id.id
-            if self.payment_mode_id.bank_account_link == 'fixed':
-                vals['partner_bank_id'] =\
-                    self.payment_mode_id.fixed_journal_id.bank_account_id.id
+            vals["payment_mode_id"] = self.payment_mode_id.id
+            if self.payment_mode_id.bank_account_link == "fixed":
+                vals[
+                    "partner_bank_id"
+                ] = self.payment_mode_id.fixed_journal_id.bank_account_id.id
         return vals
 
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def onchange_partner_id(self):
         res = super().onchange_partner_id()
         if self.partner_id:
@@ -48,13 +51,13 @@ class SaleOrder(models.Model):
         payment_vals_by_invoice = {}
         for invoice in invoices.values():
             payment_vals_by_invoice[invoice] = {
-                'payment_mode_id': invoice.payment_mode_id.id,
-                'partner_bank_id': invoice.partner_bank_id.id
+                "payment_mode_id": invoice.payment_mode_id.id,
+                "partner_bank_id": invoice.partner_bank_id.id,
             }
         res = super()._finalize_invoices(invoices, references)
         for invoice in invoices.values():
             payment_vals = payment_vals_by_invoice[invoice]
-            if invoice.payment_mode_id.id == payment_vals['payment_mode_id']:
+            if invoice.payment_mode_id.id == payment_vals["payment_mode_id"]:
                 payment_vals.pop("payment_mode_id")
             if invoice.partner_bank_id.id == payment_vals["partner_bank_id"]:
                 payment_vals.pop("partner_bank_id")
