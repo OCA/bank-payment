615dc1bdf0568bb85015a46c6277cf590a8d319b
Raf Ven <raf.ven@dynapps.be>
2019-11-08 11:40:36 +0100

    [IMP] account_payment_partner: black, isort

--- account_payment_partner/models/account_payment_mode.py
+++ account_payment_partner/models/account_payment_mode.py
@@ -2,7 +2,7 @@
 # Copyright 2018 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -11,39 +11,60 @@ class AccountPaymentMode(models.Model):
 
     show_bank_account = fields.Selection(
         selection=[
-            ('full', 'Full'),
-            ('first', 'First n chars'),
-            ('last', 'Last n chars'),
-            ('no', 'No'),
+            ("full", "Full"),
+            ("first", "First n chars"),
+            ("last", "Last n chars"),
+            ("no", "No"),
         ],
-        string='Show bank account',
-        default='full',
-        help="Show in invoices partial or full bank account number")
-    show_bank_account_from_journal = fields.Boolean(
-        string='Bank account from journals'
+        string="Show bank account",
+        default="full",
+        help="Show in invoices partial or full bank account number",
     )
+    show_bank_account_from_journal = fields.Boolean(string="Bank account from journals")
     show_bank_account_chars = fields.Integer(
-        string="# of digits for customer bank account",
+        string="# of digits for customer bank account"
     )
 
-    @api.constrains('company_id')
+    @api.constrains("company_id")
     def account_invoice_company_constrains(self):
         for mode in self:
-            if self.env['account.invoice'].sudo().search(
-                    [('payment_mode_id', '=', mode.id),
-                     ('company_id', '!=', mode.company_id.id)], limit=1):
-                raise ValidationError(_(
-                    "You cannot change the Company. There exists "
-                    "at least one Invoice with this Payment Mode, "
-                    "already assigned to another Company."))
+            if (
+                self.env["account.invoice"]
+                .sudo()
+                .search(
+                    [
+                        ("payment_mode_id", "=", mode.id),
+                        ("company_id", "!=", mode.company_id.id),
+                    ],
+                    limit=1,
+                )
+            ):
+                raise ValidationError(
+                    _(
+                        "You cannot change the Company. There exists "
+                        "at least one Invoice with this Payment Mode, "
+                        "already assigned to another Company."
+                    )
+                )
 
-    @api.constrains('company_id')
+    @api.constrains("company_id")
     def account_move_line_company_constrains(self):
         for mode in self:
-            if self.env['account.move.line'].sudo().search(
-                    [('payment_mode_id', '=', mode.id),
-                     ('company_id', '!=', mode.company_id.id)], limit=1):
-                raise ValidationError(_(
-                    "You cannot change the Company. There exists "
-                    "at least one Journal Item with this Payment Mode, "
-                    "already assigned to another Company."))
+            if (
+                self.env["account.move.line"]
+                .sudo()
+                .search(
+                    [
+                        ("payment_mode_id", "=", mode.id),
+                        ("company_id", "!=", mode.company_id.id),
+                    ],
+                    limit=1,
+                )
+            ):
+                raise ValidationError(
+                    _(
+                        "You cannot change the Company. There exists "
+                        "at least one Journal Item with this Payment Mode, "
+                        "already assigned to another Company."
+                    )
+                )
