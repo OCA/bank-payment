1ec4093c3a98ac5237de6d9c2a4bbcfa8a59c816
Raf Ven <raf.ven@dynapps.be>
2019-11-08 11:40:36 +0100

    [IMP] account_payment_partner: black, isort

--- account_payment_partner/models/account_move_line.py
+++ account_payment_partner/models/account_move_line.py
@@ -5,12 +5,12 @@ from odoo import fields, models
 
 
 class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
     payment_mode_id = fields.Many2one(
-        'account.payment.mode',
-        string='Payment Mode',
+        "account.payment.mode",
+        string="Payment Mode",
         domain="[('company_id', '=', company_id)]",
-        ondelete='restrict',
+        ondelete="restrict",
         index=True,
     )
--- account_payment_partner/models/res_partner.py
+++ account_payment_partner/models/res_partner.py
@@ -6,22 +6,26 @@ from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     # v8 fields : same without the _id suffix
     supplier_payment_mode_id = fields.Many2one(
-        'account.payment.mode', string='Supplier Payment Mode',
+        "account.payment.mode",
+        string="Supplier Payment Mode",
         company_dependent=True,
         domain="[('payment_type', '=', 'outbound')]",
-        help="Select the default payment mode for this supplier.")
+        help="Select the default payment mode for this supplier.",
+    )
     customer_payment_mode_id = fields.Many2one(
-        'account.payment.mode', string='Customer Payment Mode',
+        "account.payment.mode",
+        string="Customer Payment Mode",
         company_dependent=True,
         domain="[('payment_type', '=', 'inbound')]",
-        help="Select the default payment mode for this customer.")
+        help="Select the default payment mode for this customer.",
+    )
 
     @api.model
     def _commercial_fields(self):
         res = super(ResPartner, self)._commercial_fields()
-        res += ['supplier_payment_mode_id', 'customer_payment_mode_id']
+        res += ["supplier_payment_mode_id", "customer_payment_mode_id"]
         return res
