5e01124ced2551bed44b483291116a27b2d85ec8
Nooka10 <mails@benoitschopfer.ch>
2019-12-10 15:55:37 +0100

    Remove all useless # -*- coding: utf-8 -*-

--- account_banking_sepa_credit_transfer/__init__.py
+++ account_banking_sepa_credit_transfer/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 from . import models
 from .post_install import update_bank_journals
--- account_banking_sepa_direct_debit/models/__init__.py
+++ account_banking_sepa_direct_debit/models/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from . import res_company
 from . import res_config
 from . import account_banking_mandate
