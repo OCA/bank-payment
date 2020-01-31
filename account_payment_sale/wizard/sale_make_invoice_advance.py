cf54f65fcfb67c067d71ccecf5a921344e966a9a
Raf Ven <raf.ven@dynapps.be>
2020-01-31 15:37:36 +0100

    [MIG] account_payment_sale: Migration to 13.0

--- account_payment_sale/wizard/sale_make_invoice_advance.py
+++ account_payment_sale/wizard/sale_make_invoice_advance.py
@@ -1,13 +1,12 @@
 # Copyright 2016 Akretion - Alexis de Lattre
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class SaleAdvancePaymentInv(models.TransientModel):
     _inherit = "sale.advance.payment.inv"
 
-    @api.multi
     def _create_invoice(self, order, so_line, amount):
         """Copy payment mode from sale order to invoice"""
         inv = super()._create_invoice(order, so_line, amount)
