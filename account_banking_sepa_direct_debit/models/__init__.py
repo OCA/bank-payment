dd69936fbd81c13ba2a0eb930e8d7bc6c7686e99
Marçal Isern <marsal.isern@qubiq.es>
2020-02-24 20:35:52 +0100

    [IMP] account_banking_sepa_direct_debit: black, isort

--- account_banking_sepa_direct_debit/models/__init__.py
+++ account_banking_sepa_direct_debit/models/__init__.py
@@ -1,4 +1,3 @@
-
 from . import res_company
 from . import res_config
 from . import account_banking_mandate
--- account_banking_sepa_direct_debit/models/res_config.py
+++ account_banking_sepa_direct_debit/models/res_config.py
@@ -1,13 +1,12 @@
 # Copyright 2016 Akretion - Alexis de Lattre
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     sepa_creditor_identifier = fields.Char(
-        related='company_id.sepa_creditor_identifier',
-        readonly=False,
+        related="company_id.sepa_creditor_identifier", readonly=False
     )
