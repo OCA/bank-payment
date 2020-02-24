110b5415bcfdeadabb7d575bb75000aca565cb9b
Marçal Isern <marsal.isern@qubiq.es>
2020-02-24 20:35:52 +0100

    [IMP] account_banking_sepa_direct_debit: black, isort

--- account_banking_sepa_direct_debit/models/bank_payment_line.py
+++ account_banking_sepa_direct_debit/models/bank_payment_line.py
@@ -1,11 +1,11 @@
 # Copyright 2015-2016 Akretion - Alexis de Lattre
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, api
+from odoo import api, models
 
 
 class BankPaymentLine(models.Model):
-    _inherit = 'bank.payment.line'
+    _inherit = "bank.payment.line"
 
     @api.multi
     def move_line_offsetting_account_hashcode(self):
@@ -16,7 +16,6 @@ class BankPaymentLine(models.Model):
         So we split the transfer move lines by mandate type, so easier
         reconciliation of the bank statement.
         """
-        hashcode = super(BankPaymentLine, self).\
-            move_line_offsetting_account_hashcode()
-        hashcode += '-' + str(self.mandate_id.recurrent_sequence_type)
+        hashcode = super(BankPaymentLine, self).move_line_offsetting_account_hashcode()
+        hashcode += "-" + str(self.mandate_id.recurrent_sequence_type)
         return hashcode
