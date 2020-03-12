b6471afcbf970fef767f3f6153e37608fbd46327
Raf Ven <raf.ven@dynapps.be>
2020-03-12 19:46:51 +0100

    [IMP] account_payment_order: black, isort

--- account_payment_order/migrations/12.0.1.2.0/post-migration.py
+++ account_payment_order/migrations/12.0.1.2.0/post-migration.py
@@ -7,9 +7,12 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.set_xml_ids_noupdate_value(
-        env, 'account_payment_order', [
-            'bank_payment_line_seq',
-            'account_payment_line_seq',
-            'account_payment_order_seq',
-        ], True,
+        env,
+        "account_payment_order",
+        [
+            "bank_payment_line_seq",
+            "account_payment_line_seq",
+            "account_payment_order_seq",
+        ],
+        True,
     )
