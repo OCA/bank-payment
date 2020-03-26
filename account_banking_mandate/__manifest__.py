bf1c60d784049d88b2fb477932c9292e769d282a
Marçal Isern <marsal.isern@qubiq.es>
2020-03-26 20:58:31 +0100

    [IMP] account_banking_madante: black, isort

--- account_banking_mandate/__manifest__.py
+++ account_banking_mandate/__manifest__.py
@@ -5,31 +5,29 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Account Banking Mandate',
-    'summary': 'Banking mandates',
-    'version': '12.0.2.0.0',
-    'license': 'AGPL-3',
-    'author': "Compassion CH, "
-              "Tecnativa, "
-              "Akretion, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/bank-payment',
-    'category': 'Banking addons',
-    'depends': [
-        'account_payment_order',
+    "name": "Account Banking Mandate",
+    "summary": "Banking mandates",
+    "version": "12.0.2.0.0",
+    "license": "AGPL-3",
+    "author": "Compassion CH, "
+    "Tecnativa, "
+    "Akretion, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/bank-payment",
+    "category": "Banking addons",
+    "depends": ["account_payment_order"],
+    "data": [
+        "views/account_banking_mandate_view.xml",
+        "views/account_payment_method.xml",
+        "views/account_invoice_view.xml",
+        "views/account_payment_line.xml",
+        "views/res_partner_bank_view.xml",
+        "views/res_partner.xml",
+        "views/bank_payment_line_view.xml",
+        "views/account_move_line.xml",
+        "data/mandate_reference_sequence.xml",
+        "security/mandate_security.xml",
+        "security/ir.model.access.csv",
     ],
-    'data': [
-        'views/account_banking_mandate_view.xml',
-        'views/account_payment_method.xml',
-        'views/account_invoice_view.xml',
-        'views/account_payment_line.xml',
-        'views/res_partner_bank_view.xml',
-        'views/res_partner.xml',
-        'views/bank_payment_line_view.xml',
-        'views/account_move_line.xml',
-        'data/mandate_reference_sequence.xml',
-        'security/mandate_security.xml',
-        'security/ir.model.access.csv',
-    ],
-    'installable': True,
+    "installable": True,
 }
