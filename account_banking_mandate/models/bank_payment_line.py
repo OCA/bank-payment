61f3f0b358f61c2e0374bd04af05ef95d9f5ca07
Marçal Isern <marsal.isern@qubiq.es>
2020-03-26 20:58:31 +0100

    [IMP] account_banking_madante: black, isort

--- account_banking_mandate/models/bank_payment_line.py
+++ account_banking_mandate/models/bank_payment_line.py
@@ -7,15 +7,18 @@ from odoo import api, fields, models
 
 
 class BankPaymentLine(models.Model):
-    _inherit = 'bank.payment.line'
+    _inherit = "bank.payment.line"
 
     mandate_id = fields.Many2one(
-        comodel_name='account.banking.mandate', string='Direct Debit Mandate',
-        related='payment_line_ids.mandate_id')
+        comodel_name="account.banking.mandate",
+        string="Direct Debit Mandate",
+        related="payment_line_ids.mandate_id",
+    )
 
     @api.model
     def same_fields_payment_line_and_bank_payment_line(self):
-        res = super(BankPaymentLine, self).\
-            same_fields_payment_line_and_bank_payment_line()
-        res.append('mandate_id')
+        res = super(
+            BankPaymentLine, self
+        ).same_fields_payment_line_and_bank_payment_line()
+        res.append("mandate_id")
         return res
