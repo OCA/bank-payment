c02412b487b8f984253228abb0cd46e0a6032a6a
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-04-21 18:36:00 +0200

    [IMP] account_banking_pain_base: black, isort, prettier

--- account_banking_pain_base/__manifest__.py
+++ account_banking_pain_base/__manifest__.py
@@ -4,29 +4,24 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Account Banking PAIN Base Module',
-    'summary': 'Base module for PAIN file generation',
-    'version': '12.0.1.0.2',
-    'license': 'AGPL-3',
-    'author': "Akretion, "
-              "Noviat, "
-              "Tecnativa, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/bank-payment',
-    'category': 'Hidden',
-    'depends': ['account_payment_order'],
-    'external_dependencies': {
-        'python': ['unidecode', 'lxml'],
-    },
-    'data': [
-        'security/security.xml',
-        'views/account_payment_line.xml',
-        'views/account_payment_order.xml',
-        'views/bank_payment_line_view.xml',
-        'views/account_payment_mode.xml',
-        'views/res_config_settings.xml',
-        'views/account_payment_method.xml',
+    "name": "Account Banking PAIN Base Module",
+    "summary": "Base module for PAIN file generation",
+    "version": "12.0.1.0.2",
+    "license": "AGPL-3",
+    "author": "Akretion, " "Noviat, " "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/bank-payment",
+    "category": "Hidden",
+    "depends": ["account_payment_order"],
+    "external_dependencies": {"python": ["unidecode", "lxml"],},
+    "data": [
+        "security/security.xml",
+        "views/account_payment_line.xml",
+        "views/account_payment_order.xml",
+        "views/bank_payment_line_view.xml",
+        "views/account_payment_mode.xml",
+        "views/res_config_settings.xml",
+        "views/account_payment_method.xml",
     ],
-    'post_init_hook': 'set_default_initiating_party',
-    'installable': True,
+    "post_init_hook": "set_default_initiating_party",
+    "installable": True,
 }
