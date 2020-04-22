79d5b17c657a33701eb0305d4e6bafbabe913dd1
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-04-22 13:48:57 +0200

    [MIG] account_banking_sepa_credit_transfer: Migration to 13.0

--- account_banking_sepa_credit_transfer/models/account_payment_line.py
+++ account_banking_sepa_credit_transfer/models/account_payment_line.py
@@ -1,5 +1,5 @@
 # Copyright 2017 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
@@ -7,7 +7,7 @@ from odoo import fields, models
 class AccountPaymentLine(models.Model):
     _inherit = "account.payment.line"
 
-    # local_instrument 'INST' used for instant credit transfers
+    # Local_instrument 'INST' used for instant credit transfers
     # which will begin on November 21st 2017, cf
     # https://www.europeanpaymentscouncil.eu/document-library/
     # rulebooks/2017-sepa-instant-credit-transfer-rulebook
