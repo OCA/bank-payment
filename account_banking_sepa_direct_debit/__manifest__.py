597e2b273d89e534c315da18fbebb7ff75608842
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-08-18 17:16:45 +0200

    [IMP] account_banking_sepa_direct_debit: Ease inheritance in tests
    - Unfold SDD test in 2 classes: the base with initialization + the other with tests
    - Internal variable for parametrizing the chart template to load

--- account_banking_sepa_direct_debit/__manifest__.py
+++ account_banking_sepa_direct_debit/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Account Banking SEPA Direct Debit",
     "summary": "Create SEPA files for Direct Debit",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "license": "AGPL-3",
     "author": "Akretion, " "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/bank-payment",
