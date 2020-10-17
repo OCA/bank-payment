f268125f3ead0857c63963b5961b7878874bb48c
Marçal Isern <marsal.isern@qubiq.es>
2020-10-17 09:00:24 +0200

    [MIG] account_payment_partner: Migration to 14.0

--- account_payment_partner/models/account_move.py
+++ account_payment_partner/models/account_move.py
@@ -25,29 +25,29 @@ class AccountMove(models.Model):
     bank_account_required = fields.Boolean(
         related="payment_mode_id.payment_method_id.bank_account_required", readonly=True
     )
-    invoice_partner_bank_id = fields.Many2one(
-        compute="_compute_invoice_partner_bank",
+    partner_bank_id = fields.Many2one(
+        compute="_compute_partner_bank",
         store=True,
         ondelete="restrict",
         readonly=False,
     )
 
-    @api.depends("type")
+    @api.depends("move_type")
     def _compute_payment_mode_filter_type_domain(self):
         for move in self:
-            if move.type in ("out_invoice", "in_refund"):
+            if move.move_type in ("out_invoice", "in_refund"):
                 move.payment_mode_filter_type_domain = "inbound"
-            elif move.type in ("in_invoice", "out_refund"):
+            elif move.move_type in ("in_invoice", "out_refund"):
                 move.payment_mode_filter_type_domain = "outbound"
             else:
                 move.payment_mode_filter_type_domain = False
 
-    @api.depends("partner_id", "type")
+    @api.depends("partner_id", "move_type")
     def _compute_partner_bank_filter_type_domain(self):
         for move in self:
-            if move.type in ("out_invoice", "in_refund"):
+            if move.move_type in ("out_invoice", "in_refund"):
                 move.partner_bank_filter_type_domain = move.bank_partner_id
-            elif move.type in ("in_invoice", "out_refund"):
+            elif move.move_type in ("in_invoice", "out_refund"):
                 move.partner_bank_filter_type_domain = move.commercial_partner_id
             else:
                 move.partner_bank_filter_type_domain = False
@@ -57,17 +57,17 @@ class AccountMove(models.Model):
         for move in self:
             move.payment_mode_id = False
             if move.partner_id:
-                if move.type == "in_invoice":
-                    move.payment_mode_id = move.with_context(
-                        force_company=move.company_id.id
+                if move.move_type == "in_invoice":
+                    move.payment_mode_id = move.with_company(
+                        move.company_id.id
                     ).partner_id.supplier_payment_mode_id
-                elif move.type == "out_invoice":
-                    move.payment_mode_id = move.with_context(
-                        force_company=move.company_id.id
+                elif move.move_type == "out_invoice":
+                    move.payment_mode_id = move.with_company(
+                        move.company_id.id
                     ).partner_id.customer_payment_mode_id
 
     @api.depends("partner_id", "payment_mode_id")
-    def _compute_invoice_partner_bank(self):
+    def _compute_partner_bank(self):
         for move in self:
             # No bank account assignation is done for out_invoice as this is only
             # needed for printing purposes and it can conflict with
@@ -80,7 +80,7 @@ class AccountMove(models.Model):
             bank_id = False
             if move.partner_id:
                 pay_mode = move.payment_mode_id
-                if move.type == "in_invoice":
+                if move.move_type == "in_invoice":
                     if (
                         pay_mode
                         and pay_mode.payment_type == "outbound"
@@ -88,7 +88,7 @@ class AccountMove(models.Model):
                         and move.commercial_partner_id.bank_ids
                     ):
                         bank_id = get_bank_id()
-            move.invoice_partner_bank_id = bank_id
+            move.partner_bank_id = bank_id
 
     # I think copying payment mode from invoice to refund by default
     # is a good idea because the most common way of "paying" a refund is to
@@ -97,8 +97,8 @@ class AccountMove(models.Model):
     def _reverse_move_vals(self, default_values, cancel=True):
         move_vals = super()._reverse_move_vals(default_values, cancel=cancel)
         move_vals["payment_mode_id"] = self.payment_mode_id.id
-        if self.type == "in_invoice":
-            move_vals["invoice_partner_bank_id"] = self.invoice_partner_bank_id.id
+        if self.move_type == "in_invoice":
+            move_vals["partner_bank_id"] = self.partner_bank_id.id
         return move_vals
 
     @api.constrains("company_id", "payment_mode_id")
@@ -115,8 +115,8 @@ class AccountMove(models.Model):
 
     def partner_banks_to_show(self):
         self.ensure_one()
-        if self.invoice_partner_bank_id:
-            return self.invoice_partner_bank_id
+        if self.partner_bank_id:
+            return self.partner_bank_id
         if self.payment_mode_id.show_bank_account_from_journal:
             if self.payment_mode_id.bank_account_link == "fixed":
                 return self.payment_mode_id.fixed_journal_id.bank_account_id
@@ -132,4 +132,4 @@ class AccountMove(models.Model):
                 or self.partner_id.valid_mandate_id.partner_bank_id
             )
         # Return this as empty recordset
-        return self.invoice_partner_bank_id
+        return self.partner_bank_id
