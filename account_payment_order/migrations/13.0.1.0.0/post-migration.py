165138624e9f8dceafd7e7cfe419381e3cfa589d
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-12-04 20:31:26 +0100

    [OU] account_payment_order: Transfer reference_type in invoice

--- account_payment_order/migrations/13.0.1.0.0/post-migration.py
+++ account_payment_order/migrations/13.0.1.0.0/post-migration.py
@@ -9,3 +9,11 @@ def migrate(env, version):
     openupgrade.load_data(
         env.cr, "account_payment_order", "migrations/13.0.1.0.0/noupdate_changes.xml"
     )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE account_move am
+        SET reference_type = ai.reference_type
+        FROM account_invoice ai
+        WHERE ai.id = am.old_invoice_id""",
+    )
