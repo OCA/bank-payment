a052dcf1a30bc2ba5dd1fe06a41234c74f9b98c0
Alexis de Lattre <alexis.delattre@akretion.com>
2020-12-24 10:38:19 +0100

    [MIG] account_banking_mandate from v12 to v14

--- account_banking_mandate_sale/models/sale_order.py
+++ account_banking_mandate_sale/models/sale_order.py
@@ -1,7 +1,7 @@
-# © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2014-2020 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
@@ -9,34 +9,36 @@ class SaleOrder(models.Model):
 
     # This field commercial_partner_id should be moved
     # in an OCA base module named for example sale_commercial_partner
-    commercial_partner_id = fields.Many2one(
-        related='partner_id.commercial_partner_id', string='Commercial Entity',
-        store=True, readonly=True)
     mandate_id = fields.Many2one(
-        'account.banking.mandate', string='Direct Debit Mandate',
-        ondelete='restrict', readonly=True,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
+        'account.banking.mandate',
+        string='Direct Debit Mandate',
+        ondelete='restrict',
+        check_company=True,
+        readonly=False,
+        domain="[('partner_id', '=', commercial_partner_id), ('state', 'in', ('draft', 'valid')), ('company_id', '=', company_id)]",
+        )
     mandate_required = fields.Boolean(
         related='payment_mode_id.payment_method_id.mandate_required',
-        readonly=True,
     )
 
-    @api.multi
     def _prepare_invoice(self):
         """Copy mandate from sale order to invoice"""
-        vals = super(SaleOrder, self)._prepare_invoice()
-        vals['mandate_id'] = self.mandate_id.id
+        vals = super()._prepare_invoice()
+        vals['mandate_id'] = self.mandate_id.id or False
         return vals
 
-    @api.onchange('payment_mode_id')
-    def payment_mode_change(self):
+    @api.depends("partner_id")
+    def _compute_payment_mode(self):
         """Select by default the first valid mandate of the partner"""
-        self.ensure_one()
-        if self.mandate_required and self.partner_id:
-            mandates = self.env['account.banking.mandate'].search([
-                ('state', '=', 'valid'),
-                ('partner_id', '=', self.commercial_partner_id.id),
-            ])
-            self.mandate_id = mandates[:1]
-        else:
-            self.mandate_id = False
+        super()._compute_payment_mode()
+        abm_obj = self.env['account.banking.mandate']
+        for order in self:
+            if order.mandate_required and order.partner_id:
+                mandate = abm_obj.search([
+                    ('state', '=', 'valid'),
+                    ('partner_id', '=', order.commercial_partner_id.id),
+                    ('company_id', '=', order.company_id.id),
+                ], limit=1)
+                order.mandate_id = mandate or False
+            else:
+                order.mandate_id = False
