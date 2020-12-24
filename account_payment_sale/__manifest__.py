663b6f83b8e1f22441e5136a6e7ab71ea87f14d0
Alexis de Lattre <alexis.delattre@akretion.com>
2020-12-24 10:33:49 +0100

    [MIG] account_payment_sale to 14.0

--- account_payment_sale/__manifest__.py
+++ account_payment_sale/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Payment Sale",
-    "version": "13.0.1.1.1",
+    "version": "14.0.1.0.0",
     "category": "Banking addons",
     "license": "AGPL-3",
     "summary": "Adds payment mode on sale orders",
