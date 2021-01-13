cdbac1825b5345bc1e83cd4bc85f87e02ba5aac3
Alexis de Lattre <alexis@via.ecp.fr>
2021-01-13 16:11:30 +0100

    [MIG] account_banking_sepa_direct_debit to v14.0

--- account_banking_sepa_direct_debit/models/account_payment_method.py
+++ account_banking_sepa_direct_debit/models/account_payment_method.py
@@ -1,4 +1,4 @@
-# Copyright 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2020 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -13,7 +13,13 @@ class AccountPaymentMethod(models.Model):
             ("pain.008.001.03", "pain.008.001.03"),
             ("pain.008.001.04", "pain.008.001.04"),
             ("pain.008.003.02", "pain.008.003.02 (direct debit in Germany)"),
-        ]
+        ],
+        ondelete={
+            "pain.008.001.02": "set null",
+            "pain.008.001.03": "set null",
+            "pain.008.001.04": "set null",
+            "pain.008.003.02": "set null",
+        },
     )
 
     def get_xsd_file_path(self):
@@ -26,4 +32,4 @@ class AccountPaymentMethod(models.Model):
         ]:
             path = "account_banking_sepa_direct_debit/data/%s.xsd" % self.pain_version
             return path
-        return super(AccountPaymentMethod, self).get_xsd_file_path()
+        return super().get_xsd_file_path()
