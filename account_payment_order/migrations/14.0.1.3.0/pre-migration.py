c606ecc58b37eeb96b3b2b7fc656c65a2b54ceac
Alexis de Lattre <alexis.delattre@akretion.com>
2021-07-03 09:38:01 +0200

    account_payment_partner: add optional="hide" on payment mode in invoice tree view

--- account_payment_order/migrations/14.0.1.3.0/pre-migration.py
+++ account_payment_order/migrations/14.0.1.3.0/pre-migration.py
@@ -2,9 +2,9 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+
 def migrate(cr, version):
     if not version:
         return
 
-    cr.execute(
-        "UPDATE account_payment_order SET state='uploaded' WHERE state='done'")
+    cr.execute("UPDATE account_payment_order SET state='uploaded' WHERE state='done'")
