3dba1c44112b7cad3ad4d9d7360185d983186147
dani <dani.xtendoo@gmail.com>
2021-08-23 11:38:41 +0200

    [ADD] account_banking_mandate_sale: black, isort

--- account_banking_mandate_sale/__manifest__.py
+++ account_banking_mandate_sale/__manifest__.py
@@ -1,21 +1,18 @@
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2021 DanielDominguez (Xtendoo)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Banking Mandate Sale',
-    'version': '12.0.1.0.1',
-    'category': 'Banking addons',
-    'license': 'AGPL-3',
-    'summary': "Adds mandates on sale orders",
-    'author': "Odoo Community Association (OCA), "
-              "Akretion",
-    'website': 'https://github.com/OCA/bank-payment',
-    'depends': [
-        'account_payment_sale',
-        'account_banking_mandate',
-    ],
-    'data': [
-        'views/sale_order.xml',
-    ],
-    'installable': True,
+    "name": "Account Banking Mandate Sale",
+    "version": "13.0.1.0.1",
+    "category": "Banking addons",
+    "license": "AGPL-3",
+    "summary": "Adds mandates on sale orders",
+    "author": "Odoo Community Association (OCA), "
+    "Akretion,"
+    "Daniel Domínguez (Xtendoo)",
+    "website": "https://github.com/OCA/bank-payment",
+    "depends": ["account_payment_sale", "account_banking_mandate"],
+    "data": ["views/sale_order.xml"],
+    "installable": True,
 }
