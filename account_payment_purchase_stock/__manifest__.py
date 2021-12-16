8b02ef752465ec93c72af434509f32e89d618df2
Koen Loodts <koen.loodts@dynapps.be>
2021-12-16 17:49:49 +0100

    [MIG] account_payment_purchase_stock: Migration to 15.0

--- account_payment_purchase_stock/__manifest__.py
+++ account_payment_purchase_stock/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Payment Purchase Stock",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "category": "Banking addons",
     "license": "AGPL-3",
     "summary": "Integrate Account Payment Purchase with Stock",
