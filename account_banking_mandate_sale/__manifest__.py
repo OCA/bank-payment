c710cd09d7413b564437da5bdfcb6392683ac8dd
Alexis de Lattre <alexis.delattre@akretion.com>
2022-01-05 00:23:02 +0100

    [MIG] account_banking_mandate_sale from v14 to v15
    The mandate must now be linked to the invoicing partner and not to the partner of the sale order

--- account_banking_mandate_sale/__manifest__.py
+++ account_banking_mandate_sale/__manifest__.py
@@ -1,17 +1,17 @@
-# Copyright 2016-2020 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2022 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Account Banking Mandate Sale",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Banking addons",
     "license": "AGPL-3",
     "summary": "Adds mandates on sale orders",
-    "author": "Odoo Community Association (OCA), " "Akretion",
+    "author": "Odoo Community Association (OCA), Akretion",
+    "maintainers": ["alexis-via"],
     "website": "https://github.com/OCA/bank-payment",
     "depends": [
         "account_payment_sale",
-        "sale_commercial_partner",
         "account_banking_mandate",
     ],
     "data": [
