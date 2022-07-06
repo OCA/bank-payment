748814e2b77865273712f75122aec5571ed72669
David Ramia <david.ramia@processcontrol.es>
2022-07-06 14:00:22 +0200

    [FIX] account_banking_pain_base, account_banking_sepa_credit_transfer, account_banking_direct_debit: Remove Environment.manage()
    Environment.manage() is deprecated in v 15.0

--- account_banking_sepa_direct_debit/post_install.py
+++ account_banking_sepa_direct_debit/post_install.py
@@ -5,21 +5,20 @@ from odoo import SUPERUSER_ID, api
 
 
 def update_bank_journals(cr, registry):
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        ajo = env["account.journal"]
-        journals = ajo.search([("type", "=", "bank")])
-        sdd = env.ref("account_banking_sepa_direct_debit.sepa_direct_debit")
-        if sdd:
-            journals.write(
-                {
-                    "inbound_payment_method_line_ids": [
-                        (
-                            0,
-                            0,
-                            {"payment_method_id": sdd.id, "name": "SEPA Direct Debit"},
-                        )
-                    ]
-                }
-            )
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    ajo = env["account.journal"]
+    journals = ajo.search([("type", "=", "bank")])
+    sdd = env.ref("account_banking_sepa_direct_debit.sepa_direct_debit")
+    if sdd:
+        journals.write(
+            {
+                "inbound_payment_method_line_ids": [
+                    (
+                        0,
+                        0,
+                        {"payment_method_id": sdd.id, "name": "SEPA Direct Debit"},
+                    )
+                ]
+            }
+        )
     return
