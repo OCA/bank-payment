54b94f61e95cccdb65aa077e3e4b96fc50254dc8
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-07-30 20:02:18 -0300

    [ADD] account_payment_order_tier_validation

---
+++ account_payment_order_tier_validation/models/tier_definition.py
@@ -0,0 +1,15 @@
+# Copyright 2022 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class TierDefinition(models.Model):
+
+    _inherit = "tier.definition"
+
+    @api.model
+    def _get_tier_validation_model_names(self):
+        res = super()._get_tier_validation_model_names()
+        res.append("account.payment.order")
+        return res
