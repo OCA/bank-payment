976b3c5c53c6854c16831bc516da37d1af91bf6b
Xavier Bouquiaux <xavier.bouquiaux@acsone.eu>
2022-08-24 15:22:24 +0200

    [ADD] account_payment_order_sequence_payment_mode

---
+++ account_payment_order_sequence_payment_mode/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2022 Acsone (<http://www.acsone.eu>))
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Payment Order Sequence Payment Mode",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/bank-payment",
+    "category": "Banking addons",
+    "depends": ["account_payment_order"],
+    "data": [
+        "views/account_payment_mode.xml",
+    ],
+    "installable": True,
+}
