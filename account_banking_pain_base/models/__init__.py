1e83e49f87777c9e3fa50aaac6bdb4e8cf495734
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-11-06 13:16:17 +0100

    [REF] account_banking_pain_base: Adapt module to native payment refactoring

--- account_banking_pain_base/models/__init__.py
+++ account_banking_pain_base/models/__init__.py
@@ -1,6 +1,5 @@
 from . import account_payment_line
 from . import account_payment_order
-from . import bank_payment_line
 from . import account_payment_mode
 from . import res_company
 from . import res_config_settings
