6ab2c78111dc8e78943c75fb8f271ec0eef498f5
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-11-06 14:00:59 +0100

    [REF] account_banking_mandate: Adapt module to native payment refactoring

--- account_banking_mandate/__manifest__.py
+++ account_banking_mandate/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2014 Compassion CH - Cyril Sester <csester@compassion.ch>
-# Copyright 2014 Tecnativa - Pedro M. Baeza
 # Copyright 2015-2020 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
-# Copyright 2017 Tecnativa - Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2017 Tecnativa - Carlos Dauden
+# Copyright 2014-2022 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
@@ -24,7 +24,6 @@
         "views/account_payment_line.xml",
         "views/res_partner_bank_view.xml",
         "views/res_partner.xml",
-        "views/bank_payment_line_view.xml",
         "data/mandate_reference_sequence.xml",
         "security/mandate_security.xml",
         "security/ir.model.access.csv",
