acad3798d95b40ae64220780cb154df8faee92e7
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-11-06 09:11:45 +0100

    [REF+IMP] account_payment_order: Use native payments
    The previous approach creates manually the journal entries and does all
    the hard work, plus not being 100% compatible with the bank statement
    reconciliation widget (requiring a patch on OCB to see blue lines).
    
    That decision made sense on the moment it was done (v9), where the
    native payment model (account.payment) was very limited, and wasn't able
    to store all the needed information for the bank transaction.
    
    Now that the limitations are gone, we can get rid off this extra model,
    and generate instead `account.payment` records, using both the native
    model + methods to perform the same operations.
    
    This serves also to workaround the problem found in #966.
    
    All the code, views and tests of main module have been adapted to this
    new approach in this commit. Later commits will adapt the rest of the
    modules of the suite, and add migration scripts to transit from the
    previous approach to this new one.
    
    TT39832

--- account_payment_order/models/__init__.py
+++ account_payment_order/models/__init__.py
@@ -1,7 +1,6 @@
 from . import account_payment_mode
 from . import account_payment_order
 from . import account_payment_line
-from . import bank_payment_line
 from . import account_move
 from . import account_move_line
 from . import res_bank
