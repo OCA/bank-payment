c3ad144cf8d7c3956b55dc550553ee5e0b06a282
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-11-06 13:16:17 +0100

    [REF] account_banking_pain_base: Adapt module to native payment refactoring

--- account_banking_pain_base/__manifest__.py
+++ account_banking_pain_base/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2013-2016 Akretion - Alexis de Lattre
-# Copyright 2014-2017 Tecnativa - Pedro M. Baeza
 # Copyright 2016 Tecnativa - Antonio Espinosa
 # Copyright 2021 Tecnativa - Carlos Roca
+# Copyright 2014-2022 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Account Banking PAIN Base Module",
     "summary": "Base module for PAIN file generation",
-    "version": "15.0.1.0.3",
+    "version": "15.0.2.0.0",
     "license": "AGPL-3",
     "author": "Akretion, Noviat, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/bank-payment",
@@ -18,7 +18,6 @@
         "security/security.xml",
         "views/account_payment_line.xml",
         "views/account_payment_order.xml",
-        "views/bank_payment_line_view.xml",
         "views/account_payment_mode.xml",
         "views/res_config_settings.xml",
         "views/account_payment_method.xml",
