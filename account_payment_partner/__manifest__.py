d71fb735218aa008502e579ec1a6ab563c9b2899
David Ramia <david.ramia@processcontrol.es>
2023-01-02 13:58:36 +0100

    [FIX] account_payment_partner: fix error in _reverse_moves

--- account_payment_partner/__manifest__.py
+++ account_payment_partner/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Account Payment Partner",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "category": "Banking addons",
     "license": "AGPL-3",
     "summary": "Adds payment mode on partners and invoices",
