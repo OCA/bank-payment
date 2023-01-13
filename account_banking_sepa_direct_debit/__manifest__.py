03c400608c7d9b68743ba779ca5f67906d6867bd
Giovanni Francesco Capalbo <giovanni@therp.nl>
2023-01-13 11:23:15 +0100

    [IMP] account_banking_sepa_direct_debit: pre-commit stuff

--- account_banking_sepa_direct_debit/__manifest__.py
+++ account_banking_sepa_direct_debit/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Account Banking SEPA Direct Debit",
     "summary": "Create SEPA files for Direct Debit",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion, " "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/bank-payment",
