2b4bc079c2342b254c357069636b3a7404d80a1e
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-01-18 10:02:31 +0100

    [IMP] account_payment_partner: Make payment mode editable on entry
    Complement of 998f8df6aa254266c015fab144ba290e1a386cf0
    
    Extend the editability to the journal entry (account.move), taking
    as criteria if there's no reconciliation made, which is the definitory
    one.
    
    It includes test for this new feature, both the mode propagation and
    the editability check.
    
    TT39850

--- account_payment_partner/tests/test_account_payment_partner.py
+++ account_payment_partner/tests/test_account_payment_partner.py
@@ -258,6 +258,10 @@ class TestAccountPaymentPartner(TransactionCase):
         mode = self.supplier_payment_mode.copy()
         aml.payment_mode_id = mode
         self.assertEqual(invoice.payment_mode_id, mode)
+        # Test payment mode editability on account move
+        self.assertFalse(invoice.has_reconciled_items)
+        invoice.payment_mode_id = self.supplier_payment_mode
+        self.assertEqual(aml.payment_mode_id, self.supplier_payment_mode)
 
     def test_invoice_create_out_invoice(self):
         invoice = self._create_invoice(
