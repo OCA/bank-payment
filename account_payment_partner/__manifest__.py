adca3a889830549b1ea834a1e6317624e38dc27d
Danny W. Adair <danny.adair@unfold.co.nz>
2023-02-24 11:10:17 +1300

    Add pre_init_hook to add computed columns
    Add columns to avoid Memory error on an existing Odoo instance
    with lots of data.
    
    The payment_mode_id fields are introduced by this module and computed only
    from each other or the also newly introduced supplier_payment_mode_id and
    customer_payment_mode_id on res.partner, so they can stay NULL, nothing
    to compute.

--- account_payment_partner/__manifest__.py
+++ account_payment_partner/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Account Payment Partner",
-    "version": "16.0.1.1.0",
+    "version": "16.0.1.1.1",
     "category": "Banking addons",
     "license": "AGPL-3",
     "summary": "Adds payment mode on partners and invoices",
@@ -24,4 +24,5 @@
     ],
     "demo": ["demo/partner_demo.xml"],
     "installable": True,
+    "pre_init_hook": "pre_init_hook",
 }
