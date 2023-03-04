d62c52396784be6a0f46339743c723ba31ea8668
Ruchir Shukla <ruchir@bizzappdev.com>
2023-03-04 23:35:45 +0530

    [MIG][15.0]account_payment_order_sequence_payment_mode:Migration to 15.0

--- account_payment_order_sequence_payment_mode/__manifest__.py
+++ account_payment_order_sequence_payment_mode/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Payment Order Sequence Payment Mode",
-    "version": "14.0.1.0.0",
+    "version": "15.0.0.1.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/bank-payment",
