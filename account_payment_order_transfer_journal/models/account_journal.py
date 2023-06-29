74012dcfbc568dc9ffe494e032a7e48b572e8239
Luc De Meyer <luc.demeyer@noviat.com>
2023-06-29 14:49:56 +0200

    account_payment_order_transfer_journal - add 'general' filter on transfer journal

--- account_payment_order_transfer_journal/models/account_journal.py
+++ account_payment_order_transfer_journal/models/account_journal.py
@@ -1,15 +1,16 @@
 # Copyright 2022 ACSONE SA/NV
+# Copyright 2023 Noviat
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
 
 class AccountJournal(models.Model):
-
     _inherit = "account.journal"
 
     transfer_journal_id = fields.Many2one(
         comodel_name="account.journal",
         string="Transfer journal on payment/debit orders",
+        domain="[('type', '=', 'general')]",
         help="Journal to write payment entries when confirming payment/debit orders",
     )
