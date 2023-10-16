a0bde7a5c1db61e517a896076002d9823fffeb1e
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-16 12:27:21 +0200

    [IMP] account_payment_order: test performance improvement - Create setUpClass method - Switch to setUpClass (if needed) for avoiding repeat the same setup for each test. - Include context keys for avoiding mail operations overhead.

--- account_payment_order/tests/test_bank.py
+++ account_payment_order/tests/test_bank.py
@@ -4,8 +4,15 @@
 from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
 
+from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+
 
 class TestBank(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
+
     def test_bank(self):
         bank = self.env["res.bank"].search([], limit=1)
         self.assertTrue(bank)
