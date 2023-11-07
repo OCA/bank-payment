a3fa122f97db8bff0a7b1994a96353ae1b5dcca0
Luc De Meyer <luc.demeyer@noviat.com>
2023-11-07 20:06:26 +0100

    [IMP] integrate transfer journal option into account_payment_order

--- account_payment_order/models/__init__.py
+++ account_payment_order/models/__init__.py
@@ -7,3 +7,4 @@ from . import res_bank
 from . import account_payment_method
 from . import account_journal
 from . import account_payment
+from . import res_company
--- account_payment_order_transfer_journal/readme/CONTRIBUTORS.rst
+++
@@ -1,2 +0,0 @@
-* Adrien Peiffer <adrien.peiffer@acsone.eu> (https://acsone.eu)
-* Luc De Meyer <luc.demeyer@noviat.com> (https://noviat.com)
