c7d540b2a62180d45216538919a7d7d74daf813a
Zina Rasoamanana <zina.rasoamanana@acsone.eu>
2024-02-14 15:26:22 +0100

    [16.0][MIG] account_payment_order_sequence_payment_mode: migration to 16.0

--- account_payment_order_sequence_payment_mode/models/account_payment_order.py
+++ account_payment_order_sequence_payment_mode/models/account_payment_order.py
@@ -8,18 +8,19 @@ class AccountPaymentOrder(models.Model):
 
     _inherit = "account.payment.order"
 
-    @api.model
-    def create(self, vals):
-        if vals.get("payment_mode_id"):
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get("payment_mode_id"):
 
-            mode_id = vals.get("payment_mode_id")
-            seq = (
-                self.env["account.payment.mode"]
-                .browse(mode_id)
-                .sequence_payment_order_id
-            )
-            if seq:
+                mode_id = vals.get("payment_mode_id")
+                seq = (
+                    self.env["account.payment.mode"]
+                    .browse(mode_id)
+                    .sequence_payment_order_id
+                )
+                if seq:
 
-                vals["name"] = seq._next()
+                    vals["name"] = seq._next()
 
-        return super().create(vals)
+        return super().create(vals_list)
