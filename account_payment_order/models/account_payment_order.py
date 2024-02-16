ef05873365ca2bf73f70e90823c26f27de8becae
Víctor Martínez <victor.martinez@tecnativa.com>
2024-02-16 17:43:04 +0100

    [IMP] account_payment_order: Display a message with the archived bank accounts
    TT47789

--- account_payment_order/models/account_payment_order.py
+++ account_payment_order/models/account_payment_order.py
@@ -27,6 +27,9 @@ class AccountPaymentOrder(models.Model):
         states={"draft": [("readonly", False)]},
         check_company=True,
     )
+    partner_banks_archive_msg = fields.Html(
+        compute="_compute_partner_banks_archive_msg",
+    )
     payment_type = fields.Selection(
         selection=[("inbound", "Inbound"), ("outbound", "Outbound")],
         readonly=True,
@@ -143,6 +146,28 @@ class AccountPaymentOrder(models.Model):
         compute="_compute_move_count", string="Number of Journal Entries"
     )
     description = fields.Char()
+
+    @api.depends(
+        "payment_line_ids.partner_bank_id", "payment_line_ids.partner_bank_id.active"
+    )
+    def _compute_partner_banks_archive_msg(self):
+        """Information message to show archived bank accounts and to be able
+        to act on them before confirmation (avoid duplicates)."""
+        for item in self:
+            msg_lines = []
+            for partner_bank in item.payment_line_ids.filtered(
+                lambda x: x.partner_bank_id and not x.partner_bank_id.active
+            ).mapped("partner_bank_id"):
+                msg_line = _(
+                    "<b>Account Number</b>: %(number)s - <b>Partner</b>: %(name)s"
+                ) % {
+                    "number": partner_bank.acc_number,
+                    "name": partner_bank.partner_id.display_name,
+                }
+                msg_lines.append(msg_line)
+            item.partner_banks_archive_msg = (
+                "<br/>".join(msg_lines) if len(msg_lines) > 0 else False
+            )
 
     @api.depends("payment_mode_id")
     def _compute_allowed_journal_ids(self):
