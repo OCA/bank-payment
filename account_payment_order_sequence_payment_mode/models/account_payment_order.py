e5c360e11e70f2b7b255dfbd9c9a9f13350a28b0
Zina Rasoamanana <zina.rasoamanana@acsone.eu>
2024-02-22 12:29:40 +0100

    [IMP] account_payment_order_sequence_payment_mode: use compute to set name using sequences

--- account_payment_order_sequence_payment_mode/models/account_payment_order.py
+++ account_payment_order_sequence_payment_mode/models/account_payment_order.py
@@ -1,26 +1,36 @@
 # © 2022 Acsone (<http://www.acsone.eu>))
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class AccountPaymentOrder(models.Model):
 
     _inherit = "account.payment.order"
 
+    name = fields.Char(
+        compute="_compute_name",
+        store=True,
+    )
+
+    @api.depends("payment_mode_id.sequence_payment_order_id")
+    def _compute_name(self):
+        # do not increment sequence when computing on new id
+        for rec in self.filtered("id"):
+            if rec.payment_mode_id and rec.payment_mode_id.sequence_payment_order_id:
+                rec.name = rec.payment_mode_id.sequence_payment_order_id._next()
+            else:
+                rec.name = (
+                    self.env["ir.sequence"].next_by_code("account.payment.order")
+                    or "New"
+                )
+
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-            if vals.get("payment_mode_id"):
-
-                mode_id = vals.get("payment_mode_id")
-                seq = (
-                    self.env["account.payment.mode"]
-                    .browse(mode_id)
-                    .sequence_payment_order_id
-                )
-                if seq:
-
-                    vals["name"] = seq._next()
+            if not vals.get("name"):
+                # Put val in name to avoid computation of sequence in create
+                # It was done in module account_payment_order
+                vals["name"] = "TMP"
 
         return super().create(vals_list)
