28fc2351e2feda4e1cbe6685cf6d29017a40dc5c
Alberto Martínez <alberto.martinez@sygel.es>
2024-03-11 13:27:46 +0100

    [16.0][ADD] account_banking_mandate_sale_contact: new module

---
+++ account_banking_mandate_sale_contact/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Banking Mandate Sale Contact",
+    "summary": "Add a specific contact mandate to sale orders",
+    "version": "16.0.1.0.0",
+    "category": "Banking addons",
+    "website": "https://github.com/OCA/bank-payment",
+    "author": "Alberto Martínez, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["account_banking_mandate_contact", "account_banking_mandate_sale"],
+    "data": ["views/res_config_settings.xml", "views/res_partner.xml"],
+}
