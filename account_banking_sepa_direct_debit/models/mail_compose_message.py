0ab7af5b62440a410694520a7187737e2922763f
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2024-03-25 22:32:41 +0100

    [16.0][IMP] account_banking_sepa_direct_debit: Button send mandate per email

---
+++ account_banking_sepa_direct_debit/models/mail_compose_message.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Tecnativa - Carolina Fernandez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class MailComposeMessage(models.TransientModel):
+    _inherit = "mail.compose.message"
+
+    def _action_send_mail(self, auto_commit=False):
+        for wizard in self:
+            if self.env.context.get("is_sent"):
+                self.env[wizard.model].sudo().browse(wizard.res_id).is_sent = True
+        return super(MailComposeMessage, self)._action_send_mail(
+            auto_commit=auto_commit
+        )
