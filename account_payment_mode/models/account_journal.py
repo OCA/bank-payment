64ba56d0b2a779ce8f4dba8f1be23a6965fb9df1
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2024-06-18 09:31:53 +0200

    [FIX] account_payment_mode: do not propose an electronic payment method when creating a journal if it has already been assigned.
    Previously, when you tried to install this module and the module payment_authorize
    at the same time, an error would raise because payment_authorize creates an
    electronic payment method, that is then assigned to the cash and bank accounts
    when the chart of accounts is loaded, resulting in an error.

--- account_payment_mode/models/account_journal.py
+++ account_payment_mode/models/account_journal.py
@@ -4,6 +4,7 @@
 
 from odoo import _, api, models
 from odoo.exceptions import ValidationError
+from odoo.tools import config
 
 
 class AccountJournal(models.Model):
@@ -28,7 +29,40 @@ class AccountJournal(models.Model):
                 ("code", "not in", unique_codes),  # filter out unique codes
             ]
         )
-        return all_in
+        # 'electronic' are linked to a single journal per company per provider.
+        electronic_codes = [
+            code
+            for code, info in method_info.items()
+            if info.get("mode") == "electronic"
+        ]
+        if config["test_enable"]:
+            # This is a hack to be able to test the electronic payment methods,
+            # as they are defined when you install the account_payment module,
+            # but the restrictions on electronic codes live in the account module.
+            test_code = self.env.context.get("electronic_code", False)
+            if test_code:
+                electronic_codes.append(test_code)
+        electronic_codes = tuple(electronic_codes)
+        electronic_methods = self.env["account.payment.method"].search(
+            [
+                ("payment_type", "=", "inbound"),
+                ("code", "in", electronic_codes),  # filter out unique codes
+            ]
+        )
+        electronic_method_lines_already_assigned = self.env[
+            "account.payment.method.line"
+        ].search(
+            [
+                ("payment_method_id", "in", electronic_methods.ids),
+                ("journal_id.company_id", "=", self.company_id.id),
+                ("journal_id", "!=", self.id),
+            ]
+        )
+        electronic_methods_already_assigned = (
+            electronic_method_lines_already_assigned.mapped("payment_method_id")
+        )
+        res = all_in - electronic_methods_already_assigned
+        return res
 
     @api.constrains("company_id")
     def company_id_account_payment_mode_constrains(self):
