6a83295f068199fc841dc28e171e3a769e0599f7
Alexis de Lattre <alexis.delattre@akretion.com>
2024-08-22 00:23:37 +0200

    Big cleanup of PAIN XML code generation
    Use lxml.objectify instead of lxml.etree for XML generation
    Add support for currencies with decimal places != 2
    Stop using safe_eval() in _prepare_field()
    Add arg 'raise_if_oversized' in _prepare_field() to avoid cutting fields
    that should never be cut
    More robust implementation of allowed characters in _prepare_field():
    filter on the allower characters instead of filtering in unallowed
    characters
    Don't use _prepare_field() where it is not really needed (on dates,
    country code, currency code)
    Remove support for pain.001.001.02: nobody uses it (pain.001.001.03 must
    be used according to EPC) and it adds maintenance burden
    Switch methods from public to private where relevant
    Remove noqa: C901 in account_banking_sepa_credit_transfer
    Add tests in account_banking_pain_base

--- account_banking_sepa_credit_transfer/models/account_payment_order.py
+++ account_banking_sepa_credit_transfer/models/account_payment_order.py
@@ -2,16 +2,16 @@
 # Copyright 2014-2022 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from lxml import etree
+from lxml import objectify
 
-from odoo import _, fields, models
+from odoo import _, models
 from odoo.exceptions import UserError
 
 
 class AccountPaymentOrder(models.Model):
     _inherit = "account.payment.order"
 
-    def generate_payment_file(self):  # noqa: C901
+    def generate_payment_file(self):
         """Creates the SEPA Credit Transfer file. That's the important code!"""
         self.ensure_one()
         if self.payment_method_id.code != "sepa_credit_transfer":
@@ -23,11 +23,7 @@ class AccountPaymentOrder(models.Model):
         # pain.001.001.03.ch.02 (cf l10n_ch_sepa)
         if not pain_flavor:
             raise UserError(_("PAIN version '%s' is not supported.") % pain_flavor)
-        if pain_flavor.startswith("pain.001.001.02"):
-            bic_xml_tag = "BIC"
-            name_maxsize = 70
-            root_xml_tag = "pain.001.001.02"
-        elif pain_flavor.startswith("pain.001.001.03"):
+        elif pain_flavor.startswith(("pain.001.001.03", "pain.001.003.03")):
             bic_xml_tag = "BIC"
             # size 70 -> 140 for <Nm> with pain.001.001.03
             # BUT the European Payment Council, in the document
@@ -38,25 +34,15 @@ class AccountPaymentOrder(models.Model):
             # so we follow the "European Payment Council"
             # and we put 70 and not 140
             name_maxsize = 70
-            root_xml_tag = "CstmrCdtTrfInitn"
-        elif pain_flavor.startswith("pain.001.001.04"):
+        elif pain_flavor.startswith(("pain.001.001.04", "pain.001.001.05")):
             bic_xml_tag = "BICFI"
             name_maxsize = 140
-            root_xml_tag = "CstmrCdtTrfInitn"
-        elif pain_flavor.startswith("pain.001.001.05"):
-            bic_xml_tag = "BICFI"
-            name_maxsize = 140
-            root_xml_tag = "CstmrCdtTrfInitn"
         # added pain.001.003.03 for German Banks
         # it is not in the offical ISO 20022 documentations, but nearly all
         # german banks are working with this instead 001.001.03
-        elif pain_flavor == "pain.001.003.03":
-            bic_xml_tag = "BIC"
-            name_maxsize = 70
-            root_xml_tag = "CstmrCdtTrfInitn"
         else:
             raise UserError(_("PAIN version '%s' is not supported.") % pain_flavor)
-        xsd_file = self.payment_method_id.get_xsd_file_path()
+        xsd_file = self.payment_method_id._get_xsd_file_path()
         gen_args = {
             "bic_xml_tag": bic_xml_tag,
             "name_maxsize": name_maxsize,
@@ -66,13 +52,12 @@ class AccountPaymentOrder(models.Model):
             "pain_flavor": pain_flavor,
             "pain_xsd_file": xsd_file,
         }
-        nsmap = self.generate_pain_nsmap()
-        attrib = self.generate_pain_attrib()
-        xml_root = etree.Element("Document", nsmap=nsmap, attrib=attrib)
-        pain_root = etree.SubElement(xml_root, root_xml_tag)
+        nsmap = self._generate_pain_nsmap()
+        attrib = self._generate_pain_attrib()
+        xml_root = objectify.Element("Document", nsmap=nsmap, attrib=attrib)
+        pain_root = objectify.SubElement(xml_root, "CstmrCdtTrfInitn")
         # A. Group header
-        header = self.generate_group_header_block(pain_root, gen_args)
-        group_header, nb_of_transactions_a, control_sum_a = header
+        group_header = self._generate_group_header_block(pain_root, gen_args)
         transactions_count_a = 0
         amount_control_sum_a = 0.0
         lines_per_group = {}
@@ -96,83 +81,43 @@ class AccountPaymentOrder(models.Model):
             lines_per_group.items()
         ):
             # B. Payment info
-            requested_date = fields.Date.to_string(requested_date)
-            (
-                payment_info,
-                nb_of_transactions_b,
-                control_sum_b,
-            ) = self.generate_start_payment_info_block(
+            payment_info = self._generate_start_payment_info_block(
                 pain_root,
-                "self.name + '-' "
-                "+ requested_date.replace('-', '')  + '-' + priority + "
-                "'-' + local_instrument + '-' + category_purpose",
+                f"{self.name}-{requested_date.strftime('%Y%m%d')}-"
+                f"{priority}-{local_instrument or 'NOin'}-"
+                f"{categ_purpose or 'NOpu'}",
                 priority,
                 local_instrument,
                 categ_purpose,
                 False,
                 requested_date,
-                {
-                    "self": self,
-                    "priority": priority,
-                    "requested_date": requested_date,
-                    "local_instrument": local_instrument or "NOinstr",
-                    "category_purpose": categ_purpose or "NOcateg",
-                },
                 gen_args,
             )
-            self.generate_party_block(
+            self._generate_party_block(
                 payment_info, "Dbtr", "B", self.company_partner_bank_id, gen_args
             )
-            charge_bearer = etree.SubElement(payment_info, "ChrgBr")
-            if self.sepa:
-                charge_bearer_text = "SLEV"
-            else:
-                charge_bearer_text = self.charge_bearer
-            charge_bearer.text = charge_bearer_text
+            self._generate_charge_bearer(payment_info)
             transactions_count_b = 0
             amount_control_sum_b = 0.0
             for line in lines:
                 transactions_count_a += 1
                 transactions_count_b += 1
                 # C. Credit Transfer Transaction Info
-                credit_transfer_transaction_info = etree.SubElement(
+                credit_transfer_transaction_info = objectify.SubElement(
                     payment_info, "CdtTrfTxInf"
                 )
-                payment_identification = etree.SubElement(
+                payment_identification = objectify.SubElement(
                     credit_transfer_transaction_info, "PmtId"
                 )
-                instruction_identification = etree.SubElement(
-                    payment_identification, "InstrId"
+                payment_identification.InstrId = self._prepare_field(
+                    "Instruction Identification", str(line.move_id.id), 35, gen_args
                 )
-                instruction_identification.text = self._prepare_field(
-                    "Instruction Identification",
-                    "str(line.move_id.id)",
-                    {"line": line},
-                    35,
-                    gen_args=gen_args,
+                payment_identification.EndToEndId = self._prepare_field(
+                    "End to End Identification", str(line.move_id.id), 35, gen_args
                 )
-                end2end_identification = etree.SubElement(
-                    payment_identification, "EndToEndId"
-                )
-                end2end_identification.text = self._prepare_field(
-                    "End to End Identification",
-                    "str(line.move_id.id)",
-                    {"line": line},
-                    35,
-                    gen_args=gen_args,
-                )
-                currency_name = self._prepare_field(
-                    "Currency Code",
-                    "line.currency_id.name",
-                    {"line": line},
-                    3,
-                    gen_args=gen_args,
-                )
-                amount = etree.SubElement(credit_transfer_transaction_info, "Amt")
-                instructed_amount = etree.SubElement(
-                    amount, "InstdAmt", Ccy=currency_name
-                )
-                instructed_amount.text = "%.2f" % line.amount
+                amount = objectify.SubElement(credit_transfer_transaction_info, "Amt")
+                amount.InstdAmt = line.currency_id._pain_format(line.amount)
+                amount.InstdAmt.set("Ccy", line.currency_id.name)
                 amount_control_sum_a += line.amount
                 amount_control_sum_b += line.amount
                 if not line.partner_bank_id:
@@ -183,7 +128,7 @@ class AccountPaymentOrder(models.Model):
                         ).format(partner=line.partner_id.name, reference=line.name)
                     )
 
-                self.generate_party_block(
+                self._generate_party_block(
                     credit_transfer_transaction_info,
                     "Cdtr",
                     "C",
@@ -192,20 +137,15 @@ class AccountPaymentOrder(models.Model):
                     line,
                 )
                 payment_line = line.payment_line_ids[0]
-                payment_line.generate_purpose(credit_transfer_transaction_info)
-                payment_line.generate_regulatory_reporting(
+                payment_line._generate_purpose(credit_transfer_transaction_info)
+                payment_line._generate_regulatory_reporting(
                     credit_transfer_transaction_info, gen_args
                 )
-                self.generate_remittance_info_block(
+                self._generate_remittance_info_block(
                     credit_transfer_transaction_info, line, gen_args
                 )
-            if not pain_flavor.startswith("pain.001.001.02"):
-                nb_of_transactions_b.text = str(transactions_count_b)
-                control_sum_b.text = "%.2f" % amount_control_sum_b
-        if not pain_flavor.startswith("pain.001.001.02"):
-            nb_of_transactions_a.text = str(transactions_count_a)
-            control_sum_a.text = "%.2f" % amount_control_sum_a
-        else:
-            nb_of_transactions_a.text = str(transactions_count_a)
-            control_sum_a.text = "%.2f" % amount_control_sum_a
-        return self.finalize_sepa_file_creation(xml_root, gen_args)
+            payment_info.NbOfTxs = str(transactions_count_b)
+            payment_info.CtrlSum = self._format_control_sum(amount_control_sum_b)
+        group_header.NbOfTxs = str(transactions_count_a)
+        group_header.CtrlSum = self._format_control_sum(amount_control_sum_a)
+        return self._finalize_sepa_file_creation(xml_root, gen_args)
--- account_banking_sepa_credit_transfer/readme/DESCRIPTION.rst
+++ account_banking_sepa_credit_transfer/readme/DESCRIPTION.rst
@@ -2,7 +2,7 @@ Module to export payment orders in SEPA XML file format.
 
 SEPA PAIN (PAyment INitiation) is the new european standard for
 Customer-to-Bank payment instructions. This module implements SEPA Credit
-Transfer (SCT), more specifically PAIN versions 001.001.02, 001.001.03,
+Transfer (SCT), more specifically PAIN versions 001.001.03,
 001.001.04 and 001.001.05. It is part of the ISO 20022 standard, available on
 https://www.iso20022.org.
 
