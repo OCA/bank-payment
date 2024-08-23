2e9a08373b168b847dd7e52fda0eab7136465768
Alexis de Lattre <alexis.delattre@akretion.com>
2024-08-24 01:34:08 +0200

    [IMP] account_banking_mandate: add constraint on payment method
    On payment method, check that mandate_required is only be True on
    inbound methods.

--- account_banking_mandate/models/account_payment_method.py
+++ account_banking_mandate/models/account_payment_method.py
@@ -1,7 +1,8 @@
 # Copyright 2016-2020 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class AccountPaymentMethod(models.Model):
@@ -11,3 +12,15 @@ class AccountPaymentMethod(models.Model):
         help="Activate this option if this payment method requires your "
         "customer to sign a direct debit mandate with your company.",
     )
+
+    @api.constrains("mandate_required", "payment_type")
+    def _check_mandate_required(self):
+        for method in self:
+            if method.payment_type != "inbound" and method.mandate_required:
+                raise ValidationError(
+                    _(
+                        "The option 'Mandate Required' cannot be enabled on "
+                        "payment method %s which is not an inbound payment method."
+                    )
+                    % method.display_name
+                )
