5ca9e0b95c8d6ffa7a1c7610ef33cde547ecdfa2
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2024-09-20 09:14:59 +0200

    [MIG] account_payment_mode_default_account: Migration to 16.0

--- account_payment_mode_default_account/models/account_move.py
+++ account_payment_mode_default_account/models/account_move.py
@@ -1,5 +1,8 @@
 # Copyright 2022 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+from contextlib import contextmanager
+
 from odoo import api, models
 
 
@@ -7,32 +10,50 @@ class AccountMove(models.Model):
 
     _inherit = "account.move"
 
-    def _recompute_payment_terms_lines(self):
-        if self.payment_mode_id:
-            return super(
-                AccountMove,
-                self.with_context(
-                    _partner_property_account_payment_mode=self.payment_mode_id.id
-                ),
-            )._recompute_payment_terms_lines()
-        else:
-            return super()._recompute_payment_terms_lines()
-
     def _get_payment_term_lines(self):
         self.ensure_one()
         return self.line_ids.filtered(
-            lambda line: line.account_id.user_type_id.type in ("receivable", "payable")
+            lambda line: line.account_id.account_type
+            in ("asset_receivable", "liability_payable")
         )
+
+    def _change_account_on_lines(self):
+        self.ensure_one()
+        payment_term_lines = self._get_payment_term_lines()
+        payment_mode_id = self.payment_mode_id.id
+        partner = self.partner_id.with_context(
+            _partner_property_account_payment_mode=payment_mode_id
+        )
+        if self.is_sale_document(include_receipts=True):
+            payment_term_lines.account_id = partner.property_account_receivable_id
+        else:
+            payment_term_lines.account_id = partner.property_account_payable_id
+
+    @contextmanager
+    def _sync_dynamic_line(
+        self,
+        existing_key_fname,
+        needed_vals_fname,
+        needed_dirty_fname,
+        line_type,
+        container,
+    ):
+        with super()._sync_dynamic_line(
+            existing_key_fname,
+            needed_vals_fname,
+            needed_dirty_fname,
+            line_type,
+            container,
+        ):
+            yield
+        if line_type == "payment_term":
+            invoices = container.get("records")
+            invoices = invoices.filtered(lambda invoice: invoice.state == "draft")
+            for inv in invoices:
+                inv._change_account_on_lines()
 
     @api.onchange("payment_mode_id")
     def _onchange_payment_mode_id(self):
         if self.payment_mode_id and self.partner_id:
-            payment_term_lines = self._get_payment_term_lines()
-            partner = self.partner_id.with_context(
-                _partner_property_account_payment_mode=self.payment_mode_id.id
-            )
-            # Retrieve account from partner.
-            if self.is_sale_document(include_receipts=True):
-                payment_term_lines.account_id = partner.property_account_receivable_id
-            else:
-                payment_term_lines.account_id = partner.property_account_payable_id
+            if self.state == "draft":
+                self._change_account_on_lines()
