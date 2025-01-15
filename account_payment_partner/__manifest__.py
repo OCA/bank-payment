4a27533cdca1eb71bf959a11ee031d5e2c239824
Enric Tobella <enric.tobella@dixmit.com>
2025-01-15 13:05:40 +0100

    [FIX] account_payment_partner: Fix tests

--- account_payment_partner/__manifest__.py
+++ account_payment_partner/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Account Payment Partner",
-    "version": "17.0.1.0.5",
+    "version": "17.0.1.0.6",
     "category": "Banking addons",
     "license": "AGPL-3",
     "summary": "Adds payment mode on partners and invoices",
