41428702df25e94fcf1a6039738641a55681a0e8
Patrick Trabocchi <patricktrabocchi@openforce.it>
2025-03-14 10:55:59 +0100

    [FIX] account_payment_mode: check if records to converts exists
    In case the field to convert is empty the method called break because
    no translation value are found; so check if there are records to be
    converted and in that case call the field conversion method

--- account_payment_mode/migrations/16.0.2.0.0/post-migrate.py
+++ account_payment_mode/migrations/16.0.2.0.0/post-migrate.py
@@ -6,6 +6,10 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     """Migrate note field from Text to Html"""
-    openupgrade.convert_field_to_html(
-        env.cr, "account_payment_mode", "note", "note", False, True
-    )
+
+    if openupgrade.column_exists(env.cr, "account_payment_mode", "note"):
+        records = env["account.payment.mode"].search_count([("note", "!=", False)])
+        if records:
+            openupgrade.convert_field_to_html(
+                env.cr, "account_payment_mode", "note", "note", False, True
+            )
