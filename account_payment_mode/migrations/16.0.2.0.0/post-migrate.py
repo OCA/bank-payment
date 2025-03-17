37a717875172ebdb0ebcf24236342d74186ba8db
Enric Tobella <enric.tobella@dixmit.com>
2025-03-17 17:40:16 +0100

    [OU-FIX] account_payment_mode: Ensure that all lines have the right translation

--- account_payment_mode/migrations/16.0.2.0.0/post-migrate.py
+++ account_payment_mode/migrations/16.0.2.0.0/post-migrate.py
@@ -6,6 +6,9 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     """Migrate note field from Text to Html"""
+    openupgrade.logged_query(
+        env.cr, "UPDATE account_payment_mode SET note = '{}'::jsonb where note is NULL"
+    )
     openupgrade.convert_field_to_html(
         env.cr, "account_payment_mode", "note", "note", False, True
     )
