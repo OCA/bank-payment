9410c4cb4f498c51dc81ebe9c3ea5267e89b9259
Vicent-S73 <vicent@studio73.es>
2025-11-14 12:05:45 +0100

    [MIG] account_payment_order_return: Migration to 19.0

--- account_payment_order_return/__manifest__.py
+++ account_payment_order_return/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Payment Order Return",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Banking addons",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/bank-payment",
